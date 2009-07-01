OpenHarmony-TPC/chromium_arkweb

commit afd0ec1651967cfa125a5356afa7a4057329b953

Take two for http://codereview.chromium.org/131094 This time without browser_tests timeout that caused the revert.  The problem was BrowsingHistoryHandler trying to use a dom_ui_ before being attached.
Review URL: http://codereview.chromium.org/149117

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19556 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/dom_ui/new_tab_ui.cc
@@ -458,8 +458,12 @@ void IncognitoTabHTMLSource::StartDataRequest(const std::string& path,
 class MostVisitedHandler : public DOMMessageHandler,
                            public NotificationObserver {
  public:
-  explicit MostVisitedHandler(DOMUI* dom_ui);
-  virtual ~MostVisitedHandler();
+  MostVisitedHandler() : url_blacklist_(NULL), pinned_urls_(NULL) {}
+  virtual ~MostVisitedHandler() { }
+
+  // DOMMessageHandler override and implementation.
+  virtual DOMMessageHandler* Attach(DOMUI* dom_ui);
+  virtual void RegisterMessages();
 
   // Callback for the "getMostVisited" message.
   void HandleGetMostVisited(const Value* value);
@@ -533,33 +537,11 @@ class MostVisitedHandler : public DOMMessageHandler,
   DISALLOW_COPY_AND_ASSIGN(MostVisitedHandler);
 };
 
-MostVisitedHandler::MostVisitedHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui) {
-  // Register ourselves as the handler for the "mostvisited" message from
-  // Javascript.
-  dom_ui_->RegisterMessageCallback("getMostVisited",
-      NewCallback(this, &MostVisitedHandler::HandleGetMostVisited));
-
-  // Register ourselves for any most-visited item blacklisting.
-  dom_ui_->RegisterMessageCallback("blacklistURLFromMostVisited",
-      NewCallback(this, &MostVisitedHandler::HandleBlacklistURL));
-  dom_ui_->RegisterMessageCallback("removeURLsFromMostVisitedBlacklist",
-      NewCallback(this, &MostVisitedHandler::HandleRemoveURLsFromBlacklist));
-  dom_ui_->RegisterMessageCallback("clearMostVisitedURLsBlacklist",
-      NewCallback(this, &MostVisitedHandler::HandleClearBlacklist));
-
-  url_blacklist_ = dom_ui_->GetProfile()->GetPrefs()->
+DOMMessageHandler* MostVisitedHandler::Attach(DOMUI* dom_ui) {
+  url_blacklist_ = dom_ui->GetProfile()->GetPrefs()->
       GetMutableDictionary(prefs::kNTPMostVisitedURLsBlacklist);
-
-  // Register ourself for pinned URL messages.
-  dom_ui->RegisterMessageCallback("addPinnedURL",
-      NewCallback(this, &MostVisitedHandler::HandleAddPinnedURL));
-  dom_ui->RegisterMessageCallback("removePinnedURL",
-      NewCallback(this, &MostVisitedHandler::HandleRemovePinnedURL));
-
-  pinned_urls_ = dom_ui_->GetProfile()->GetPrefs()->
-    GetMutableDictionary(prefs::kNTPMostVisitedPinnedURLs);
-
+  pinned_urls_ = dom_ui->GetProfile()->GetPrefs()->
+      GetMutableDictionary(prefs::kNTPMostVisitedPinnedURLs);
   // Set up our sources for thumbnail and favicon data. Since we may be in
   // testing mode with no I/O thread, only add our handler when an I/O thread
   // exists. Ownership is passed to the ChromeURLDataManager.
@@ -576,10 +558,30 @@ MostVisitedHandler::MostVisitedHandler(DOMUI* dom_ui)
 
   // Get notifications when history is cleared.
   registrar_.Add(this, NotificationType::HISTORY_URLS_DELETED,
-                 Source<Profile>(dom_ui_->GetProfile()));
+      Source<Profile>(dom_ui->GetProfile()));
+
+  return DOMMessageHandler::Attach(dom_ui);
 }
 
-MostVisitedHandler::~MostVisitedHandler() {
+void MostVisitedHandler::RegisterMessages() {
+  // Register ourselves as the handler for the "mostvisited" message from
+  // Javascript.
+  dom_ui_->RegisterMessageCallback("getMostVisited",
+      NewCallback(this, &MostVisitedHandler::HandleGetMostVisited));
+
+  // Register ourselves for any most-visited item blacklisting.
+  dom_ui_->RegisterMessageCallback("blacklistURLFromMostVisited",
+      NewCallback(this, &MostVisitedHandler::HandleBlacklistURL));
+  dom_ui_->RegisterMessageCallback("removeURLsFromMostVisitedBlacklist",
+      NewCallback(this, &MostVisitedHandler::HandleRemoveURLsFromBlacklist));
+  dom_ui_->RegisterMessageCallback("clearMostVisitedURLsBlacklist",
+      NewCallback(this, &MostVisitedHandler::HandleClearBlacklist));
+
+  // Register ourself for pinned URL messages.
+  dom_ui_->RegisterMessageCallback("addPinnedURL",
+      NewCallback(this, &MostVisitedHandler::HandleAddPinnedURL));
+  dom_ui_->RegisterMessageCallback("removePinnedURL",
+      NewCallback(this, &MostVisitedHandler::HandleRemovePinnedURL));
 }
 
 void MostVisitedHandler::HandleGetMostVisited(const Value* value) {
@@ -828,8 +830,11 @@ void MostVisitedHandler::RegisterUserPrefs(PrefService* prefs) {
 class TemplateURLHandler : public DOMMessageHandler,
                            public TemplateURLModelObserver {
  public:
-  explicit TemplateURLHandler(DOMUI* dom_ui);
+  TemplateURLHandler() : DOMMessageHandler(), template_url_model_(NULL) {}
   virtual ~TemplateURLHandler();
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
 
   // Callback for the "getMostSearched" message, sent when the page requests
   // the list of available searches.
@@ -843,25 +848,21 @@ class TemplateURLHandler : public DOMMessageHandler,
   virtual void OnTemplateURLModelChanged();
 
  private:
-  DOMUI* dom_ui_;
   TemplateURLModel* template_url_model_;  // Owned by profile.
 
   DISALLOW_COPY_AND_ASSIGN(TemplateURLHandler);
 };
 
-TemplateURLHandler::TemplateURLHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui),
-      template_url_model_(NULL) {
-  dom_ui->RegisterMessageCallback("getMostSearched",
-      NewCallback(this, &TemplateURLHandler::HandleGetMostSearched));
-  dom_ui->RegisterMessageCallback("doSearch",
-      NewCallback(this, &TemplateURLHandler::HandleDoSearch));
-}
-
 TemplateURLHandler::~TemplateURLHandler() {
   if (template_url_model_)
     template_url_model_->RemoveObserver(this);
+}
+
+void TemplateURLHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("getMostSearched",
+      NewCallback(this, &TemplateURLHandler::HandleGetMostSearched));
+  dom_ui_->RegisterMessageCallback("doSearch",
+      NewCallback(this, &TemplateURLHandler::HandleDoSearch));
 }
 
 void TemplateURLHandler::HandleGetMostSearched(const Value* content) {
@@ -989,8 +990,11 @@ void TemplateURLHandler::OnTemplateURLModelChanged() {
 class RecentlyBookmarkedHandler : public DOMMessageHandler,
                                   public BookmarkModelObserver {
  public:
-  explicit RecentlyBookmarkedHandler(DOMUI* dom_ui);
-  ~RecentlyBookmarkedHandler();
+  RecentlyBookmarkedHandler() : model_(NULL) {}
+  virtual ~RecentlyBookmarkedHandler();
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();  
 
   // Callback which navigates to the bookmarks page.
   void HandleShowBookmarkPage(const Value*);
@@ -1024,25 +1028,21 @@ class RecentlyBookmarkedHandler : public DOMMessageHandler,
   virtual void BookmarkNodeFavIconLoaded(BookmarkModel* model,
                                          const BookmarkNode* node) {}
 
-  DOMUI* dom_ui_;
   // The model we're getting bookmarks from. The model is owned by the Profile.
   BookmarkModel* model_;
 
   DISALLOW_COPY_AND_ASSIGN(RecentlyBookmarkedHandler);
 };
 
-RecentlyBookmarkedHandler::RecentlyBookmarkedHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui),
-      model_(NULL) {
-  dom_ui->RegisterMessageCallback("getRecentlyBookmarked",
-      NewCallback(this,
-                  &RecentlyBookmarkedHandler::HandleGetRecentlyBookmarked));
-}
-
 RecentlyBookmarkedHandler::~RecentlyBookmarkedHandler() {
   if (model_)
     model_->RemoveObserver(this);
+}
+
+void RecentlyBookmarkedHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("getRecentlyBookmarked",
+      NewCallback(this,
+                  &RecentlyBookmarkedHandler::HandleGetRecentlyBookmarked));
 }
 
 void RecentlyBookmarkedHandler::HandleGetRecentlyBookmarked(const Value*) {
@@ -1100,8 +1100,11 @@ void RecentlyBookmarkedHandler::BookmarkNodeChanged(BookmarkModel* model,
 class RecentlyClosedTabsHandler : public DOMMessageHandler,
                                   public TabRestoreService::Observer {
  public:
-  explicit RecentlyClosedTabsHandler(DOMUI* dom_ui);
+  RecentlyClosedTabsHandler() : tab_restore_service_(NULL) {}
   virtual ~RecentlyClosedTabsHandler();
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
 
   // Callback for the "reopenTab" message. Rewrites the history of the
   // currently displayed tab to be the one in TabRestoreService with a
@@ -1131,22 +1134,17 @@ class RecentlyClosedTabsHandler : public DOMMessageHandler,
   bool WindowToValue(const TabRestoreService::Window& window,
                      DictionaryValue* dictionary);
 
-  DOMUI* dom_ui_;
-
   // TabRestoreService that we are observing.
   TabRestoreService* tab_restore_service_;
 
   DISALLOW_COPY_AND_ASSIGN(RecentlyClosedTabsHandler);
 };
 
-RecentlyClosedTabsHandler::RecentlyClosedTabsHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui),
-      tab_restore_service_(NULL) {
-  dom_ui->RegisterMessageCallback("getRecentlyClosedTabs",
+void RecentlyClosedTabsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("getRecentlyClosedTabs",
       NewCallback(this,
                   &RecentlyClosedTabsHandler::HandleGetRecentlyClosedTabs));
-  dom_ui->RegisterMessageCallback("reopenTab",
+  dom_ui_->RegisterMessageCallback("reopenTab",
       NewCallback(this, &RecentlyClosedTabsHandler::HandleReopenTab));
 }
 
@@ -1286,21 +1284,22 @@ bool RecentlyClosedTabsHandler::WindowToValue(
 
 class HistoryHandler : public DOMMessageHandler {
  public:
-  explicit HistoryHandler(DOMUI* dom_ui);
+  HistoryHandler() {}
+  virtual ~HistoryHandler() {}
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
 
   // Callback which navigates to the history page and performs a search.
   void HandleSearchHistoryPage(const Value* content);
 
  private:
-  DOMUI* dom_ui_;
 
   DISALLOW_COPY_AND_ASSIGN(HistoryHandler);
 };
 
-HistoryHandler::HistoryHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui) {
-  dom_ui->RegisterMessageCallback("searchHistoryPage",
+void HistoryHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("searchHistoryPage",
       NewCallback(this, &HistoryHandler::HandleSearchHistoryPage));
 }
 
@@ -1336,21 +1335,22 @@ void HistoryHandler::HandleSearchHistoryPage(const Value* content) {
 // information (treat it as RecordComputedMetrics)
 class MetricsHandler : public DOMMessageHandler {
  public:
-  explicit MetricsHandler(DOMUI* dom_ui);
-
+  MetricsHandler() {}
+  virtual ~MetricsHandler() {}
+  
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
+  
   // Callback which records a user action.
   void HandleMetrics(const Value* content);
 
  private:
-  DOMUI* dom_ui_;
 
   DISALLOW_COPY_AND_ASSIGN(MetricsHandler);
 };
 
-MetricsHandler::MetricsHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui) {
-  dom_ui->RegisterMessageCallback("metrics",
+void MetricsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("metrics",
       NewCallback(this, &MetricsHandler::HandleMetrics));
 }
 
@@ -1409,26 +1409,26 @@ NewTabUI::NewTabUI(TabContents* contents)
             &ChromeURLDataManager::AddDataSource,
             html_source));
   } else {
-
     if (EnableNewNewTabPage()) {
       DownloadManager* dlm = GetProfile()->GetDownloadManager();
       DownloadsDOMHandler* downloads_handler =
-          new DownloadsDOMHandler(this, dlm);
+          new DownloadsDOMHandler(dlm);
+      downloads_handler->Attach(this);
       AddMessageHandler(downloads_handler);
       downloads_handler->Init();
 
-      AddMessageHandler(new ShownSectionsHandler(this));
+      AddMessageHandler((new ShownSectionsHandler())->Attach(this));
     }
 
+    AddMessageHandler((new TemplateURLHandler())->Attach(this));
+    AddMessageHandler((new MostVisitedHandler())->Attach(this));
+    AddMessageHandler((new RecentlyBookmarkedHandler())->Attach(this));
+    AddMessageHandler((new RecentlyClosedTabsHandler())->Attach(this));
+    AddMessageHandler((new HistoryHandler())->Attach(this));
+    AddMessageHandler((new MetricsHandler())->Attach(this));
     if (EnableWebResources())
-      AddMessageHandler(new TipsHandler(this));
+      AddMessageHandler((new TipsHandler())->Attach(this));
 
-    AddMessageHandler(new TemplateURLHandler(this));
-    AddMessageHandler(new MostVisitedHandler(this));
-    AddMessageHandler(new RecentlyBookmarkedHandler(this));
-    AddMessageHandler(new RecentlyClosedTabsHandler(this));
-    AddMessageHandler(new HistoryHandler(this));
-    AddMessageHandler(new MetricsHandler(this));
 #ifdef CHROME_PERSONALIZATION
     if (!Personalization::IsP13NDisabled(GetProfile())) {
       AddMessageHandler(Personalization::CreateNewTabPageHandler(this));

chrome/browser/dom_ui/tips_handler.cc
@@ -21,14 +21,15 @@ namespace {
       L"Tips and recommendations to help you discover interesting websites.";
 }
 
-TipsHandler::TipsHandler(DOMUI* dom_ui)
-    : DOMMessageHandler(dom_ui),
-      dom_ui_(dom_ui) {
-  dom_ui->RegisterMessageCallback("getTips",
-      NewCallback(this, &TipsHandler::HandleGetTips));
-
+DOMMessageHandler* TipsHandler::Attach(DOMUI* dom_ui) {
   tips_cache_ = dom_ui_->GetProfile()->GetPrefs()->
       GetDictionary(prefs::kNTPTipsCache);
+  return DOMMessageHandler::Attach(dom_ui);  
+}
+
+void TipsHandler::RegisterMessages() {
+  dom_ui_->RegisterMessageCallback("getTips",
+      NewCallback(this, &TipsHandler::HandleGetTips));
 }
 
 void TipsHandler::HandleGetTips(const Value* content) {

chrome/browser/dom_ui/tips_handler.h
@@ -28,9 +28,12 @@ class Value;
 
 class TipsHandler : public DOMMessageHandler {
  public:
-  explicit TipsHandler(DOMUI* dom_ui);
+  TipsHandler() : tips_cache_(NULL) {}
+  virtual ~TipsHandler() {}
 
-  TipsHandler();
+  // DOMMessageHandler implementation and overrides. 
+  virtual DOMMessageHandler* Attach(DOMUI* dom_ui); 
+  virtual void RegisterMessages();
 
   // Callback which pulls tips data from the preferences.
   void HandleGetTips(const Value* content);
@@ -41,9 +44,6 @@ class TipsHandler : public DOMMessageHandler {
  private:
   // Make sure the string we are pushing to the NTP is a valid URL.
   bool IsValidURL(const std::wstring& url_string);
-
-  // So we can push data out to the page that has called this handler.
-  DOMUI* dom_ui_;
 
   // Filled with data from cache in preferences.
   const DictionaryValue* tips_cache_;

chrome/browser/chrome_plugin_host.cc
@@ -313,6 +313,8 @@ class ModelessHtmlDialogDelegate : public HtmlDialogUIDelegate {
   virtual bool IsDialogModal() const { return false; }
   virtual std::wstring GetDialogTitle() const { return L"Gears"; }
   virtual GURL GetDialogContentURL() const { return params_.url; }
+  virtual void GetDOMMessageHandlers(
+      std::vector<DOMMessageHandler*>* handlers) const {}
   virtual void GetDialogSize(gfx::Size* size) const {
     size->set_width(params_.width);
     size->set_height(params_.height);

chrome/browser/dom_ui/dom_ui.cc
@@ -110,7 +110,10 @@ void DOMUI::ExecuteJavascript(const std::wstring& javascript) {
 ///////////////////////////////////////////////////////////////////////////////
 // DOMMessageHandler
 
-DOMMessageHandler::DOMMessageHandler(DOMUI *dom_ui) : dom_ui_(dom_ui) {
+DOMMessageHandler* DOMMessageHandler::Attach(DOMUI* dom_ui) {
+  dom_ui_ = dom_ui;
+  RegisterMessages();
+  return this;
 }
 
 // DOMMessageHandler, protected: ----------------------------------------------

chrome/browser/dom_ui/dom_ui.h
@@ -129,9 +129,13 @@ class DOMUI {
 // host is destroyed.
 class DOMMessageHandler {
  public:
-  explicit DOMMessageHandler(DOMUI* dom_ui);
+  DOMMessageHandler() : dom_ui_(NULL) {}
   virtual ~DOMMessageHandler() {};
 
+  // Attaches |this| to |dom_ui| in order to handle messages from it.  Declared
+  // virtual so that subclasses can do special init work as soon as the dom_ui
+  // is provided.  Returns |this| for convenience.
+  virtual DOMMessageHandler* Attach(DOMUI* dom_ui);
  protected:
   // Adds "url" and "title" keys on incoming dictionary, setting title
   // as the url as a fallback on empty title.
@@ -139,13 +143,16 @@ class DOMMessageHandler {
                              std::wstring title,
                              const GURL& gurl);
 
+  // This is where subclasses specify which messages they'd like to handle.
+  virtual void RegisterMessages() = 0;
+
   // Extract an integer value from a Value.
   bool ExtractIntegerValue(const Value* value, int* out_int);
 
   // Extract a string value from a Value.
   std::wstring ExtractStringValue(const Value* value);
 
-  DOMUI* const dom_ui_;
+  DOMUI* dom_ui_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DOMMessageHandler);

chrome/browser/dom_ui/downloads_dom_handler.cc
@@ -47,10 +47,28 @@ class DownloadItemSorter : public std::binary_function<DownloadItem*,
 
 } // namespace
 
-DownloadsDOMHandler::DownloadsDOMHandler(DOMUI* dom_ui, DownloadManager* dlm)
-    : DOMMessageHandler(dom_ui),
-      search_text_(),
+DownloadsDOMHandler::DownloadsDOMHandler(DownloadManager* dlm)
+    : search_text_(),
       download_manager_(dlm) {
+  // Create our fileicon data source.
+  g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
+      NewRunnableMethod(&chrome_url_data_manager,
+                        &ChromeURLDataManager::AddDataSource,
+                        new FileIconSource()));
+}
+
+DownloadsDOMHandler::~DownloadsDOMHandler() {
+  ClearDownloadItems();
+  download_manager_->RemoveObserver(this);
+}
+
+// DownloadsDOMHandler, public: -----------------------------------------------
+
+void DownloadsDOMHandler::Init() {
+  download_manager_->AddObserver(this);
+}
+
+void DownloadsDOMHandler::RegisterMessages() {
   dom_ui_->RegisterMessageCallback("getDownloads",
       NewCallback(this, &DownloadsDOMHandler::HandleGetDownloads));
   dom_ui_->RegisterMessageCallback("openFile",
@@ -73,24 +91,6 @@ DownloadsDOMHandler::DownloadsDOMHandler(DOMUI* dom_ui, DownloadManager* dlm)
       NewCallback(this, &DownloadsDOMHandler::HandleCancel));
   dom_ui_->RegisterMessageCallback("clearAll",
       NewCallback(this, &DownloadsDOMHandler::HandleClearAll));
-
-
-  // Create our fileicon data source.
-  g_browser_process->io_thread()->message_loop()->PostTask(FROM_HERE,
-      NewRunnableMethod(&chrome_url_data_manager,
-                        &ChromeURLDataManager::AddDataSource,
-                        new FileIconSource()));
-}
-
-DownloadsDOMHandler::~DownloadsDOMHandler() {
-  ClearDownloadItems();
-  download_manager_->RemoveObserver(this);
-}
-
-// DownloadsDOMHandler, public: -----------------------------------------------
-
-void DownloadsDOMHandler::Init() {
-  download_manager_->AddObserver(this);
 }
 
 void DownloadsDOMHandler::OnDownloadUpdated(DownloadItem* download) {

chrome/browser/dom_ui/downloads_dom_handler.h
@@ -19,10 +19,13 @@ class DownloadsDOMHandler : public DOMMessageHandler,
                             public DownloadManager::Observer,
                             public DownloadItem::Observer {
  public:
-  explicit DownloadsDOMHandler(DOMUI* dom_ui, DownloadManager* dlm);
+  explicit DownloadsDOMHandler(DownloadManager* dlm);
   virtual ~DownloadsDOMHandler();
 
   void Init();
+
+  // DOMMessageHandler implementation.
+  virtual void RegisterMessages();
 
   // DownloadItem::Observer interface
   virtual void OnDownloadUpdated(DownloadItem* download);

commit 1386a6de2b49e327fc00b3a088a167c02e7042d5

Remove null link from "welcome to tips" default suggestion.  Also make some hard-coded strings in new tab page into localizable .grd strings, and fix bug introduced in r19556.
BUG= http://crbug.com/15467
TEST= Run with new new tab page for the first time, with no tips stored in preferences.  Note that welcome tip is not a link.

Review URL: http://codereview.chromium.org/147247

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19740 0039d316-1c4b-4281-b951-d872f2087c98

chrome/app/generated_resources.grd
@@ -3440,6 +3440,14 @@ each locale. -->
                desc="Heading text for the section containing recent activities. Recent activies includes closed windows and tabs as well as downloads.">
         Recent activities
       </message>
+      <message name="IDS_NEW_TAB_TIPS"
+               desc="Heading text for the section containing tips and suggestions.">
+        Tips and Suggestions
+      </message>
+      <message name="IDS_NEW_TAB_DEFAULT_TIPS_TITLE"
+               desc="Default content of tip box when no tips are available.  This is a short advertisement and explanation for the tips and suggestions to come.">
+        Watch this space for tips and suggestions for better browsing!
+      </message>
       <message name="IDS_NEW_TAB_DOWNLOADS"
                desc="The header text of the recent downloads section.">
         Downloads

chrome/browser/dom_ui/new_tab_ui.cc
@@ -297,6 +297,10 @@ void NewTabHTMLSource::StartDataRequest(const std::string& path,
       l10n_util::GetString(IDS_NEW_TAB_WEB_RESOURCE_CACHE));
   localized_strings.SetString(L"recentactivities",
       l10n_util::GetString(IDS_NEW_TAB_RECENT_ACTIVITIES));
+  localized_strings.SetString(L"tipsandsuggestions",
+      l10n_util::GetString(IDS_NEW_TAB_TIPS));
+  localized_strings.SetString(L"defaulttipstitle",
+      l10n_util::GetString(IDS_NEW_TAB_DEFAULT_TIPS_TITLE));
   localized_strings.SetString(L"downloads",
       l10n_util::GetString(IDS_NEW_TAB_DOWNLOADS));
   localized_strings.SetString(L"viewfullhistory",
@@ -1490,6 +1494,7 @@ bool NewTabUI::UseOldNewTabPage() {
   return command_line->HasSwitch(switches::kOldNewTabPage);
 }
 
+// static
 bool NewTabUI::EnableWebResources() {
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
   return command_line->HasSwitch(switches::kWebResources);

chrome/browser/dom_ui/tips_handler.cc
@@ -22,9 +22,10 @@ namespace {
 }
 
 DOMMessageHandler* TipsHandler::Attach(DOMUI* dom_ui) {
+  dom_ui_ = dom_ui;
   tips_cache_ = dom_ui_->GetProfile()->GetPrefs()->
       GetDictionary(prefs::kNTPTipsCache);
-  return DOMMessageHandler::Attach(dom_ui);  
+  return DOMMessageHandler::Attach(dom_ui);
 }
 
 void TipsHandler::RegisterMessages() {

chrome/browser/dom_ui/tips_handler.h
@@ -31,8 +31,8 @@ class TipsHandler : public DOMMessageHandler {
   TipsHandler() : tips_cache_(NULL) {}
   virtual ~TipsHandler() {}
 
-  // DOMMessageHandler implementation and overrides. 
-  virtual DOMMessageHandler* Attach(DOMUI* dom_ui); 
+  // DOMMessageHandler implementation and overrides.
+  virtual DOMMessageHandler* Attach(DOMUI* dom_ui);
   virtual void RegisterMessages();
 
   // Callback which pulls tips data from the preferences.
@@ -44,6 +44,9 @@ class TipsHandler : public DOMMessageHandler {
  private:
   // Make sure the string we are pushing to the NTP is a valid URL.
   bool IsValidURL(const std::wstring& url_string);
+
+  // So we can push data out to the page that has called this handler.
+  DOMUI* dom_ui_;
 
   // Filled with data from cache in preferences.
   const DictionaryValue* tips_cache_;

chrome/browser/resources/new_new_tab.css
@@ -439,6 +439,17 @@ html[dir='rtl'] .item {
   text-align: right;
 }
 
+.tips-title {
+  color: black;
+  text-decoration: none;
+  padding-top: 6px;
+  white-space: normal;
+}
+
+.tips-container {
+  padding-top: 4px;
+}
+
 .window {
   position: relative;
   overflow: visible; /* We use visible so that the menu can be a child and shown

chrome/browser/resources/new_new_tab.html
@@ -164,10 +164,15 @@ logEvent('log start');
     </div><div class="spacer">
 
     </div><div id="tips" class="section">
-      <h2>Tips and Suggestions</h2>
-      <div class="item-container">
-        <div id="tip-items" jsskip="!processing">
+      <h2 jscontent="tipsandsuggestions"></h2>
+      <div id="tip-items" jsskip="!processing">
+        <div class="tips-title item" jsselect="$this"
+            jsdisplay="!url"
+            jscontent="localStrings.getString('defaulttipstitle')">
+        </div>
+        <div class="tips-container item-container">
           <a class="item" jsselect="$this"
+              jsdisplay="url.length"
               jsvalues="href:url;title:title"
               jscontent="title"></a>
         </div>