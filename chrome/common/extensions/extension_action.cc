OpenHarmony-TPC/chromium_arkweb

commit 469ce726cf419590e982d67d62c25b77b3727abc

Force the same even-ness between the badge and the icon. Otherwise, the badge looks off center.
BUG=26400
TEST=I guess this is just a matter of eyeballing
it. The badge should not look off-center when
compared to the icon, even with different sized
icons  and different width of text in the badge.

Review URL: http://codereview.chromium.org/362007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30994 0039d316-1c4b-4281-b951-d872f2087c98

chrome/common/extensions/extension_action.cc
@@ -106,7 +106,7 @@ void ExtensionAction::PaintBadge(gfx::Canvas* canvas,
     text_color = SK_ColorWHITE;
 
   if (SkColorGetA(background_color) == 0x00)
-    background_color = SkColorSetARGB(255, 218, 0, 24);  // default badge color
+    background_color = SkColorSetARGB(255, 218, 0, 24);  // Default badge color.
 
   canvas->save();
 
@@ -118,9 +118,14 @@ void ExtensionAction::PaintBadge(gfx::Canvas* canvas,
   text_width = SkIntToScalar(
       std::min(kMaxTextWidth, SkScalarFloor(text_width)));
 
-  // Cacluate badge size. It is clamped to a min width just because it looks
+  // Calculate badge size. It is clamped to a min width just because it looks
   // silly if it is too skinny.
   int badge_width = SkScalarFloor(text_width) + kPadding * 2;
+  int icon_width = GetIcon(tab_id).width();
+  // Force the pixel width of badge to be either odd (if the icon width is odd)
+  // or even otherwise. If there is a mismatch you get http://crbug.com/26400.
+  if (icon_width != 0 && (badge_width % 2 != GetIcon(tab_id).width() % 2))
+    badge_width += 1;
   badge_width = std::max(kBadgeHeight, badge_width);
 
   // Paint the badge background color in the right location. It is usually
@@ -129,7 +134,9 @@ void ExtensionAction::PaintBadge(gfx::Canvas* canvas,
   rect.fBottom = SkIntToScalar(bounds.bottom() - kBottomMargin);
   rect.fTop = rect.fBottom - SkIntToScalar(kBadgeHeight);
   if (badge_width >= kCenterAlignThreshold) {
-    rect.fLeft = SkIntToScalar(bounds.CenterPoint().x() - badge_width / 2);
+    rect.fLeft = SkIntToScalar(
+                     SkScalarFloor(SkIntToScalar(bounds.width() / 2.0) -
+                                   SkIntToScalar(badge_width / 2.0)));
     rect.fRight = rect.fLeft + SkIntToScalar(badge_width);
   } else {
     rect.fRight = SkIntToScalar(bounds.right());

commit 168b297e909fc05d2fb5d97191495363cbb7d448

Fix badge center-alignment logic.
This was initially fixed in r29152. It regressed in r30994.

BUG=27648
TEST=see bug

Review URL: http://codereview.chromium.org/506003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34513 0039d316-1c4b-4281-b951-d872f2087c98

chrome/common/extensions/extension_action.cc
@@ -137,7 +137,8 @@ void ExtensionAction::PaintBadge(gfx::Canvas* canvas,
   rect.fTop = rect.fBottom - SkIntToScalar(kBadgeHeight);
   if (badge_width >= kCenterAlignThreshold) {
     rect.fLeft = SkIntToScalar(
-                     SkScalarFloor(SkIntToScalar(bounds.width() / 2.0) -
+                     SkScalarFloor(SkIntToScalar(bounds.x()) +
+                                   SkIntToScalar(bounds.width() / 2.0) -
                                    SkIntToScalar(badge_width / 2.0)));
     rect.fRight = rect.fLeft + SkIntToScalar(badge_width);
   } else {