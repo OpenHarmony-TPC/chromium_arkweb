OpenHarmony-TPC/chromium_arkweb

commit 4cd443ae9dc48e5a76cf0fbfc5426bb1096712cb

Show the location bar icon (almost) all the time, and have its contents match what the user is doing.
There are a couple major moving parts here:

* Change AutocompletePopupModel::URLsForCurrentText() to InfoForCurrentText() and have it return an AutocompleteMatch, which callers can use to parse out whatever they want.  I needed to get at the match type for the current text and found the proliferation of arguments here ridiculous.  This had major ripple effects throughout the codebase, including changing the name and location of SearchVersusNavigateClassifier as it no longer had an "is_search" parameter directly, so the name became misleading and too narrow.  I also ended up adding a null constructor for AutocompleteMatch because it was too cumbersome otherwise.

* Change the name of the "SecurityImageView" (or similar) to reflect its broader purpose, and plumb it to the edit to get an icon instead of to the toolbar model.

* Add an AutocompleteMatch::Type to icon mapping function, and use it not only in the new code but also to simplify showing the popup contents.

BUG=27570,39725
TEST=An icon should appear next to the address at all times.  It should be a globe on non-secure pages, a magnifying glass on the NTP, and a match for whatever the user is typing as he types.
Review URL: http://codereview.chromium.org/1457002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43025 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/autocomplete/autocomplete_popup_view_gtk.cc
@@ -165,47 +165,21 @@ void SetupLayoutForMatch(PangoLayout* layout,
 }
 
 GdkPixbuf* IconForMatch(const AutocompleteMatch& match, bool selected) {
+  int icon = match.starred ?
+      IDR_O2_STAR : AutocompleteMatch::TypeToIcon(match.type);
+  if (selected) {
+    switch (icon) {
+      case IDR_O2_GLOBE:   icon = IDR_O2_GLOBE_SELECTED; break;
+      case IDR_O2_HISTORY: icon = IDR_O2_HISTORY_SELECTED; break;
+      case IDR_O2_SEARCH:  icon = IDR_O2_SEARCH_SELECTED; break;
+      case IDR_O2_MORE:    icon = IDR_O2_MORE_SELECTED; break;
+      case IDR_O2_STAR:    icon = IDR_O2_STAR_SELECTED; break;
+      default:             NOTREACHED(); break;
+    }
+  }
   // TODO(deanm): These would be better as pixmaps someday.
   // TODO(estade): Do we want to flip these for RTL?  (Windows doesn't).
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-  static GdkPixbuf* o2_globe = rb.GetPixbufNamed(IDR_O2_GLOBE);
-  static GdkPixbuf* o2_globe_s = rb.GetPixbufNamed(IDR_O2_GLOBE_SELECTED_DARK);
-  static GdkPixbuf* o2_history = rb.GetPixbufNamed(IDR_O2_HISTORY);
-  static GdkPixbuf* o2_history_s =
-      rb.GetPixbufNamed(IDR_O2_HISTORY_SELECTED_DARK);
-  static GdkPixbuf* o2_more = rb.GetPixbufNamed(IDR_O2_MORE);
-  static GdkPixbuf* o2_more_s = rb.GetPixbufNamed(IDR_O2_MORE_SELECTED_DARK);
-  static GdkPixbuf* o2_search = rb.GetPixbufNamed(IDR_O2_SEARCH);
-  static GdkPixbuf* o2_search_s =
-      rb.GetPixbufNamed(IDR_O2_SEARCH_SELECTED_DARK);
-  static GdkPixbuf* o2_star = rb.GetPixbufNamed(IDR_O2_STAR);
-  static GdkPixbuf* o2_star_s = rb.GetPixbufNamed(IDR_O2_STAR_SELECTED_DARK);
-
-  if (match.starred)
-    return selected ? o2_star_s : o2_star;
-
-  switch (match.type) {
-    case AutocompleteMatch::URL_WHAT_YOU_TYPED:
-    case AutocompleteMatch::NAVSUGGEST:
-      return selected ? o2_globe_s : o2_globe;
-    case AutocompleteMatch::HISTORY_URL:
-    case AutocompleteMatch::HISTORY_TITLE:
-    case AutocompleteMatch::HISTORY_BODY:
-    case AutocompleteMatch::HISTORY_KEYWORD:
-      return selected ? o2_history_s : o2_history;
-    case AutocompleteMatch::SEARCH_WHAT_YOU_TYPED:
-    case AutocompleteMatch::SEARCH_HISTORY:
-    case AutocompleteMatch::SEARCH_SUGGEST:
-    case AutocompleteMatch::SEARCH_OTHER_ENGINE:
-      return selected ? o2_search_s : o2_search;
-    case AutocompleteMatch::OPEN_HISTORY_PAGE:
-      return selected ? o2_more_s : o2_more;
-    default:
-      NOTREACHED();
-      break;
-  }
-
-  return NULL;
+  return ResourceBundle::GetSharedInstance().GetPixbufNamed(icon);
 }
 
 }  // namespace

chrome/browser/autocomplete/autocomplete.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,6 +27,7 @@
 #include "googleurl/src/url_canon_ip.h"
 #include "googleurl/src/url_util.h"
 #include "grit/generated_resources.h"
+#include "grit/theme_resources.h"
 #include "net/base/net_util.h"
 #include "net/base/registry_controlled_domain.h"
 #include "net/url_request/url_request.h"
@@ -367,6 +368,18 @@ void AutocompleteInput::Clear() {
 
 // AutocompleteMatch ----------------------------------------------------------
 
+AutocompleteMatch::AutocompleteMatch()
+    : provider(NULL),
+      relevance(0),
+      deletable(false),
+      inline_autocomplete_offset(std::wstring::npos),
+      transition(PageTransition::GENERATED),
+      is_history_what_you_typed_match(false),
+      type(SEARCH_WHAT_YOU_TYPED),
+      template_url(NULL),
+      starred(false) {
+}
+
 AutocompleteMatch::AutocompleteMatch(AutocompleteProvider* provider,
                                      int relevance,
                                      bool deletable,
@@ -384,23 +397,40 @@ AutocompleteMatch::AutocompleteMatch(AutocompleteProvider* provider,
 
 // static
 std::string AutocompleteMatch::TypeToString(Type type) {
-  switch (type) {
-    case URL_WHAT_YOU_TYPED:    return "url-what-you-typed";
-    case HISTORY_URL:           return "history-url";
-    case HISTORY_TITLE:         return "history-title";
-    case HISTORY_BODY:          return "history-body";
-    case HISTORY_KEYWORD:       return "history-keyword";
-    case NAVSUGGEST:            return "navsuggest";
-    case SEARCH_WHAT_YOU_TYPED: return "search-what-you-typed";
-    case SEARCH_HISTORY:        return "search-history";
-    case SEARCH_SUGGEST:        return "search-suggest";
-    case SEARCH_OTHER_ENGINE:   return "search-other-engine";
-    case OPEN_HISTORY_PAGE:     return "open-history-page";
+  const char* strings[NUM_TYPES] = {
+    "url-what-you-typed",
+    "history-url",
+    "history-title",
+    "history-body",
+    "history-keyword",
+    "navsuggest",
+    "search-what-you-typed",
+    "search-history",
+    "search-suggest",
+    "search-other-engine",
+    "open-history-page",
+  };
+  DCHECK(arraysize(strings) == NUM_TYPES);
+  return strings[type];
+}
 
-    default:
-      NOTREACHED();
-      return std::string();
-  }
+// static
+int AutocompleteMatch::TypeToIcon(Type type) {
+  int icons[NUM_TYPES] = {
+    IDR_O2_GLOBE,
+    IDR_O2_GLOBE,
+    IDR_O2_HISTORY,
+    IDR_O2_HISTORY,
+    IDR_O2_HISTORY,
+    IDR_O2_GLOBE,
+    IDR_O2_SEARCH,
+    IDR_O2_SEARCH,
+    IDR_O2_SEARCH,
+    IDR_O2_SEARCH,
+    IDR_O2_MORE,
+  };
+  DCHECK(arraysize(icons) == NUM_TYPES);
+  return icons[type];
 }
 
 // static

chrome/browser/autocomplete/autocomplete.h
@@ -314,22 +314,24 @@ struct AutocompleteMatch {
 
   // The type of this match.
   enum Type {
-    URL_WHAT_YOU_TYPED,     // The input as a URL.
-    HISTORY_URL,            // A past page whose URL contains the input.
-    HISTORY_TITLE,          // A past page whose title contains the input.
-    HISTORY_BODY,           // A past page whose body contains the input.
-    HISTORY_KEYWORD,        // A past page whose keyword contains the input.
-    NAVSUGGEST,             // A suggested URL.
-    SEARCH_WHAT_YOU_TYPED,  // The input as a search query (with the default
-                            // engine).
-    SEARCH_HISTORY,         // A past search (with the default engine)
-                            // containing the input.
-    SEARCH_SUGGEST,         // A suggested search (with the default engine).
-    SEARCH_OTHER_ENGINE,    // A search with a non-default engine.
-    OPEN_HISTORY_PAGE,      // A synthetic result that opens the history page to
-                            // search for the input.
+    URL_WHAT_YOU_TYPED = 0,  // The input as a URL.
+    HISTORY_URL,             // A past page whose URL contains the input.
+    HISTORY_TITLE,           // A past page whose title contains the input.
+    HISTORY_BODY,            // A past page whose body contains the input.
+    HISTORY_KEYWORD,         // A past page whose keyword contains the input.
+    NAVSUGGEST,              // A suggested URL.
+    SEARCH_WHAT_YOU_TYPED,   // The input as a search query (with the default
+                             // engine).
+    SEARCH_HISTORY,          // A past search (with the default engine)
+                             // containing the input.
+    SEARCH_SUGGEST,          // A suggested search (with the default engine).
+    SEARCH_OTHER_ENGINE,     // A search with a non-default engine.
+    OPEN_HISTORY_PAGE,       // A synthetic result that opens the history page
+                             // to search for the input.
+    NUM_TYPES,
   };
 
+  AutocompleteMatch();
   AutocompleteMatch(AutocompleteProvider* provider,
                     int relevance,
                     bool deletable,
@@ -337,6 +339,10 @@ struct AutocompleteMatch {
 
   // Converts |type| to a string representation.  Used in logging.
   static std::string TypeToString(Type type);
+
+  // Converts |type| to a resource identifier for the appropriate icon for this
+  // type.
+  static int TypeToIcon(Type type);
 
   // Comparison function for determining when one match is better than another.
   static bool MoreRelevant(const AutocompleteMatch& elem1,
@@ -776,7 +782,7 @@ class AutocompleteController : public ACProviderListener {
   const AutocompleteInput& input() const { return input_; }
   const AutocompleteResult& result() const { return result_; }
   // This next is temporary and should go away when
-  // AutocompletePopup::URLsForCurrentSelection() moves to the controller.
+  // AutocompletePopup::InfoForCurrentSelection() moves to the controller.
   const AutocompleteResult& latest_result() const { return latest_result_; }
   bool done() const { return done_ && !update_delay_timer_.IsRunning(); }
 

chrome/browser/autocomplete/autocomplete_classifier.cc
@@ -1,0 +1,34 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autocomplete/autocomplete_classifier.h"
+
+#include "chrome/browser/autocomplete/autocomplete.h"
+#include "googleurl/src/gurl.h"
+
+AutocompleteClassifier::AutocompleteClassifier(Profile* profile)
+    : controller_(new AutocompleteController(profile)) {
+}
+
+AutocompleteClassifier::~AutocompleteClassifier() {
+}
+
+void AutocompleteClassifier::Classify(const std::wstring& text,
+                                      const std::wstring& desired_tld,
+                                      AutocompleteMatch* match,
+                                      GURL* alternate_nav_url) {
+  controller_->Start(text, desired_tld, true, false, true);
+  DCHECK(controller_->done());
+  const AutocompleteResult& result = controller_->result();
+  if (result.empty()) {
+    if (alternate_nav_url)
+      *alternate_nav_url = GURL();
+    return;
+  }
+
+  DCHECK(result.default_match() != result.end());
+  *match = *result.default_match();
+  if (alternate_nav_url)
+    *alternate_nav_url = result.alternate_nav_url();
+}

chrome/browser/autocomplete/autocomplete_classifier.h
@@ -1,0 +1,43 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_CLASSIFIER_H_
+#define CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_CLASSIFIER_H_
+
+#include <string>
+
+#include "base/basictypes.h"
+#include "base/scoped_ptr.h"
+
+class AutocompleteController;
+struct AutocompleteMatch;
+class GURL;
+class Profile;
+
+class AutocompleteClassifier {
+ public:
+  explicit AutocompleteClassifier(Profile* profile);
+  virtual ~AutocompleteClassifier();
+
+  // Given some string |text| that the user wants to use for navigation,
+  // determines how it should be interpreted.  |desired_tld| is the user's
+  // desired TLD, if any; see AutocompleteInput::desired_tld().  |match| should
+  // be a non-NULL outparam that will be set to the default match for this
+  // input, if any (for invalid input, there will be no default match, and
+  // |match| will be left unchanged).  |alternate_nav_url| is a possibly-NULL
+  // outparam that, if non-NULL, will be set to the navigational URL (if any) in
+  // case of an accidental search; see comments on
+  // AutocompleteResult::alternate_nav_url_ in autocomplete.h.
+  void Classify(const std::wstring& text,
+                const std::wstring& desired_tld,
+                AutocompleteMatch* match,
+                GURL* alternate_nav_url);
+
+ private:
+  scoped_ptr<AutocompleteController> controller_;
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(AutocompleteClassifier);
+};
+
+#endif  // CHROME_BROWSER_AUTOCOMPLETE_AUTOCOMPLETE_CLASSIFIER_H_

chrome/browser/autocomplete/autocomplete_edit.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@
 #include "base/basictypes.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/app/chrome_dll_resource.h"
+#include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autocomplete/autocomplete_edit_view.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_model.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
@@ -19,7 +20,6 @@
 #include "chrome/browser/profile.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_model.h"
-#include "chrome/browser/search_versus_navigate_classifier.h"
 #include "chrome/common/notification_service.h"
 #include "googleurl/src/gurl.h"
 #include "googleurl/src/url_util.h"
@@ -122,7 +122,9 @@ void AutocompleteEditModel::SetUserText(const std::wstring& text) {
 void AutocompleteEditModel::GetDataForURLExport(GURL* url,
                                                 std::wstring* title,
                                                 SkBitmap* favicon) {
-  *url = GetURLForCurrentText(NULL, NULL, NULL);
+  AutocompleteMatch match;
+  GetInfoForCurrentText(&match, NULL);
+  *url = match.destination_url;
   if (UTF8ToWide(url->possibly_invalid_spec()) == permanent_text_) {
     *title = controller_->GetTitle();
     *favicon = controller_->GetFavIcon();
@@ -134,7 +136,7 @@ std::wstring AutocompleteEditModel::GetDesiredTLD() const {
     std::wstring(L"com") : std::wstring();
 }
 
-bool AutocompleteEditModel::CurrentTextIsURL() {
+bool AutocompleteEditModel::CurrentTextIsURL() const {
   // If !user_input_in_progress_, the permanent text is showing, which should
   // always be a URL, so no further checking is needed.  By avoiding checking in
   // this case, we avoid calling into the autocomplete providers, and thus
@@ -142,9 +144,15 @@ bool AutocompleteEditModel::CurrentTextIsURL() {
   if (!user_input_in_progress_)
     return true;
 
-  PageTransition::Type transition = PageTransition::LINK;
-  GetURLForCurrentText(&transition, NULL, NULL);
-  return transition == PageTransition::TYPED;
+  AutocompleteMatch match;
+  GetInfoForCurrentText(&match, NULL);
+  return match.transition == PageTransition::TYPED;
+}
+
+AutocompleteMatch::Type AutocompleteEditModel::CurrentTextType() const {
+  AutocompleteMatch match;
+  GetInfoForCurrentText(&match, NULL);
+  return match.type;
 }
 
 bool AutocompleteEditModel::GetURLForText(const std::wstring& text,
@@ -191,14 +199,11 @@ bool AutocompleteEditModel::CanPasteAndGo(const std::wstring& text) const {
   if (!view_->GetCommandUpdater()->IsCommandEnabled(IDC_OPEN_CURRENT_URL))
     return false;
 
-  paste_and_go_url_ = GURL();
-  paste_and_go_transition_ = PageTransition::TYPED;
-  paste_and_go_alternate_nav_url_ = GURL();
-
-  profile_->GetSearchVersusNavigateClassifier()->Classify(text, std::wstring(),
-      NULL, &paste_and_go_url_, &paste_and_go_transition_, NULL,
-      &paste_and_go_alternate_nav_url_);
-
+  AutocompleteMatch match;
+  profile_->GetAutocompleteClassifier()->Classify(text, std::wstring(),
+      &match, &paste_and_go_alternate_nav_url_);
+  paste_and_go_url_ = match.destination_url;
+  paste_and_go_transition_ = match.transition;
   return paste_and_go_url_.is_valid();
 }
 
@@ -215,33 +220,30 @@ void AutocompleteEditModel::PasteAndGo() {
 void AutocompleteEditModel::AcceptInput(WindowOpenDisposition disposition,
                                         bool for_drop) {
   // Get the URL and transition type for the selected entry.
-  PageTransition::Type transition;
-  bool is_history_what_you_typed_match;
+  AutocompleteMatch match;
   GURL alternate_nav_url;
-  const GURL url(GetURLForCurrentText(&transition,
-                                      &is_history_what_you_typed_match,
-                                      &alternate_nav_url));
-  if (!url.is_valid())
+  GetInfoForCurrentText(&match, &alternate_nav_url);
+  if (!match.destination_url.is_valid())
     return;
 
-  if (UTF8ToWide(url.spec()) == permanent_text_) {
+  if (UTF8ToWide(match.destination_url.spec()) == permanent_text_) {
     // When the user hit enter on the existing permanent URL, treat it like a
     // reload for scoring purposes.  We could detect this by just checking
     // user_input_in_progress_, but it seems better to treat "edits" that end
     // up leaving the URL unchanged (e.g. deleting the last character and then
     // retyping it) as reloads too.
-    transition = PageTransition::RELOAD;
+    match.transition = PageTransition::RELOAD;
   } else if (for_drop || ((paste_state_ != NONE) &&
-                          is_history_what_you_typed_match)) {
+                          match.is_history_what_you_typed_match)) {
     // When the user pasted in a URL and hit enter, score it like a link click
     // rather than a normal typed URL, so it doesn't get inline autocompleted
     // as aggressively later.
-    transition = PageTransition::LINK;
+    match.transition = PageTransition::LINK;
   }
 
-  view_->OpenURL(url, disposition, transition, alternate_nav_url,
-      AutocompletePopupModel::kNoMatch,
-      is_keyword_hint_ ? std::wstring() : keyword_);
+  view_->OpenURL(match.destination_url, disposition, match.transition,
+                 alternate_nav_url, AutocompletePopupModel::kNoMatch,
+                 is_keyword_hint_ ? std::wstring() : keyword_);
 }
 
 void AutocompleteEditModel::SendOpenNotification(size_t selected_line,
@@ -325,17 +327,20 @@ void AutocompleteEditModel::OnKillFocus() {
 }
 
 bool AutocompleteEditModel::OnEscapeKeyPressed() {
-  if (has_temporary_text_ &&
-      (popup_->URLsForCurrentSelection(NULL, NULL, NULL) != original_url_)) {
-    // The user typed something, then selected a different item.  Restore the
-    // text they typed and change back to the default item.
-    // NOTE: This purposefully does not reset paste_state_.
-    just_deleted_text_ = false;
-    has_temporary_text_ = false;
-    keyword_ui_state_ = original_keyword_ui_state_;
-    popup_->ResetToDefaultMatch();
-    view_->OnRevertTemporaryText();
-    return true;
+  if (has_temporary_text_) {
+    AutocompleteMatch match;
+    popup_->InfoForCurrentSelection(&match, NULL);
+    if (match.destination_url != original_url_) {
+      // The user typed something, then selected a different item.  Restore the
+      // text they typed and change back to the default item.
+      // NOTE: This purposefully does not reset paste_state_.
+      just_deleted_text_ = false;
+      has_temporary_text_ = false;
+      keyword_ui_state_ = original_keyword_ui_state_;
+      popup_->ResetToDefaultMatch();
+      view_->OnRevertTemporaryText();
+      return true;
+    }
   }
 
   // If the user wasn't editing, but merely had focus in the edit, allow <esc>
@@ -405,7 +410,7 @@ void AutocompleteEditModel::OnUpOrDownKeyPressed(int count) {
 
 void AutocompleteEditModel::OnPopupDataChanged(
     const std::wstring& text,
-    bool is_temporary_text,
+    GURL* destination_for_temporary_text_change,
     const std::wstring& keyword,
     bool is_keyword_hint,
     AutocompleteMatch::Type type) {
@@ -428,12 +433,12 @@ void AutocompleteEditModel::OnPopupDataChanged(
   }
 
   // Handle changes to temporary text.
-  if (is_temporary_text) {
+  if (destination_for_temporary_text_change != NULL) {
     const bool save_original_selection = !has_temporary_text_;
     if (save_original_selection) {
       // Save the original selection and URL so it can be reverted later.
       has_temporary_text_ = true;
-      original_url_ = popup_->URLsForCurrentSelection(NULL, NULL, NULL);
+      original_url_ = *destination_for_temporary_text_change;
       original_keyword_ui_state_ = keyword_ui_state_;
     }
     if (control_key_state_ == DOWN_WITHOUT_CHANGE) {
@@ -562,7 +567,7 @@ void AutocompleteEditModel::Observe(NotificationType type,
     match_type = match->type;
   }
 
-  OnPopupDataChanged(inline_autocomplete_text, false, keyword, is_keyword_hint,
+  OnPopupDataChanged(inline_autocomplete_text, NULL, keyword, is_keyword_hint,
                      match_type);
 }
 
@@ -586,20 +591,14 @@ std::wstring AutocompleteEditModel::UserTextFromDisplayText(
       text : (keyword_ + L" " + text);
 }
 
-GURL AutocompleteEditModel::GetURLForCurrentText(
-    PageTransition::Type* transition,
-    bool* is_history_what_you_typed_match,
+void AutocompleteEditModel::GetInfoForCurrentText(
+    AutocompleteMatch* match,
     GURL* alternate_nav_url) const {
   if (popup_->IsOpen() || query_in_progress()) {
-    return popup_->URLsForCurrentSelection(transition,
-                                           is_history_what_you_typed_match,
-                                           alternate_nav_url);
+    popup_->InfoForCurrentSelection(match, alternate_nav_url);
+  } else {
+    profile_->GetAutocompleteClassifier()->Classify(
+        UserTextFromDisplayText(view_->GetText()), GetDesiredTLD(), match,
+        alternate_nav_url);
   }
-
-  GURL destination_url;
-  profile_->GetSearchVersusNavigateClassifier()->Classify(
-      UserTextFromDisplayText(view_->GetText()), GetDesiredTLD(), NULL,
-      &destination_url, transition, is_history_what_you_typed_match,
-      alternate_nav_url);
-  return destination_url;
 }

chrome/browser/autocomplete/autocomplete_edit.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,7 +137,10 @@ class AutocompleteEditModel : public NotificationObserver {
 
   // Returns true if the current edit contents will be treated as a
   // URL/navigation, as opposed to a search.
-  bool CurrentTextIsURL();
+  bool CurrentTextIsURL() const;
+
+  // Returns the match type for the current edit contents.
+  AutocompleteMatch::Type CurrentTextType() const;
 
   // Returns true if |text| (which is display text in the current context)
   // parses as a URL, and in that case sets |url| to the calculated URL.
@@ -208,7 +211,7 @@ class AutocompleteEditModel : public NotificationObserver {
   // Accessors for keyword-related state (see comments on keyword_ and
   // is_keyword_hint_).
   std::wstring keyword() const {
-    return (is_keyword_hint_ ? has_focus_ : (keyword_ui_state_ != NO_KEYWORD)) ?
+    return (is_keyword_hint_ || (keyword_ui_state_ != NO_KEYWORD)) ?
         keyword_ : std::wstring();
   }
   bool is_keyword_hint() const { return is_keyword_hint_; }
@@ -222,7 +225,7 @@ class AutocompleteEditModel : public NotificationObserver {
 
   // True if we should show the "Type to search" hint (see comments on
   // show_search_hint_).
-  bool show_search_hint() const { return has_focus_ && show_search_hint_; }
+  bool show_search_hint() const { return show_search_hint_; }
 
   // Returns true if a query to an autocomplete provider is currently
   // in progress.  This logic should in the future live in
@@ -260,9 +263,12 @@ class AutocompleteEditModel : public NotificationObserver {
   // Called when any relevant data changes.  This rolls together several
   // separate pieces of data into one call so we can update all the UI
   // efficiently:
-  //   |text| is either the new temporary text (if |is_temporary_text| is true)
-  //     from the user manually selecting a different match, or the inline
-  //     autocomplete text (if |is_temporary_text| is false).
+  //   |text| is either the new temporary text from the user manually selecting
+  //     a different match, or the inline autocomplete text.  We distinguish by
+  //     checking if |destination_for_temporary_text_change| is NULL.
+  //   |destination_for_temporary_text_change| is NULL (if temporary text should
+  //     not change) or the pre-change desitnation URL (if temporary text should
+  //     change) so we can save it off to restore later.
   //   |keyword| is the keyword to show a hint for if |is_keyword_hint| is true,
   //     or the currently selected keyword if |is_keyword_hint| is false (see
   //     comments on keyword_ and is_keyword_hint_).
@@ -271,7 +277,7 @@ class AutocompleteEditModel : public NotificationObserver {
   //     show_search_hint_).
   void OnPopupDataChanged(
       const std::wstring& text,
-      bool is_temporary_text,
+      GURL* destination_for_temporary_text_change,
       const std::wstring& keyword,
       bool is_keyword_hint,
       AutocompleteMatch::Type type);
@@ -326,16 +332,10 @@ class AutocompleteEditModel : public NotificationObserver {
   std::wstring DisplayTextFromUserText(const std::wstring& text) const;
   std::wstring UserTextFromDisplayText(const std::wstring& text) const;
 
-  // Returns the URL. If the user has not edited the text, this returns the
-  // permanent text. If the user has edited the text, this returns the default
-  // match based on the current text, which may be a search URL, or keyword
-  // generated URL.
-  //
-  // See AutocompleteEdit for a description of the args (they may be null if
-  // not needed).
-  GURL GetURLForCurrentText(PageTransition::Type* transition,
-                            bool* is_history_what_you_typed_match,
-                            GURL* alternate_nav_url) const;
+  // Returns the default match for the current text, as well as the alternate
+  // nav URL, if |alternate_nav_url| is non-NULL and there is such a URL.
+  void GetInfoForCurrentText(AutocompleteMatch* match,
+                             GURL* alternate_nav_url) const;
 
   AutocompleteEditView* view_;
 

chrome/browser/autocomplete/autocomplete_edit_view.h
@@ -59,6 +59,9 @@ class AutocompleteEditView {
   // browser, or just whatever the user has currently typed.
   virtual std::wstring GetText() const = 0;
 
+  // Returns the resource ID of the icon to show for the current text.
+  virtual int GetIcon() const = 0;
+
   // The user text is the text the user has manually keyed in.  When present,
   // this is shown in preference to the permanent text; hitting escape will
   // revert to the permanent text.

commit 1c192a4e82029dc064d96a14fbbcea1193663672

gtk: fix display of icons in omnibox popup
This regressed in r43025.  I don't know this code; I just made it behave
like it did before that change.

BUG=40056
TEST=highlight icons in the omnibox drop-down; make sure you can see them

Review URL: http://codereview.chromium.org/1584007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43290 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/autocomplete/autocomplete_popup_view_gtk.cc
@@ -168,11 +168,11 @@ GdkPixbuf* IconForMatch(const AutocompleteMatch& match, bool selected) {
       IDR_O2_STAR : AutocompleteMatch::TypeToIcon(match.type);
   if (selected) {
     switch (icon) {
-      case IDR_O2_GLOBE:   icon = IDR_O2_GLOBE_SELECTED; break;
-      case IDR_O2_HISTORY: icon = IDR_O2_HISTORY_SELECTED; break;
-      case IDR_O2_SEARCH:  icon = IDR_O2_SEARCH_SELECTED; break;
-      case IDR_O2_MORE:    icon = IDR_O2_MORE_SELECTED; break;
-      case IDR_O2_STAR:    icon = IDR_O2_STAR_SELECTED; break;
+      case IDR_O2_GLOBE:   icon = IDR_O2_GLOBE_SELECTED_DARK; break;
+      case IDR_O2_HISTORY: icon = IDR_O2_HISTORY_SELECTED_DARK; break;
+      case IDR_O2_SEARCH:  icon = IDR_O2_SEARCH_SELECTED_DARK; break;
+      case IDR_O2_MORE:    icon = IDR_O2_MORE_SELECTED_DARK; break;
+      case IDR_O2_STAR:    icon = IDR_O2_STAR_SELECTED_DARK; break;
       default:             NOTREACHED(); break;
     }
   }