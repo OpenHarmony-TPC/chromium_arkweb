OpenHarmony-TPC/chromium_arkweb

commit 07e9b7fe5027315e91da03c859b68416812097ca

Refactor AppModalDialog to more clearly isolate platform specific UI pieces and eliminate the dependency on individual frontends from cross platform code.
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/3398015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60785 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/js_modal_dialog_cocoa.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/js_modal_dialog.h"
+#include "chrome/browser/cocoa/js_modal_dialog_cocoa.h"
 
 #import <Cocoa/Cocoa.h>
 
@@ -12,6 +12,7 @@
 #include "base/logging.h"
 #include "base/sys_string_conversions.h"
 #import "chrome/browser/chrome_browser_application_mac.h"
+#include "chrome/browser/js_modal_dialog.h"
 #include "grit/app_strings.h"
 #include "grit/generated_resources.h"
 
@@ -51,13 +52,12 @@
   [super dealloc];
 }
 
-// |contextInfo| is the bridge back to the C++ JavaScriptAppModalDialog. When
-// complete, autorelease to clean ourselves up.
+// |contextInfo| is the JSModalDialogCocoa that owns us.
 - (void)alertDidEnd:(NSAlert*)alert
          returnCode:(int)returnCode
         contextInfo:(void*)contextInfo {
-  JavaScriptAppModalDialog* bridge =
-      reinterpret_cast<JavaScriptAppModalDialog*>(contextInfo);
+  scoped_ptr<JSModalDialogCocoa> native_dialog(
+      reinterpret_cast<JSModalDialogCocoa*>(contextInfo));
   std::wstring input;
   if (textField_)
     input = base::SysNSStringToWide([textField_ stringValue]);
@@ -66,35 +66,38 @@
       bool shouldSuppress = false;
       if ([alert showsSuppressionButton])
         shouldSuppress = [[alert suppressionButton] state] == NSOnState;
-      bridge->OnAccept(input, shouldSuppress);
+      native_dialog->dialog()->OnAccept(input, shouldSuppress);
       break;
     }
     case NSAlertSecondButtonReturn:  {  // Cancel
       // If the user wants to stay on this page, stop quitting (if a quit is in
       // progress).
-      if (bridge->is_before_unload_dialog())
+      if (native_dialog->dialog()->is_before_unload_dialog())
         chrome_browser_application_mac::CancelTerminate();
 
-      bridge->OnCancel();
+      native_dialog->dialog()->OnCancel();
       break;
     }
     case NSRunStoppedResponse: {  // Window was closed underneath us
       // Need to call OnCancel() because there is some cleanup that needs
       // to be done.  It won't call back to the javascript since the
       // JavaScriptAppModalDialog knows that the TabContents was destroyed.
-      bridge->OnCancel();
+      native_dialog->dialog()->OnCancel();
       break;
     }
     default:  {
       NOTREACHED();
     }
   }
-  [self autorelease];
-  delete bridge;  // Done with the dialog, it needs be destroyed.
 }
 @end
 
-void JavaScriptAppModalDialog::CreateAndShowDialog() {
+////////////////////////////////////////////////////////////////////////////////
+// JSModalDialogCocoa, public:
+
+JSModalDialogCocoa::JSModalDialogCocoa(JavaScriptAppModalDialog* dialog)
+    : dialog_(dialog),
+      helper_(NULL) {
   // Determine the names of the dialog buttons based on the flags. "Default"
   // is the OK button. "Other" is the cancel button. We don't use the
   // "Alternate" button in NSRunAlertPanel.
@@ -102,12 +105,12 @@ void JavaScriptAppModalDialog::CreateAndShowDialog() {
   NSString* other_button = l10n_util::GetNSStringWithFixup(IDS_APP_CANCEL);
   bool text_field = false;
   bool one_button = false;
-  switch (dialog_flags_) {
+  switch (dialog_->dialog_flags()) {
     case MessageBoxFlags::kIsJavascriptAlert:
       one_button = true;
       break;
     case MessageBoxFlags::kIsJavascriptConfirm:
-      if (is_before_unload_dialog_) {
+      if (dialog_->is_before_unload_dialog()) {
         default_button = l10n_util::GetNSStringWithFixup(
             IDS_BEFOREUNLOAD_MESSAGEBOX_OK_BUTTON_LABEL);
         other_button = l10n_util::GetNSStringWithFixup(
@@ -125,46 +128,42 @@ void JavaScriptAppModalDialog::CreateAndShowDialog() {
   // Create a helper which will receive the sheet ended selector. It will
   // delete itself when done. It doesn't need anything passed to its init
   // as it will get a contextInfo parameter.
-  JavaScriptAppModalDialogHelper* helper =
-      [[JavaScriptAppModalDialogHelper alloc] init];
+  helper_.reset([[JavaScriptAppModalDialogHelper alloc] init]);
 
   // Show the modal dialog.
-  NSAlert* alert = [helper alert];
-  dialog_ = alert;
+  alert_ = [helper_ alert];
   NSTextField* field = nil;
   if (text_field) {
-    field = [helper textField];
-    [field setStringValue:base::SysWideToNSString(default_prompt_text_)];
+    field = [helper_ textField];
+    [field setStringValue:base::SysWideToNSString(
+        dialog_->default_prompt_text())];
   }
-  [alert setDelegate:helper];
-  [alert setInformativeText:base::SysWideToNSString(message_text_)];
-  [alert setMessageText:base::SysWideToNSString(title_)];
-  [alert addButtonWithTitle:default_button];
+  [alert_ setDelegate:helper_];
+  [alert_ setInformativeText:base::SysWideToNSString(dialog_->message_text())];
+  [alert_ setMessageText:base::SysWideToNSString(dialog_->title())];
+  [alert_ addButtonWithTitle:default_button];
   if (!one_button) {
-    NSButton* other = [alert addButtonWithTitle:other_button];
+    NSButton* other = [alert_ addButtonWithTitle:other_button];
     [other setKeyEquivalent:@"\e"];
   }
-  if (display_suppress_checkbox_) {
-    [alert setShowsSuppressionButton:YES];
+  if (dialog_->display_suppress_checkbox()) {
+    [alert_ setShowsSuppressionButton:YES];
     NSString* suppression_title = l10n_util::GetNSStringWithFixup(
         IDS_JAVASCRIPT_MESSAGEBOX_SUPPRESS_OPTION);
-    [[alert suppressionButton] setTitle:suppression_title];
+    [[alert_ suppressionButton] setTitle:suppression_title];
   }
-
-  [alert beginSheetModalForWindow:nil  // nil here makes it app-modal
-                    modalDelegate:helper
-                   didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:)
-                      contextInfo:this];
-
-  if (field)
-    [[alert window] makeFirstResponder:field];
 }
 
-// The functions below are used by the automation framework.
-int JavaScriptAppModalDialog::GetDialogButtons() {
+JSModalDialogCocoa::~JSModalDialogCocoa() {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// JSModalDialogCocoa, NativeAppModalDialog implementation:
+
+int JSModalDialogCocoa::GetAppModalDialogButtons() const {
   // From the above, it is the case that if there is 1 button, it is always the
   // OK button.  The second button, if it exists, is always the Cancel button.
-  int num_buttons = [[dialog_ buttons] count];
+  int num_buttons = [[alert_ buttons] count];
   switch (num_buttons) {
     case 1:
       return MessageBoxFlags::DIALOGBUTTON_OK;
@@ -177,27 +176,43 @@ int JavaScriptAppModalDialog::GetDialogButtons() {
   }
 }
 
-// On Mac, this is only used in testing.
-void JavaScriptAppModalDialog::AcceptWindow() {
-  NSButton* first = [[dialog_ buttons] objectAtIndex:0];
+void JSModalDialogCocoa::ShowAppModalDialog() {
+  [alert_
+      beginSheetModalForWindow:nil  // nil here makes it app-modal
+                 modalDelegate:helper_.get()
+                didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:)
+                   contextInfo:this];
+
+  if ([helper_ textField])
+    [[alert_ window] makeFirstResponder:[helper_ textField]];
+}
+
+void JSModalDialogCocoa::ActivateAppModalDialog() {
+}
+
+void JSModalDialogCocoa::CloseAppModalDialog() {
+  DCHECK([alert_ isKindOfClass:[NSAlert class]]);
+  [NSApp endSheet:[alert_ window]];
+  alert_ = NULL;
+}
+
+void JSModalDialogCocoa::AcceptAppModalDialog() {
+  NSButton* first = [[alert_ buttons] objectAtIndex:0];
   [first performClick:nil];
 }
 
-void JavaScriptAppModalDialog::CancelWindow() {
-  DCHECK([[dialog_ buttons] count] >= 2);
-  NSButton* second = [[dialog_ buttons] objectAtIndex:1];
+void JSModalDialogCocoa::CancelAppModalDialog() {
+  DCHECK([[alert_ buttons] count] >= 2);
+  NSButton* second = [[alert_ buttons] objectAtIndex:1];
   [second performClick:nil];
 }
 
-// This is only used by the app-modal dialog machinery on windows.
-NativeDialog JavaScriptAppModalDialog::CreateNativeDialog() {
-  NOTIMPLEMENTED();
-  return nil;
-}
+////////////////////////////////////////////////////////////////////////////////
+// NativeAppModalDialog, public:
 
-void JavaScriptAppModalDialog::CloseModalDialog() {
-  NSAlert* alert = dialog_;
-  DCHECK([alert isKindOfClass:[NSAlert class]]);
-  [NSApp endSheet:[alert window]];
-  dialog_ = nil;
+// static
+NativeAppModalDialog* NativeAppModalDialog::CreateNativeJavaScriptPrompt(
+    JavaScriptAppModalDialog* dialog,
+    gfx::NativeWindow parent_window) {
+  return new JSModalDialogCocoa(dialog);
 }

chrome/browser/app_modal_dialog.cc
@@ -5,20 +5,20 @@
 #include "chrome/browser/app_modal_dialog.h"
 
 #include "chrome/browser/app_modal_dialog_queue.h"
+#include "chrome/browser/native_app_modal_dialog.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/notification_type.h"
 
 AppModalDialog::AppModalDialog(TabContents* tab_contents,
                                const std::wstring& title)
-#if defined(OS_WIN) || defined(OS_LINUX)
-    : dialog_(NULL),
-#elif defined(OS_MACOSX)
-    :
-#endif
+    : skip_this_dialog_(false),
       tab_contents_(tab_contents),
-      title_(title),
-      skip_this_dialog_(false) {
+      native_dialog_(NULL),
+      title_(title) {
+}
+
+AppModalDialog::~AppModalDialog() {
 }
 
 void AppModalDialog::ShowModalDialog() {
@@ -33,11 +33,19 @@ void AppModalDialog::ShowModalDialog() {
       NotificationService::NoDetails());
 }
 
-void AppModalDialog::Cleanup() {
-  NotificationService::current()->Notify(
-      NotificationType::APP_MODAL_DIALOG_CLOSED,
-      Source<AppModalDialog>(this),
-      NotificationService::NoDetails());
+void AppModalDialog::CreateAndShowDialog() {
+  native_dialog_ = CreateNativeDialog();
+  native_dialog_->ShowAppModalDialog();
+}
+
+void AppModalDialog::ActivateModalDialog() {
+  DCHECK(native_dialog_);
+  native_dialog_->ActivateAppModalDialog();
+}
+
+void AppModalDialog::CloseModalDialog() {
+  DCHECK(native_dialog_);
+  native_dialog_->CloseAppModalDialog();
 }
 
 void AppModalDialog::CompleteDialog() {

chrome/browser/app_modal_dialog.h
@@ -13,19 +13,7 @@
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
 
-// Define NativeDialog type as platform specific dialog view.
-#if defined(OS_WIN)
-class ModalDialogDelegate;
-typedef ModalDialogDelegate* NativeDialog;
-#elif defined(OS_MACOSX)
-typedef void* NativeDialog;
-#elif defined(TOOLKIT_USES_GTK)
-typedef struct _GtkDialog GtkDialog;
-typedef struct _GtkWidget GtkWidget;
-typedef int gint;
-typedef GtkWidget* NativeDialog;
-#endif
-
+class NativeAppModalDialog;
 class TabContents;
 
 // A controller+model base class for modal dialogs.
@@ -37,8 +25,32 @@ class AppModalDialog {
   AppModalDialog(TabContents* tab_contents, const std::wstring& title);
   virtual ~AppModalDialog();
 
-  // Called by the app modal window queue when it is time to show this window.
+  // Called by the AppModalDialogQueue to show this dialog.
   void ShowModalDialog();
+
+  // Called by the AppModalDialogQueue to activate the dialog.
+  void ActivateModalDialog();
+
+  // Closes the dialog if it is showing.
+  void CloseModalDialog();
+
+  // Completes dialog handling, shows next modal dialog from the queue.
+  // TODO(beng): Get rid of this method.
+  void CompleteDialog();
+
+  // Dialog window title.
+  std::wstring title() const { return title_; }
+
+  NativeAppModalDialog* native_dialog() const { return native_dialog_; }
+
+  // Methods overridable by AppModalDialog subclasses:
+
+  // Creates an implementation of NativeAppModalDialog and shows it.
+  // When the native dialog is closed, the implementation of
+  // NativeAppModalDialog should call OnAccept or OnCancel to notify the
+  // renderer of the user's action. The NativeAppModalDialog is also
+  // expected to delete the AppModalDialog associated with it.
+  virtual void CreateAndShowDialog();
 
   // Returns true if the dialog is still valid. As dialogs are created they are
   // added to the AppModalDialogQueue. When the current modal dialog finishes
@@ -46,65 +58,24 @@ class AppModalDialog {
   // If IsValid returns false the dialog is deleted and not shown.
   virtual bool IsValid() { return !skip_this_dialog_; }
 
-  /////////////////////////////////////////////////////////////////////////////
-  // The following methods are platform specific and should be implemented in
-  // the platform specific .cc files.
-  // Create the platform specific NativeDialog and display it.  When the
-  // NativeDialog is closed, it should call OnAccept or OnCancel to notify the
-  // renderer of the user's action.  The NativeDialog is also expected to
-  // delete the AppModalDialog associated with it.
-  virtual void CreateAndShowDialog();
-
-#if defined(TOOLKIT_USES_GTK)
-  virtual void HandleDialogResponse(GtkDialog* dialog, gint response_id) = 0;
-  // Callback for dialog response calls, passes results to specialized
-  // HandleDialogResponse() implementation.
-  static void OnDialogResponse(GtkDialog* dialog, gint response_id,
-                               AppModalDialog* app_modal_dialog);
-#endif
-
-  // Close the dialog if it is showing.
-  virtual void CloseModalDialog();
-
-  // Called by the app modal window queue to activate the window.
-  void ActivateModalDialog();
-
-  // Completes dialog handling, shows next modal dialog from the queue.
-  void CompleteDialog();
-
-  // Dialog window title.
-  std::wstring title() {
-    return title_;
-  }
-
-  // Helper methods used to query or control the dialog. This is used by
-  // automation.
-  virtual int GetDialogButtons() = 0;
-  virtual void AcceptWindow() = 0;
-  virtual void CancelWindow() = 0;
-
  protected:
-  // Cleans up the dialog class.
-  virtual void Cleanup();
-  // Creates the actual platform specific dialog view class.
-  virtual NativeDialog CreateNativeDialog() = 0;
-
-  // A reference to the platform native dialog box.
-#if defined(OS_LINUX) || defined(OS_WIN)
-  NativeDialog dialog_;
-#endif
-
-  // Parent tab contents.
-  TabContents* tab_contents_;
-
-  // Information about the message box is held in the following variables.
-  std::wstring title_;
+  // Overridden by subclasses to create the feature-specific native dialog box.
+  virtual NativeAppModalDialog* CreateNativeDialog() = 0;
 
   // True if the dialog should no longer be shown, e.g. because the underlying
   // tab navigated away while the dialog was queued.
   bool skip_this_dialog_;
 
+  // Parent tab contents.
+  TabContents* tab_contents_;
+
  private:
+  // The toolkit-specific implementation of the app modal dialog box.
+  NativeAppModalDialog* native_dialog_;
+
+  // Information about the message box is held in the following variables.
+  std::wstring title_;
+
   DISALLOW_COPY_AND_ASSIGN(AppModalDialog);
 };
 

chrome/browser/app_modal_dialog_gtk.cc
@@ -1,49 +1,0 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/app_modal_dialog.h"
-
-#include <gtk/gtk.h>
-
-#include "base/logging.h"
-#include "base/string_util.h"
-#include "chrome/browser/browser_list.h"
-#include "chrome/browser/browser_window.h"
-#include "chrome/browser/gtk/gtk_util.h"
-#include "chrome/browser/tab_contents/tab_contents.h"
-#include "chrome/browser/tab_contents/tab_contents_view.h"
-#include "grit/generated_resources.h"
-#include "grit/locale_settings.h"
-
-AppModalDialog::~AppModalDialog() {
-}
-
-void AppModalDialog::CreateAndShowDialog() {
-}
-
-// static
-void AppModalDialog::OnDialogResponse(GtkDialog* dialog, gint response_id,
-                                      AppModalDialog* app_modal_dialog) {
-  app_modal_dialog->HandleDialogResponse(dialog, response_id);
-}
-
-void AppModalDialog::ActivateModalDialog() {
-  DCHECK(dialog_);
-  gtk_window_present(GTK_WINDOW(dialog_));
-}
-
-void AppModalDialog::CloseModalDialog() {
-  DCHECK(dialog_);
-  HandleDialogResponse(GTK_DIALOG(dialog_), GTK_RESPONSE_DELETE_EVENT);
-}
-
-void AppModalDialog::AcceptWindow() {
-  DCHECK(dialog_);
-  HandleDialogResponse(GTK_DIALOG(dialog_), GTK_RESPONSE_OK);
-}
-
-void AppModalDialog::CancelWindow() {
-  DCHECK(dialog_);
-  HandleDialogResponse(GTK_DIALOG(dialog_), GTK_RESPONSE_CANCEL);
-}

chrome/browser/app_modal_dialog_mac.mm
@@ -1,24 +1,0 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/app_modal_dialog.h"
-
-#import <Cocoa/Cocoa.h>
-
-#include "base/logging.h"
-
-AppModalDialog::~AppModalDialog() {
-}
-
-void AppModalDialog::CreateAndShowDialog() {
-  NOTIMPLEMENTED();
-}
-
-void AppModalDialog::ActivateModalDialog() {
-  NOTIMPLEMENTED();
-}
-
-void AppModalDialog::CloseModalDialog() {
-  NOTIMPLEMENTED();
-}

chrome/browser/app_modal_dialog_win.cc
@@ -1,29 +1,0 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/app_modal_dialog.h"
-
-#include "base/logging.h"
-#include "chrome/browser/views/jsmessage_box_dialog.h"
-#include "views/window/dialog_delegate.h"
-#include "views/window/window.h"
-
-AppModalDialog::~AppModalDialog() {
-}
-
-void AppModalDialog::CreateAndShowDialog() {
-  dialog_ = CreateNativeDialog();
-  DCHECK(dialog_->IsModal());
-  dialog_->ShowModalDialog();
-}
-
-void AppModalDialog::ActivateModalDialog() {
-  DCHECK(dialog_);
-  dialog_->ActivateModalDialog();
-}
-
-void AppModalDialog::CloseModalDialog() {
-  DCHECK(dialog_);
-  dialog_->CloseModalDialog();
-}

chrome/browser/automation/testing_automation_provider.cc
@@ -39,6 +39,7 @@
 #include "chrome/browser/find_bar.h"
 #include "chrome/browser/location_bar.h"
 #include "chrome/browser/login_prompt.h"
+#include "chrome/browser/native_app_modal_dialog.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/prefs/pref_service.h"
@@ -1865,11 +1866,11 @@ void TestingAutomationProvider::SetBooleanPreference(int handle,
 
 void TestingAutomationProvider::GetShowingAppModalDialog(bool* showing_dialog,
                                                          int* dialog_button) {
-  AppModalDialog* dialog_delegate =
-      Singleton<AppModalDialogQueue>()->active_dialog();
-  *showing_dialog = (dialog_delegate != NULL);
+  NativeAppModalDialog* native_dialog =
+      Singleton<AppModalDialogQueue>()->active_dialog()->native_dialog();
+  *showing_dialog = (native_dialog != NULL);
   if (*showing_dialog)
-    *dialog_button = dialog_delegate->GetDialogButtons();
+    *dialog_button = native_dialog->GetAppModalDialogButtons();
   else
     *dialog_button = MessageBoxFlags::DIALOGBUTTON_NONE;
 }
@@ -1878,19 +1879,19 @@ void TestingAutomationProvider::ClickAppModalDialogButton(int button,
                                                           bool* success) {
   *success = false;
 
-  AppModalDialog* dialog_delegate =
-      Singleton<AppModalDialogQueue>()->active_dialog();
-  if (dialog_delegate &&
-      (dialog_delegate->GetDialogButtons() & button) == button) {
+  NativeAppModalDialog* native_dialog =
+      Singleton<AppModalDialogQueue>()->active_dialog()->native_dialog();
+  if (native_dialog &&
+      (native_dialog->GetAppModalDialogButtons() & button) == button) {
     if ((button & MessageBoxFlags::DIALOGBUTTON_OK) ==
         MessageBoxFlags::DIALOGBUTTON_OK) {
-      dialog_delegate->AcceptWindow();
+      native_dialog->AcceptAppModalDialog();
       *success =  true;
     }
     if ((button & MessageBoxFlags::DIALOGBUTTON_CANCEL) ==
         MessageBoxFlags::DIALOGBUTTON_CANCEL) {
       DCHECK(!*success) << "invalid param, OK and CANCEL specified";
-      dialog_delegate->CancelWindow();
+      native_dialog->CancelAppModalDialog();
       *success =  true;
     }
   }

chrome/browser/browser_browsertest.cc
@@ -20,6 +20,7 @@
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/js_modal_dialog.h"
+#include "chrome/browser/native_app_modal_dialog.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
@@ -290,12 +291,12 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_SingleBeforeUnloadAfterWindowClose) {
   browser()->GetTabContentsAt(0)->render_view_host()->
       ExecuteJavascriptInWebFrame(L"", L"w.close(); alert('bar');");
   AppModalDialog* alert = ui_test_utils::WaitForAppModalDialog();
-  alert->AcceptWindow();
+  alert->native_dialog()->AcceptAppModalDialog();
 
   alert = ui_test_utils::WaitForAppModalDialog();
   EXPECT_FALSE(static_cast<JavaScriptAppModalDialog*>(alert)->
                    is_before_unload_dialog());
-  alert->AcceptWindow();
+  alert->native_dialog()->AcceptAppModalDialog();
 }
 
 // Test that get_process_idle_time() returns reasonable values when compared

commit 0393b69a9ff9006b5186d7254d5d4e9eae60f486

Make JavaScript alert and confirm dialog boxes driveable by keyboard.
This regressed in r60785. An error during refactoring used
-[JavaScriptAppModalDialogHelper textField] to determine whether an accessory
view was added to a JavaScript dialog box, but this isn't a simple getter, and
was allays returning true.

BUG=58299
TEST=- javascript:alert("x") allows return keypress to dismiss dialog (OK);
       if FKA is on, allows space bar operation
     - javascript:confirm("x") allows return keypress to dismiss dialog (OK);
       if FKA is on, has focus on Cancel, is tabbable between Cancel and OK,
       and allows space bar operation;
     - javascript:prompt("x") focuses text field and allows return keypress to
       dismiss dialog (OK);
       if FKA is on, is tabbable between text field and buttons, and allows
       space bar operation on buttons
     - Note: FKA is Full Keyboard Access, from System Preferences:Keyboard
Review URL: http://codereview.chromium.org/3555021

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61847 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/cocoa/js_modal_dialog_cocoa.mm
@@ -183,8 +183,8 @@ void JSModalDialogCocoa::ShowAppModalDialog() {
                 didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:)
                    contextInfo:this];
 
-  if ([helper_ textField])
-    [[alert_ window] makeFirstResponder:[helper_ textField]];
+  if ([alert_ accessoryView])
+    [[alert_ window] makeFirstResponder:[alert_ accessoryView]];
 }
 
 void JSModalDialogCocoa::ActivateAppModalDialog() {