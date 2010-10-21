OpenHarmony-TPC/chromium_arkweb

commit 1f934d0af213c330e1604c2abadba5bf702ff460

Screen Locker visual fixes  * Removed "Resume" button  * Added rounded corners at the bottom of signout view  * Moved users label inside ImageView.    I moved UsernameView out from user_controller.cc so that  ScreenLocker can use it.  * Fixed a bug in UsernameView which was using parent's origin    instead of children's.
BUG=chromium-os:5763
TEST=none

Review URL: http://codereview.chromium.org/3889003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63314 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/login/screen_lock_view.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/login/user_view.h"
+#include "chrome/browser/chromeos/login/username_view.h"
 #include "chrome/browser/chromeos/login/wizard_accessibility_helper.h"
 #include "chrome/browser/profile_manager.h"
 #include "chrome/common/notification_service.h"
@@ -19,7 +20,6 @@
 #include "grit/theme_resources.h"
 #include "views/background.h"
 #include "views/border.h"
-#include "views/controls/button/text_button.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
 #include "views/grid_layout.h"
@@ -59,9 +59,24 @@ using login::kBorderSize;
 ScreenLockView::ScreenLockView(ScreenLocker* screen_locker)
     : user_view_(NULL),
       password_field_(NULL),
-      unlock_button_(NULL),
-      screen_locker_(screen_locker) {
+      screen_locker_(screen_locker),
+      main_(NULL),
+      username_(NULL) {
   DCHECK(screen_locker_);
+}
+
+gfx::Size ScreenLockView::GetPreferredSize() {
+  return main_->GetPreferredSize();
+}
+
+void ScreenLockView::Layout() {
+  int username_height = username_->GetPreferredSize().height();
+  main_->SetBounds(0, 0, width(), height());
+  username_->SetBounds(
+      kBorderSize,
+      login::kUserImageSize - username_height + kBorderSize,
+      login::kUserImageSize,
+      username_height);
 }
 
 void ScreenLockView::Init() {
@@ -72,23 +87,18 @@ void ScreenLockView::Init() {
   user_view_ = new UserView(this,
                             false,  // is_login
                             true);  // need_background
-  views::View* main = new views::View();
+  main_ = new views::View();
   // Use rounded rect background.
   views::Painter* painter =
       CreateWizardPainter(&BorderDefinition::kUserBorder);
 
-  main->set_background(
+  main_->set_background(
       views::Background::CreateBackgroundPainter(true, painter));
-  main->set_border(CreateWizardBorder(&BorderDefinition::kUserBorder));
+  main_->set_border(CreateWizardBorder(&BorderDefinition::kUserBorder));
 
   // Password field.
   password_field_ = new PasswordField();
   password_field_->SetController(this);
-
-  // Unlock button.
-  unlock_button_ = new views::TextButton(
-      this, l10n_util::GetString(IDS_UNLOCK_BUTTON));
-  unlock_button_->set_tag(login::UNLOCK);
 
   // User icon.
   UserManager::User user = screen_locker_->user();
@@ -96,16 +106,14 @@ void ScreenLockView::Init() {
 
   // User name.
   std::wstring text = UTF8ToWide(user.GetDisplayName());
-  views::Label* label = new views::Label(text);
-  label->SetColor(login::kTextColor);
+
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
   const gfx::Font& font =
       rb.GetFont(ResourceBundle::LargeFont).DeriveFont(0, gfx::Font::BOLD);
-  label->SetFont(font);
 
   // Layouts image, textfield and button components.
-  GridLayout* layout = new GridLayout(main);
-  main->SetLayoutManager(layout);
+  GridLayout* layout = new GridLayout(main_);
+  main_->SetLayoutManager(layout);
   views::ColumnSet* column_set = layout->AddColumnSet(0);
   column_set->AddPaddingColumn(0, kBorderSize);
   column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 1,
@@ -117,9 +125,6 @@ void ScreenLockView::Init() {
   column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 1,
                         GridLayout::USE_PREF, 0, 0);
   column_set->AddPaddingColumn(0, 5);
-  column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 0,
-                        GridLayout::USE_PREF, 0, 0);
-  column_set->AddPaddingColumn(0, 5);
 
   layout->AddPaddingRow(0, kBorderSize);
   layout->StartRow(0, 0);
@@ -127,26 +132,14 @@ void ScreenLockView::Init() {
   layout->AddPaddingRow(0, kBorderSize);
   layout->StartRow(0, 1);
   layout->AddView(password_field_);
-  layout->AddView(unlock_button_);
-  layout->AddPaddingRow(0, 5);
+  layout->AddPaddingRow(0, kBorderSize);
 
-  unlock_button_->SetFocusable(true);
+  AddChildView(main_);
 
-  // Layouts the main view and the account label.
-  layout = new GridLayout(this);
-  SetLayoutManager(layout);
-  column_set = layout->AddColumnSet(0);
-  column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 1,
-                        GridLayout::USE_PREF, 0, 0);
-
-  column_set = layout->AddColumnSet(1);
-  column_set->AddColumn(GridLayout::FILL, GridLayout::CENTER, 1,
-                        GridLayout::USE_PREF, 0, 0);
-
-  layout->StartRow(0, 0);
-  layout->AddView(main);
-  layout->StartRow(0, 1);
-  layout->AddView(label);
+  UsernameView* username = new UsernameView(text);
+  username_ = username;
+  username->SetFont(font);
+  AddChildView(username);
 }
 
 void ScreenLockView::ClearAndSetFocusToPassword() {
@@ -169,23 +162,10 @@ void ScreenLockView::SetEnabled(bool enabled) {
 
   if (!enabled) {
     user_view_->StartThrobber();
-    // TODO(oshima): Re-enabling does not move the focus to the view
-    // that had a focus (issue http://crbug.com/43131).
-    // Move the focus to other field as a workaround.
-    unlock_button_->RequestFocus();
   } else {
     user_view_->StopThrobber();
   }
-  unlock_button_->SetEnabled(enabled);
   password_field_->SetEnabled(enabled);
-}
-
-void ScreenLockView::ButtonPressed(views::Button* sender,
-                                   const views::Event& event) {
-  if (sender->tag() == login::UNLOCK)
-    screen_locker_->Authenticate(password_field_->text());
-  else
-    NOTREACHED();
 }
 
 void ScreenLockView::OnSignout() {
@@ -213,7 +193,6 @@ void ScreenLockView::Observe(
   UserManager::User* user = Details<UserManager::User>(details).ptr();
   if (screen_locker_->user().email() != user->email())
     return;
-
   user_view_->SetImage(user->image());
 }
 

chrome/app/generated_resources.grd
@@ -8960,9 +8960,6 @@ Keep your key file in a safe place. You will need it to create new versions of y
       <message name="IDS_SCREEN_LOCK_SIGN_OUT">
         Sign out
       </message>
-      <message name="IDS_UNLOCK_BUTTON">
-        Resume
-      </message>
       <message name="IDS_CREATE_ACCOUNT_BUTTON">
         Create a Google Account now
       </message>

chrome/browser/chromeos/login/helper.h
@@ -47,7 +47,6 @@ namespace login {
 
 // Command tag for buttons on the lock screen.
 enum Command {
-  UNLOCK,
   SIGN_OUT,
 };
 

chrome/browser/chromeos/login/screen_lock_view.h
@@ -9,7 +9,6 @@
 #include "chrome/browser/chromeos/login/user_view.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
-#include "views/controls/button/button.h"
 #include "views/controls/textfield/textfield.h"
 #include "views/view.h"
 
@@ -29,7 +28,6 @@ class ScreenLockerTester;
 // ScreenLockView creates view components necessary to authenticate
 // a user to unlock the screen.
 class ScreenLockView : public views::View,
-                       public views::ButtonListener,
                        public views::Textfield::Controller,
                        public NotificationObserver,
                        public UserView::Delegate {
@@ -50,14 +48,13 @@ class ScreenLockView : public views::View,
 
   // views::View implementation:
   virtual void SetEnabled(bool enabled);
+  virtual void Layout();
+  virtual gfx::Size GetPreferredSize();
 
   // NotificationObserver implementation:
   virtual void Observe(NotificationType type,
                        const NotificationSource& source,
                        const NotificationDetails& details);
-
-  // views::ButtonListener implmeentation:
-  virtual void ButtonPressed(views::Button* sender, const views::Event& event);
 
   // views::Textfield::Controller implementation:
   virtual void ContentsChanged(views::Textfield* sender,
@@ -81,12 +78,17 @@ class ScreenLockView : public views::View,
 
   // For editing the password.
   views::Textfield* password_field_;
-  views::Button* unlock_button_;
 
   // ScreenLocker is owned by itself.
   ScreenLocker* screen_locker_;
 
   NotificationRegistrar registrar_;
+
+  // User's picture, signout button and password field.
+  views::View* main_;
+
+  // Username thaty overlays on top of user's picture.
+  views::View* username_;
 
   DISALLOW_COPY_AND_ASSIGN(ScreenLockView);
 };

chrome/browser/chromeos/login/screen_locker.cc
@@ -340,8 +340,7 @@ class ScreenLockerBackgroundView : public chromeos::BackgroundView {
   virtual void Layout() {
     chromeos::BackgroundView::Layout();
     gfx::Rect screen = bounds();
-    gfx::Rect size;
-    lock_widget_->GetBounds(&size, false);
+    gfx::Size size = lock_widget_->GetRootView()->GetPreferredSize();
     lock_widget_->SetBounds(
         gfx::Rect((screen.width() - size.width()) / 2,
                   (screen.height() - size.height()) / 2,
@@ -524,12 +523,10 @@ void ScreenLocker::Init() {
                    G_CALLBACK(OnClientEventThunk), this);
 
   // GTK does not like zero width/height.
-  gfx::Size size(1, 1);
   if (!unlock_on_input_) {
     screen_lock_view_ = new ScreenLockView(this);
     screen_lock_view_->Init();
     screen_lock_view_->SetEnabled(false);
-    size = screen_lock_view_->GetPreferredSize();
   } else {
     input_event_observer_.reset(new InputEventObserver(this));
     MessageLoopForUI::current()->AddObserver(input_event_observer_.get());
@@ -537,7 +534,7 @@ void ScreenLocker::Init() {
 
   lock_widget_ = new GrabWidget(this);
   lock_widget_->MakeTransparent();
-  lock_widget_->InitWithWidget(lock_window_, gfx::Rect(size));
+  lock_widget_->InitWithWidget(lock_window_, gfx::Rect());
   if (screen_lock_view_)
     lock_widget_->SetContentsView(screen_lock_view_);
   lock_widget_->Show();

chrome/browser/chromeos/login/user_controller.cc
@@ -17,6 +17,7 @@
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
 #include "chrome/browser/chromeos/login/user_view.h"
+#include "chrome/browser/chromeos/login/username_view.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/notification_type.h"
@@ -48,9 +49,6 @@ const int kUserNameGap = 4;
 // case to make border window size close to existing users.
 const int kControlsHeight = 26;
 
-// Username label background color.
-const SkColor kLabelBackgoundColor = 0x55000000;
-
 // Widget that notifies window manager about clicking on itself.
 // Doesn't send anything if user is selected.
 class ClickNotifyingWidget : public views::WidgetGtk {
@@ -72,53 +70,6 @@ class ClickNotifyingWidget : public views::WidgetGtk {
   UserController* controller_;
 
   DISALLOW_COPY_AND_ASSIGN(ClickNotifyingWidget);
-};
-
-// View that contains two parts. First one is a label with username, second one
-// is an empty view with gradient transparency background.
-class UsernameView : public views::View {
- public:
-  explicit UsernameView(const std::wstring& username) {
-    label_ = new views::Label(username);
-    label_->set_background(
-        views::Background::CreateSolidBackground(kLabelBackgoundColor));
-    label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
-    AddChildView(label_);
-
-    gradient_ = new views::View;
-    gradient_->SetVisible(false);
-    views::Background* gradient_background =
-        views::Background::CreateBackgroundPainter(true,
-            views::Painter::CreateHorizontalGradient(
-                kLabelBackgoundColor, 0x00000000));
-    gradient_->set_background(gradient_background);
-    AddChildView(gradient_);
-  }
-
-  virtual ~UsernameView() {}
-
-  // Returns username label.
-  views::Label* get_label() {
-    return label_;
-  }
-
-  // views::View
-  virtual void Layout() {
-    int text_width = std::min(label_->GetPreferredSize().width(), width());
-    label_->SetBounds(x(), y(), text_width, height());
-    int gradient_width = std::min(width() - text_width, height());
-    if (gradient_width > 0)  {
-      gradient_->SetVisible(true);
-      gradient_->SetBounds(x() + text_width, y(), gradient_width, height());
-    } else {
-      // No place for the gradient part.
-      gradient_->SetVisible(false);
-    }
-  }
-
- private:
-  views::Label* label_;
-  views::View* gradient_;
 };
 
 }  // namespace
@@ -507,15 +458,16 @@ WidgetGtk* UserController::CreateLabelWindow(int index,
   views::View *view;
   if (is_new_user_) {
     label = new views::Label(text);
+    label->SetColor(kTextColor);
+    label->SetFont(font);
     view = label;
   } else {
     UsernameView* username_view = new UsernameView(text);
-    label = username_view->get_label();
+    username_view->SetFont(font);
+    label = username_view->label();
     view = username_view;
   }
 
-  label->SetColor(kTextColor);
-  label->SetFont(font);
   if (type == WM_IPC_WINDOW_LOGIN_LABEL)
     label_view_ = label;
   else

chrome/browser/chromeos/login/user_view.cc
@@ -8,19 +8,24 @@
 #include "app/resource_bundle.h"
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
+#include "gfx/canvas_skia.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "views/background.h"
 #include "views/controls/button/menu_button.h"
 #include "views/controls/button/text_button.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
 #include "views/controls/link.h"
 #include "views/controls/throbber.h"
+#include "views/painter.h"
 
 namespace {
 
-// Background color of the login status label and signout button.
+// Background color and corner radius of the login status label and
+// signout button.
 const SkColor kSignoutBackgroundColor = 0xFF007700;
+const int kSignoutBackgroundCornerRadius = 4;
 
 // Horiz/Vert insets for Signout view.
 const int kSignoutViewHorizontalInsets = 10;
@@ -28,6 +33,29 @@ const int kSignoutViewVerticalInsets = 5;
 
 // Padding between remove button and top right image corner.
 const int kRemoveButtonPadding = 3;
+
+// Draws green-ish background for signout view with
+// rounded corners at the bottom.
+class SignoutBackgroundPainter : public views::Painter {
+  virtual void Paint(int w, int h, gfx::Canvas* canvas) {
+    SkRect rect = {0, 0, w, h};
+    SkPath path;
+    SkScalar corners[] = {
+      0, 0,
+      0, 0,
+      kSignoutBackgroundCornerRadius,
+      kSignoutBackgroundCornerRadius,
+      kSignoutBackgroundCornerRadius,
+      kSignoutBackgroundCornerRadius,
+    };
+    path.addRoundRect(rect, corners);
+    SkPaint paint;
+    paint.setStyle(SkPaint::kFill_Style);
+    paint.setFlags(SkPaint::kAntiAlias_Flag);
+    paint.setColor(kSignoutBackgroundColor);
+    canvas->AsCanvasSkia()->drawPath(path, paint);
+  }
+};
 
 }  // namespace
 
@@ -59,8 +87,8 @@ class SignoutView : public views::View {
     AddChildView(active_user_label_);
     AddChildView(signout_link_);
 
-    set_background(views::Background::CreateSolidBackground(
-        kSignoutBackgroundColor));
+    set_background(views::Background::CreateBackgroundPainter(
+        true, new SignoutBackgroundPainter()));
   }
 
   // views::View overrides.

chrome/browser/chromeos/login/username_view.cc
@@ -1,0 +1,59 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/login/username_view.h"
+
+#include "chrome/browser/chromeos/login/helper.h"
+#include "views/background.h"
+#include "views/painter.h"
+#include "views/controls/label.h"
+
+namespace {
+// Username label background color.
+const SkColor kLabelBackgoundColor = 0x55000000;
+}  // namespace
+
+namespace chromeos {
+
+UsernameView::UsernameView(const std::wstring& username) {
+  label_ = new views::Label(username);
+  label_->SetColor(login::kTextColor);
+  label_->set_background(
+      views::Background::CreateSolidBackground(kLabelBackgoundColor));
+  label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
+  AddChildView(label_);
+
+  gradient_ = new views::View;
+  gradient_->SetVisible(false);
+  views::Background* gradient_background =
+      views::Background::CreateBackgroundPainter(true,
+          views::Painter::CreateHorizontalGradient(
+              kLabelBackgoundColor, 0x00000000));
+  gradient_->set_background(gradient_background);
+  AddChildView(gradient_);
+}
+
+void UsernameView::SetFont(const gfx::Font& font) {
+  label_->SetFont(font);
+}
+
+// views::View
+void UsernameView::Layout() {
+  int text_width = std::min(label_->GetPreferredSize().width(), width());
+  label_->SetBounds(0, 0, text_width, height());
+  int gradient_width = std::min(width() - text_width, height());
+  if (gradient_width > 0)  {
+    gradient_->SetVisible(true);
+    gradient_->SetBounds(0 + text_width, 0, gradient_width, height());
+  } else {
+    // No place for the gradient part.
+    gradient_->SetVisible(false);
+  }
+}
+
+gfx::Size UsernameView::GetPreferredSize() {
+  return label_->GetPreferredSize();
+}
+
+}  // namespace chromeos

commit 31334141d00222ecc767d5056047fefb22118dd2

Fix UI regression in r63314.   Clear focus of textfield so that re-enabling textfield sets the focus to   the textfield.
BUG=chromium-os:5763
TEST=manual. in locked state, type wrong password. the focus should
    be back to text field.

Review URL: http://codereview.chromium.org/3996005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63442 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chromeos/login/screen_lock_view.cc
@@ -162,6 +162,14 @@ void ScreenLockView::SetEnabled(bool enabled) {
 
   if (!enabled) {
     user_view_->StartThrobber();
+    // TODO(oshima): Re-enabling does not move the focus to the view
+    // that had a focus (issue http://crbug.com/43131).
+    // Clear focus on the textfield so that re-enabling can set focus
+    // back to the text field.
+    // FocusManager may be null if the view does not have
+    // associated Widget yet.
+    if (password_field_->GetFocusManager())
+      password_field_->GetFocusManager()->ClearFocus();
   } else {
     user_view_->StopThrobber();
   }