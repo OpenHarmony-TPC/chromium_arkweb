OpenHarmony-TPC/chromium_arkweb

commit 8d09b7c48ff184e9977cd45b82d14e0a482aace2

Correctly handle visibility-only move messages in accelerated Mac plugins
BUG=60792
TEST=Toggle the container of an accelerated plugin between visible and invisible; the plugin should change visibility correctly.

Review URL: http://codereview.chromium.org/5273002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67027 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/render_widget_host_view_mac.mm
@@ -577,30 +577,25 @@ void RenderWidgetHostViewMac::MovePluginWindows(
        iter != moves.end();
        ++iter) {
     webkit_glue::WebPluginGeometry geom = *iter;
-    // Ignore bogus moves which claim to move the plugin to (0, 0)
-    // with width and height (0, 0)
-    if (geom.window_rect.x() == 0 &&
-        geom.window_rect.y() == 0 &&
-        geom.window_rect.IsEmpty()) {
-      continue;
-    }
-
-    gfx::Rect rect = geom.window_rect;
-    if (geom.visible) {
-      rect.set_x(rect.x() + geom.clip_rect.x());
-      rect.set_y(rect.y() + geom.clip_rect.y());
-      rect.set_width(geom.clip_rect.width());
-      rect.set_height(geom.clip_rect.height());
-    }
 
     PluginViewMap::iterator it = plugin_views_.find(geom.window);
     DCHECK(plugin_views_.end() != it);
     if (plugin_views_.end() == it) {
       continue;
     }
-    NSRect new_rect([cocoa_view_ flipRectToNSRect:rect]);
-    [it->second setFrame:new_rect];
-    [it->second setNeedsDisplay:YES];
+
+    if (geom.rects_valid) {
+      gfx::Rect rect = geom.window_rect;
+      if (geom.visible) {
+        rect.set_x(rect.x() + geom.clip_rect.x());
+        rect.set_y(rect.y() + geom.clip_rect.y());
+        rect.set_width(geom.clip_rect.width());
+        rect.set_height(geom.clip_rect.height());
+      }
+      NSRect new_rect([cocoa_view_ flipRectToNSRect:rect]);
+      [it->second setFrame:new_rect];
+      [it->second setNeedsDisplay:YES];
+    }
 
     plugin_container_manager_.SetPluginContainerGeometry(geom);
 

chrome/browser/renderer_host/accelerated_surface_container_mac.cc
@@ -53,7 +53,8 @@ void AcceleratedSurfaceContainerMac::SetSizeAndTransportDIB(
 void AcceleratedSurfaceContainerMac::SetGeometry(
     const webkit_glue::WebPluginGeometry& geom) {
   visible_ = geom.visible;
-  clipRect_ = geom.clip_rect;
+  if (geom.rects_valid)
+    clip_rect_ = geom.clip_rect;
 }
 
 void AcceleratedSurfaceContainerMac::Draw(CGLContextObj context) {
@@ -140,10 +141,10 @@ void AcceleratedSurfaceContainerMac::Draw(CGLContextObj context) {
     // TODO(kbr): convert this to use only OpenGL ES 2.0 functionality.
 
     // TODO(kbr): may need to pay attention to cutout rects.
-    int clipX = clipRect_.x();
-    int clipY = clipRect_.y();
-    int clipWidth = clipRect_.width();
-    int clipHeight = clipRect_.height();
+    int clipX = clip_rect_.x();
+    int clipY = clip_rect_.y();
+    int clipWidth = clip_rect_.width();
+    int clipHeight = clip_rect_.height();
 
     if (clipX + clipWidth > texture_width)
       clipWidth = texture_width - clipX;
@@ -195,6 +196,10 @@ void AcceleratedSurfaceContainerMac::Draw(CGLContextObj context) {
     glEnd();
     glDisable(target);
   }
+}
+
+bool AcceleratedSurfaceContainerMac::ShouldBeVisible() const {
+  return visible_ && was_painted_to_ && !clip_rect_.IsEmpty();
 }
 
 void AcceleratedSurfaceContainerMac::set_was_painted_to(uint64 surface_id) {

chrome/browser/renderer_host/accelerated_surface_container_mac.h
@@ -76,14 +76,14 @@ class AcceleratedSurfaceContainerMac {
   // time the drawing context has changed.
   void ForceTextureReload() { texture_needs_upload_ = true; }
 
+  // Returns if the surface should be shown.
+  bool ShouldBeVisible() const;
+
   // Notifies the the container that its surface was painted to.
   void set_was_painted_to(uint64 surface_id);
 
   // Notifies the container that its surface is invalid.
   void set_surface_invalid() { was_painted_to_ = false; }
-
-  // Returns if the surface should be shown.
-  bool should_be_visible() const { return visible_ && was_painted_to_; }
  private:
   // The manager of this accelerated surface container.
   AcceleratedSurfaceContainerManagerMac* manager_;
@@ -115,7 +115,7 @@ class AcceleratedSurfaceContainerMac {
   int32 height_;
 
   // The clip rectangle, relative to the (x_, y_) origin.
-  gfx::Rect clipRect_;
+  gfx::Rect clip_rect_;
 
   // The "live" OpenGL texture referring to this IOSurfaceRef. Note
   // that per the CGLTexImageIOSurface2D API we do not need to

chrome/browser/renderer_host/accelerated_surface_container_manager_mac.cc
@@ -156,7 +156,7 @@ bool AcceleratedSurfaceContainerManagerMac::SurfaceShouldBeVisible(
     return false;
 
   AcceleratedSurfaceContainerMac* container = MapIDToContainer(id);
-  return container && container->should_be_visible();
+  return container && container->ShouldBeVisible();
 }
 
 AcceleratedSurfaceContainerMac*

commit 4a4b153ad59289b135027c28bbb64dae1c3e91f2

Fix regression introduced in r67027 which broke the accelerated compositor on Mac OS X.
BUG=64295
TEST=none (ran CSS 3D and WebGL content)
TBR=stuartmorgan

Review URL: http://codereview.chromium.org/5314006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67220 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/renderer_host/render_widget_host_view_mac.mm
@@ -920,6 +920,7 @@ void RenderWidgetHostViewMac::AcceleratedSurfaceSetIOSurface(
     geom.window_rect = rect;
     geom.clip_rect = rect;
     geom.visible = true;
+    geom.rects_valid = true;
     MovePluginWindows(std::vector<webkit_glue::WebPluginGeometry>(1, geom));
   }
 }