OpenHarmony-TPC/chromium_arkweb

commit 82250ba133f57cda284affbd415ea0de4ffc907a

Attempt to reland http://codereview.chromium.org/4928002/
For some reason the Win bot failed to compile the sandbox unit test.
Trybots and my machines don't have these build problems, so I'm going to try again, unchanged.

Original description:

Changing the installer switches from wchar_t[] to char[].

Because of this I'm also refactoring some code that before
was using wstring to build command lines by hand instead of
using the CommandLine class. Now we use CommandLine.

To get this to work correctly, I also needed to fix CommandLine::AppendArguments so I added a little test for it.

TEST=There should be no changes in functionality. Run all installer tests.
BUG=61609

TBR=robertshield

Review URL: http://codereview.chromium.org/4989001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66096 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/browser_main_win.cc
@@ -188,12 +188,12 @@ bool CheckMachineLevelInstall() {
       const std::wstring caption = l10n_util::GetString(IDS_PRODUCT_NAME);
       const UINT flags = MB_OK | MB_ICONERROR | MB_TOPMOST;
       win_util::MessageBox(NULL, text, caption, flags);
-      std::wstring uninstall_cmd = InstallUtil::GetChromeUninstallCmd(false);
-      if (!uninstall_cmd.empty()) {
-        uninstall_cmd.append(L" --");
-        uninstall_cmd.append(installer_util::switches::kForceUninstall);
-        uninstall_cmd.append(L" --");
-        uninstall_cmd.append(installer_util::switches::kDoNotRemoveSharedItems);
+      FilePath uninstall_path(InstallUtil::GetChromeUninstallCmd(false));
+      CommandLine uninstall_cmd(uninstall_path);
+      if (!uninstall_cmd.GetProgram().value().empty()) {
+        uninstall_cmd.AppendSwitch(installer_util::switches::kForceUninstall);
+        uninstall_cmd.AppendSwitch(
+            installer_util::switches::kDoNotRemoveSharedItems);
         base::LaunchApp(uninstall_cmd, false, false, NULL);
       }
       return true;

chrome/installer/util/google_chrome_distribution.cc
@@ -139,15 +139,14 @@ bool RelaunchSetup(const std::string& flag, int value,
 
   // Re-add the system level toast flag.
   if (system_level_toast) {
-    new_cmd_line.AppendSwitch(
-      WideToASCII(installer_util::switches::kSystemLevelToast));
+    new_cmd_line.AppendSwitch(installer_util::switches::kSystemLevelToast);
 
     // Re-add the toast result key. We need to do this because Setup running as
     // system passes the key to Setup running as user, but that child process
     // does not perform the actual toasting, it launches another Setup (as user)
     // to do so. That is the process that needs the key.
     const CommandLine& current_cmd_line = *CommandLine::ForCurrentProcess();
-    std::string key = WideToASCII(installer_util::switches::kToastResultsKey);
+    std::string key(installer_util::switches::kToastResultsKey);
     std::string toast_key = current_cmd_line.GetSwitchValueASCII(key);
     if (!toast_key.empty()) {
       new_cmd_line.AppendSwitchASCII(key, toast_key);
@@ -217,17 +216,16 @@ bool FixDACLsForExecute(const wchar_t* exe) {
 // the computer is on but nobody has logged in locally.
 // Remote Desktop sessions do not count as interactive sessions; running this
 // method as a user logged in via remote desktop will do nothing.
-bool RelaunchSetupAsConsoleUser(const std::wstring& flag) {
+bool RelaunchSetupAsConsoleUser(const std::string& flag) {
   FilePath setup_exe = CommandLine::ForCurrentProcess()->GetProgram();
   CommandLine cmd_line(setup_exe);
-  cmd_line.AppendSwitch(WideToASCII(flag));
+  cmd_line.AppendSwitch(flag);
 
   // Get the Google Update results key, and pass it on the command line to
   // the child process.
   int key = GoogleUpdateSettings::DuplicateGoogleUpdateSystemClientKey();
-  cmd_line.AppendSwitchASCII(
-      WideToASCII(installer_util::switches::kToastResultsKey),
-      base::IntToString(key));
+  cmd_line.AppendSwitchASCII(installer_util::switches::kToastResultsKey,
+                             base::IntToString(key));
 
   if (base::win::GetVersion() > base::win::VERSION_XP) {
     // Make sure that in Vista and Above we have the proper DACLs so
@@ -530,7 +528,7 @@ void SetClient(std::wstring experiment_group, bool last_write) {
     if (cmd_line.HasSwitch(installer_util::switches::kToastResultsKey)) {
       // Get the handle to the key under HKLM.
       base::StringToInt(cmd_line.GetSwitchValueASCII(
-          WideToASCII(installer_util::switches::kToastResultsKey)).c_str(),
+          installer_util::switches::kToastResultsKey).c_str(),
           &reg_key_handle);
     } else {
       reg_key_handle = 0;

chrome/installer/util/install_util.cc
@@ -12,7 +12,6 @@
 
 #include <algorithm>
 
-#include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
@@ -37,14 +36,28 @@ const MasterPreferences& InstallUtil::GetMasterPreferencesForCurrentProcess() {
   return prefs;
 }
 
-bool InstallUtil::ExecuteExeAsAdmin(const std::wstring& exe,
-                                    const std::wstring& params,
-                                    DWORD* exit_code) {
+bool InstallUtil::ExecuteExeAsAdmin(const CommandLine& cmd, DWORD* exit_code) {
+  FilePath::StringType program(cmd.GetProgram().value());
+  DCHECK(!program.empty());
+  DCHECK(program[0] != '\"');
+
+  CommandLine::StringType params(cmd.command_line_string());
+  if (params[0] == '"') {
+    DCHECK_EQ('"', params[program.length() + 1]);
+    DCHECK_EQ(program, params.substr(1, program.length()));
+    params = params.substr(program.length() + 2);
+  } else {
+    DCHECK_EQ(program, params.substr(0, program.length()));
+    params = params.substr(program.length());
+  }
+
+  TrimWhitespace(params, TRIM_ALL, &params);
+
   SHELLEXECUTEINFO info = {0};
   info.cbSize = sizeof(SHELLEXECUTEINFO);
   info.fMask = SEE_MASK_NOCLOSEPROCESS;
   info.lpVerb = L"runas";
-  info.lpFile = exe.c_str();
+  info.lpFile = program.c_str();
   info.lpParameters = params.c_str();
   info.nShow = SW_SHOW;
   if (::ShellExecuteEx(&info) == FALSE)

chrome/installer/util/install_util.h
@@ -15,6 +15,7 @@
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/command_line.h"
 #include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/util_constants.h"
 #include "chrome/installer/util/version.h"
@@ -33,9 +34,7 @@ class RegKey;
 class InstallUtil {
  public:
   // Launches given exe as admin on Vista.
-  static bool ExecuteExeAsAdmin(const std::wstring& exe,
-                                const std::wstring& params,
-                                DWORD* exit_code);
+  static bool ExecuteExeAsAdmin(const CommandLine& cmd, DWORD* exit_code);
 
   // Reads the uninstall command for Chromium from registry and returns it.
   // If system_install is true the command is read from HKLM, otherwise

base/command_line.cc
@@ -431,7 +431,14 @@ void CommandLine::AppendArguments(const CommandLine& other,
   // Verify include_program is used correctly.
   // Logic could be shorter but this is clearer.
   DCHECK_EQ(include_program, !other.GetProgram().empty());
-  command_line_string_ += L" " + other.command_line_string_;
+  if (include_program)
+    program_ = other.program_;
+
+  if (!command_line_string_.empty())
+    command_line_string_ += L' ';
+
+  command_line_string_ += other.command_line_string_;
+
   std::map<std::string, StringType>::const_iterator i;
   for (i = other.switches_.begin(); i != other.switches_.end(); ++i)
     switches_[i->first] = i->second;
@@ -482,9 +489,15 @@ void CommandLine::AppendArguments(const CommandLine& other,
   // Verify include_program is used correctly.
   // Logic could be shorter but this is clearer.
   DCHECK_EQ(include_program, !other.GetProgram().empty());
-  size_t first_arg = include_program ? 0 : 1;
-  for (size_t i = first_arg; i < other.argv_.size(); ++i)
+
+  if (include_program)
+    argv_[0] = other.argv_[0];
+
+  // Skip the first arg when copying since it's the program but push all
+  // arguments to our arg vector.
+  for (size_t i = 1; i < other.argv_.size(); ++i)
     argv_.push_back(other.argv_[i]);
+
   std::map<std::string, StringType>::const_iterator i;
   for (i = other.switches_.begin(); i != other.switches_.end(); ++i)
     switches_[i->first] = i->second;

base/command_line_unittest.cc
@@ -5,8 +5,8 @@
 #include <string>
 #include <vector>
 
-#include "base/command_line.h"
 #include "base/basictypes.h"
+#include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -154,3 +154,28 @@ TEST(CommandLineTest, AppendSwitches) {
             cl.command_line_string());
 #endif
 }
+
+// Tests that when AppendArguments is called that the program is set correctly
+// on the target CommandLine object and the switches from the source
+// CommandLine are added to the target.
+TEST(CommandLineTest, AppendArguments) {
+  CommandLine cl1(FilePath(FILE_PATH_LITERAL("Program")));
+  cl1.AppendSwitch("switch1");
+  cl1.AppendSwitchASCII("switch2", "foo");
+
+  CommandLine cl2(CommandLine::NO_PROGRAM);
+  cl2.AppendArguments(cl1, true);
+  EXPECT_EQ(cl1.GetProgram().value(), cl2.GetProgram().value());
+  EXPECT_EQ(cl1.command_line_string(), cl2.command_line_string());
+
+  CommandLine c1(FilePath(FILE_PATH_LITERAL("Program1")));
+  c1.AppendSwitch("switch1");
+  CommandLine c2(FilePath(FILE_PATH_LITERAL("Program2")));
+  c2.AppendSwitch("switch2");
+
+  c1.AppendArguments(c2, true);
+  EXPECT_EQ(c1.GetProgram().value(), c2.GetProgram().value());
+  EXPECT_TRUE(c1.HasSwitch("switch1"));
+  EXPECT_TRUE(c1.HasSwitch("switch2"));
+}
+

chrome/installer/setup/install.cc
@@ -82,15 +82,10 @@ void AddInstallerCopyTasks(const std::wstring& exe_path,
   }
 }
 
-// A little helper function to save on tons of WideToASCII call sites.
-void AppendWideSwitch(CommandLine* cmd, const wchar_t* switch_name) {
-  cmd->AppendSwitch(WideToASCII(switch_name));
-}
-
 void AppendUninstallCommandLineFlags(CommandLine* uninstall_cmd,
                                      bool is_system) {
   DCHECK(uninstall_cmd);
-  AppendWideSwitch(uninstall_cmd, installer_util::switches::kUninstall);
+  uninstall_cmd->AppendSwitch(installer_util::switches::kUninstall);
 
   // TODO(tommi): In case of multiple installations, we need to create multiple
   // uninstall entries, and not one magic one for all.
@@ -99,26 +94,26 @@ void AppendUninstallCommandLineFlags(CommandLine* uninstall_cmd,
   DCHECK(!prefs.is_multi_install());
 
   if (prefs.install_chrome_frame()) {
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kDeleteProfile);
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kChromeFrame);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kDeleteProfile);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kChromeFrame);
   }
 
   if (InstallUtil::IsChromeSxSProcess()) {
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kChromeSxS);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kChromeSxS);
   }
 
   if (InstallUtil::IsMSIProcess(is_system)) {
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kMsi);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kMsi);
   }
 
   // Propagate the verbose logging switch to uninstalls too.
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(installer_util::switches::kVerboseLogging)) {
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kVerboseLogging);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kVerboseLogging);
   }
 
   if (is_system) {
-    AppendWideSwitch(uninstall_cmd, installer_util::switches::kSystemLevel);
+    uninstall_cmd->AppendSwitch(installer_util::switches::kSystemLevel);
   }
 }
 
@@ -420,30 +415,26 @@ bool DoPostInstallTasks(HKEY reg_root,
                                        google_update::kRegOldVersionField,
                                        current_version.c_str(),
                                        true);
+    FilePath installer_path(installer::GetInstallerPathUnderChrome(install_path,
+        new_version.GetString()));
+    installer_path = installer_path.Append(
+        file_util::GetFilenameFromPath(exe_path));
 
-    std::wstring rename_cmd(installer::GetInstallerPathUnderChrome(
-        install_path, new_version.GetString()));
-    file_util::AppendToPath(&rename_cmd,
-                            file_util::GetFilenameFromPath(exe_path));
-    rename_cmd = L"\"" + rename_cmd +
-                 L"\" --" + installer_util::switches::kRenameChromeExe;
+    CommandLine rename_cmd(installer_path);
+    rename_cmd.AppendSwitch(installer_util::switches::kRenameChromeExe);
     if (is_system_install)
-      rename_cmd = rename_cmd + L" --" + installer_util::switches::kSystemLevel;
+      rename_cmd.AppendSwitch(installer_util::switches::kSystemLevel);
 
-    if (prefs.install_chrome_frame()) {
-      rename_cmd += L" --";
-      rename_cmd += installer_util::switches::kChromeFrame;
-    }
+    if (prefs.install_chrome_frame())
+      rename_cmd.AppendSwitch(installer_util::switches::kChromeFrame);
 
-    if (InstallUtil::IsChromeSxSProcess()) {
-      rename_cmd += L" --";
-      rename_cmd += installer_util::switches::kChromeSxS;
-    }
+    if (InstallUtil::IsChromeSxSProcess())
+      rename_cmd.AppendSwitch(installer_util::switches::kChromeSxS);
 
     inuse_list->AddSetRegValueWorkItem(reg_root,
                                        version_key,
                                        google_update::kRegRenameCmdField,
-                                       rename_cmd.c_str(),
+                                       rename_cmd.command_line_string(),
                                        true);
     if (!inuse_list->Do()) {
       LOG(ERROR) << "Couldn't write opv/cmd values to registry.";

chrome/installer/setup/setup_main.cc
@@ -401,7 +401,6 @@ installer_util::InstallStatus InstallChrome(const CommandLine& cmd_line,
 }
 
 installer_util::InstallStatus UninstallChrome(const CommandLine& cmd_line,
-                                              const wchar_t* cmd_params,
                                               const installer::Version* version,
                                               bool system_install) {
   VLOG(1) << "Uninstalling Chome";
@@ -420,7 +419,7 @@ installer_util::InstallStatus UninstallChrome(const CommandLine& cmd_line,
   return installer_setup::UninstallChrome(cmd_line.GetProgram().value(),
                                           system_install,
                                           remove_all, force,
-                                          cmd_line, cmd_params);
+                                          cmd_line);
 }
 
 installer_util::InstallStatus ShowEULADialog(const std::wstring& inner_frame) {
@@ -681,14 +680,13 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
   if (system_install && !IsUserAnAdmin()) {
     if (base::win::GetVersion() >= base::win::VERSION_VISTA &&
         !parsed_command_line.HasSwitch(installer_util::switches::kRunAsAdmin)) {
-      std::wstring exe = parsed_command_line.GetProgram().value();
-      std::wstring params(command_line);
+      CommandLine new_cmd(CommandLine::NO_PROGRAM);
+      new_cmd.AppendArguments(parsed_command_line, true);
       // Append --run-as-admin flag to let the new instance of setup.exe know
       // that we already tried to launch ourselves as admin.
-      params.append(L" --");
-      params.append(installer_util::switches::kRunAsAdmin);
+      new_cmd.AppendSwitch(installer_util::switches::kRunAsAdmin);
       DWORD exit_code = installer_util::UNKNOWN_STATUS;
-      InstallUtil::ExecuteExeAsAdmin(exe, params, &exit_code);
+      InstallUtil::ExecuteExeAsAdmin(new_cmd, &exit_code);
       return exit_code;
     } else {
       LOG(ERROR) << "Non admin user can not install system level Chrome.";
@@ -710,7 +708,6 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
   // If --uninstall option is given, uninstall chrome
   if (parsed_command_line.HasSwitch(installer_util::switches::kUninstall)) {
     install_status = UninstallChrome(parsed_command_line,
-                                     command_line,
                                      installed_version.get(),
                                      system_install);
   // If --uninstall option is not specified, we assume it is install case.

commit 00f644a52cc6e3efaecb890433104f79d94d25ab

Fix user-to-system-level installation delegation. This looks to have been broken since r66096.
BUG=72620
TEST=Install user level Chrome then system level Chrome. Observer that user level Chrome removes itself and delegates to system level.


Review URL: http://codereview.chromium.org/6490008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74609 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/browser_main_win.cc
@@ -194,9 +194,9 @@ bool CheckMachineLevelInstall() {
       const string16 caption = l10n_util::GetStringUTF16(IDS_PRODUCT_NAME);
       const UINT flags = MB_OK | MB_ICONERROR | MB_TOPMOST;
       ui::MessageBox(NULL, text, caption, flags);
-      FilePath uninstall_path(InstallUtil::GetChromeUninstallCmd(false, dist));
-      CommandLine uninstall_cmd(uninstall_path);
-      if (!uninstall_cmd.GetProgram().value().empty()) {
+      CommandLine uninstall_cmd(
+          InstallUtil::GetChromeUninstallCmd(false, dist->GetType()));
+      if (!uninstall_cmd.GetProgram().empty()) {
         uninstall_cmd.AppendSwitch(installer::switches::kForceUninstall);
         uninstall_cmd.AppendSwitch(
             installer::switches::kDoNotRemoveSharedItems);

chrome/installer/util/google_chrome_distribution.cc
@@ -686,9 +686,8 @@ void GoogleChromeDistribution::InactiveUserToastExperiment(int flavor,
   bool system_level_toast = CommandLine::ForCurrentProcess()->HasSwitch(
       installer::switches::kSystemLevelToast);
 
-  std::wstring cmd(InstallUtil::GetChromeUninstallCmd(
-      system_level_toast, this));
-
+  CommandLine cmd(InstallUtil::GetChromeUninstallCmd(system_level_toast,
+                                                     GetType()));
   base::LaunchApp(cmd, false, false, NULL);
 }
 #endif

chrome/installer/util/install_util.cc
@@ -26,10 +26,12 @@
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/l10n_string_util.h"
+#include "chrome/installer/util/installation_state.h"
 #include "chrome/installer/util/util_constants.h"
 #include "chrome/installer/util/work_item_list.h"
 
 using base::win::RegKey;
+using installer::ProductState;
 
 bool InstallUtil::ExecuteExeAsAdmin(const CommandLine& cmd, DWORD* exit_code) {
   FilePath::StringType program(cmd.GetProgram().value());
@@ -68,14 +70,13 @@ bool InstallUtil::ExecuteExeAsAdmin(const CommandLine& cmd, DWORD* exit_code) {
   return true;
 }
 
-std::wstring InstallUtil::GetChromeUninstallCmd(bool system_install,
-                                                BrowserDistribution* dist) {
-  DCHECK(dist);
-  HKEY root = system_install ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER;
-  RegKey key(root, dist->GetUninstallRegPath().c_str(), KEY_READ);
-  std::wstring uninstall_cmd;
-  key.ReadValue(installer::kUninstallStringField, &uninstall_cmd);
-  return uninstall_cmd;
+CommandLine InstallUtil::GetChromeUninstallCmd(
+    bool system_install, BrowserDistribution::Type distribution_type) {
+  ProductState state;
+  if (state.Initialize(system_install, distribution_type)) {
+    return state.uninstall_command();
+  }
+  return CommandLine(CommandLine::NO_PROGRAM);
 }
 
 Version* InstallUtil::GetChromeVersion(BrowserDistribution* dist,

chrome/installer/util/install_util.h
@@ -15,10 +15,10 @@
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/command_line.h"
+#include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/util_constants.h"
 
-class BrowserDistribution;
-class CommandLine;
 class Version;
 class WorkItemList;
 
@@ -39,8 +39,9 @@ class InstallUtil {
   // Reads the uninstall command for Chromium from registry and returns it.
   // If system_install is true the command is read from HKLM, otherwise
   // from HKCU.
-  static std::wstring GetChromeUninstallCmd(bool system_install,
-                                            BrowserDistribution* dist);
+  static CommandLine GetChromeUninstallCmd(
+      bool system_install,
+      BrowserDistribution::Type distribution_type);
 
   // Find the version of Chrome installed on the system by checking the
   // Google Update registry key. Returns the version or NULL if no version is