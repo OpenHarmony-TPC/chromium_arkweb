OpenHarmony-TPC/chromium_arkweb

commit 71b673e4e276145e9219271564123f612209f0fd

Clear RenderThread of any Chrome specific code, and move a bunch of stuff out of RenderView.
Took out CookieMessageFilter since it's not used anymore.  Removed RenderProcessTest and RendererMainTest since they really weren't testing much.
Review URL: http://codereview.chromium.org/6873014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81851 0039d316-1c4b-4281-b951-d872f2087c98

chrome/chrome_renderer.gypi
@@ -95,8 +95,10 @@
         'renderer/blocked_plugin.h',
         'renderer/chrome_content_renderer_client.cc',
         'renderer/chrome_content_renderer_client.h',
-        'renderer/chrome_render_observer.cc',
-        'renderer/chrome_render_observer.h',
+        'renderer/chrome_render_process_observer.cc',
+        'renderer/chrome_render_process_observer.h',
+        'renderer/chrome_render_view_observer.cc',
+        'renderer/chrome_render_view_observer.h',
         'renderer/devtools_agent.cc',
         'renderer/devtools_agent.h',
         'renderer/devtools_agent_filter.cc',

chrome/chrome_tests.gypi
@@ -1828,7 +1828,6 @@
         'renderer/net/predictor_queue_unittest.cc',
         'renderer/net/renderer_predictor_unittest.cc',
         'renderer/renderer_about_handler_unittest.cc',
-        'renderer/renderer_main_unittest.cc',
         'renderer/safe_browsing/features_unittest.cc',
         'renderer/safe_browsing/phishing_term_feature_extractor_unittest.cc',
         'renderer/safe_browsing/phishing_url_feature_extractor_unittest.cc',
@@ -1915,7 +1914,6 @@
         '../content/renderer/media/audio_renderer_impl_unittest.cc',
         '../content/renderer/p2p/p2p_transport_impl_unittest.cc',
         '../content/renderer/paint_aggregator_unittest.cc',
-        '../content/renderer/render_process_unittest.cc',
         '../content/renderer/render_widget_unittest.cc',
         '../testing/gtest_mac_unittest.mm',
         '../third_party/cld/encodings/compact_lang_det/compact_lang_det_unittest_small.cc',

chrome/common/chrome_switches.cc
@@ -152,10 +152,6 @@ const char kDeviceManagementUrl[]           = "device-management-url";
 // Triggers a pletora of diagnostic modes.
 const char kDiagnostics[]                   = "diagnostics";
 
-// Disables accelerated compositing.
-const char kDisableAcceleratedCompositing[] =
-    "disable-accelerated-compositing";
-
 // Disables the hardware acceleration of 3D CSS and animation.
 const char kDisableAcceleratedLayers[]      = "disable-accelerated-layers";
 
@@ -207,14 +203,8 @@ const char kDisableConnectBackupJobs[]      = "disable-connect-backup-jobs";
 // Disables the custom JumpList on Windows 7.
 const char kDisableCustomJumpList[]         = "disable-custom-jumplist";
 
-// Disables data transfer items.
-const char kDisableDataTransferItems[]      = "disable-data-transfer-items";
-
 // Browser flag to disable the web inspector for all renderers.
 const char kDisableDevTools[]               = "disable-dev-tools";
-
-// Disables device orientation events.
-const char kDisableDeviceOrientation[]      = "disable-device-orientation";
 
 // Disable extensions.
 const char kDisableExtensions[]             = "disable-extensions";
@@ -227,15 +217,6 @@ const char kDisableExtensionsFileAccessCheck[] =
 // Disables the sandbox for the built-in flash player.
 const char kDisableFlashSandbox[]           = "disable-flash-sandbox";
 
-// Suppresses support for the Geolocation javascript API.
-const char kDisableGeolocation[]            = "disable-geolocation";
-
-// Disable GL multisampling.
-const char kDisableGLMultisampling[]        = "disable-gl-multisampling";
-
-// Disable the GLSL translator.
-const char kDisableGLSLTranslator[]         = "disable-glsl-translator";
-
 // Suppresses hang monitor dialogs in renderer processes.  This may allow slow
 // unload handlers on a page to prevent the tab from closing, but the Task
 // Manager can be used to terminate the offending process in this case.
@@ -246,9 +227,6 @@ const char kDisableHistoryQuickProvider[]   = "disable-history-quick-provider";
 
 // Disable the use of the HistoryURLProvider for autocomplete results.
 const char kDisableHistoryURLProvider[]     = "disable-history-url-provider";
-
-// Disable the Indexed Database API.
-const char kDisableIndexedDatabase[]        = "disable-indexed-database";
 
 // Disables HTML5 Forms interactive validation.
 const char kDisableInteractiveFormValidation[] =
@@ -271,18 +249,6 @@ const char kDisableIPPooling[]              = "disable-ip-pooling";
 // Disable speculative TCP/IP preconnection.
 const char kDisablePreconnect[]             = "disable-preconnect";
 
-// Prevent Java from running.
-const char kDisableJava[]                   = "disable-java";
-
-// Don't execute JavaScript (browser JS like the new tab page still runs).
-const char kDisableJavaScript[]             = "disable-javascript";
-
-// Disable JavaScript I18N API.
-const char kDisableJavaScriptI18NAPI[]      = "disable-javascript-i18n-api";
-
-// Disable LocalStorage.
-const char kDisableLocalStorage[]           = "disable-local-storage";
-
 // Whether we should prevent the new tab page from showing the first run
 // notification.
 const char kDisableNewTabFirstRun[]         = "disable-new-tab-first-run";
@@ -304,17 +270,8 @@ const char kDisableRendererAccessibility[]  = "disable-renderer-accessibility";
 const char kDisableRestoreBackgroundContents[] =
     "disable-restore-background-contents";
 
-// Disable session storage.
-const char kDisableSessionStorage[]         = "disable-session-storage";
-
-// Enable shared workers. Functionality not yet complete.
-const char kDisableSharedWorkers[]          = "disable-shared-workers";
-
 // Disable site-specific tailoring to compatibility issues in WebKit.
 const char kDisableSiteSpecificQuirks[]     = "disable-site-specific-quirks";
-
-// Disables speech input.
-const char kDisableSpeechInput[]            = "disable-speech-input";
 
 // Disable False Start in SSL and TLS connections.
 const char kDisableSSLFalseStart[]          = "disable-ssl-false-start";
@@ -447,9 +404,6 @@ const char kEnableConnectBackupJobs[]       = "enable-connect-backup-jobs";
 // Enables web developers to create apps for Chrome without using crx packages.
 const char kEnableCrxlessWebApps[]          = "enable-crxless-web-apps";
 
-// Enables device motion events.
-const char kEnableDeviceMotion[]            = "enable-device-motion";
-
 // Enable DNS side checking of certificates. Still experimental, should only
 // be used by developers at the current time.
 const char kEnableDNSCertProvenanceChecking[] =
@@ -571,9 +525,6 @@ const char kEnableVideoLogging[]            = "enable-video-logging";
 // Order of the listed sub-arguments does not matter.
 const char kEnableWatchdog[]                = "enable-watchdog";
 
-// Enable web audio API.
-const char kEnableWebAudio[]                = "enable-webaudio";
-
 // Enables experimental features for Spellchecker. Right now, the first
 // experimental feature is auto spell correct, which corrects words which are
 // misppelled by typing the word with two consecutive letters swapped. The
@@ -690,18 +641,12 @@ const char kImport[]                        = "import";
 // setting encodes the file path. It may be used jointly with kImport.
 const char kImportFromFile[]                = "import-from-file";
 
-// Runs plugins inside the renderer process
-const char kInProcessPlugins[]              = "in-process-plugins";
-
 // Causes the browser to launch directly in incognito mode.
 const char kIncognito[]                     = "incognito";
 
 // URL to use for instant. If specified this overrides the url from the
 // TemplateURL.
 const char kInstantURL[]                    = "instant-url";
-
-// Specifies the flags passed to JS engine
-const char kJavaScriptFlags[]               = "js-flags";
 
 // Used for testing - keeps browser alive after last browser window closes.
 const char kKeepAliveForTest[]              = "keep-alive-for-test";
@@ -780,13 +725,6 @@ const char kNoExperiments[]                 = "no-experiments";
 // you're for some reason tempted to pass them both.
 const char kNoFirstRun[]                    = "no-first-run";
 
-// Support a separate switch that enables the v8 playback extension.
-// The extension causes javascript calls to Date.now() and Math.random()
-// to return consistent values, such that subsequent loads of the same
-// page will result in consistent js-generated data and XHR requests.
-// Pages may still be able to generate inconsistent data from plugins.
-const char kNoJsRandomness[]                = "no-js-randomness";
-
 // Don't send hyperlink auditing pings
 const char kNoPings[]                       = "no-pings";
 
@@ -825,10 +763,6 @@ const char kPackExtensionKey[]              = "pack-extension-key";
 
 // Specifies the path to the user data folder for the parent profile.
 const char kParentProfile[]                 = "parent-profile";
-
-// Read previously recorded data from the cache. Only cached data is read.
-// See kRecordMode.
-const char kPlaybackMode[]                  = "playback-mode";
 
 // Forces the PPAPI version of Flash (if it's being used) to run in the
 // renderer process rather than in a separate plugin process.
@@ -897,12 +831,6 @@ const char kProxyServer[]                   = "proxy-server";
 //
 // NOTE: This is only implemented for Views.
 const char kPurgeMemoryButton[]             = "purge-memory-button";
-
-// Chrome supports a playback and record mode.  Record mode saves *everything*
-// to the cache.  Playback mode reads data exclusively from the cache.  This
-// allows us to record a session into the cache and then replay it at will.
-// See also kPlaybackMode.
-const char kRecordMode[]                    = "record-mode";
 
 // Reload pages that have been killed when they are next focused by the user.
 const char kReloadKilledTabs[]              = "reload-killed-tabs";
@@ -973,10 +901,6 @@ const char kShowCompositedLayerTree[]       = "show-composited-layer-tree";
 
 // Draws a FPS indicator
 const char kShowFPSCounter[]                = "show-fps-counter";
-
-// Visibly render a border around paint rects in the web page to help debug
-// and study painting behavior.
-const char kShowPaintRects[]                = "show-paint-rects";
 
 // Change the DCHECKS to dump memory and continue instead of displaying error
 // dialog. This is valid only in Release mode when --enable-dcheck is

chrome/common/chrome_switches.h
@@ -58,7 +58,6 @@ extern const char kDebugDevTools[];
 extern const char kDebugPrint[];
 extern const char kDeviceManagementUrl[];
 extern const char kDiagnostics[];
-extern const char kDisableAcceleratedCompositing[];
 extern const char kDisableAcceleratedLayers[];
 extern const char kDisableAcceleratedVideo[];
 extern const char kDisableAltWinstation[];
@@ -70,37 +69,24 @@ extern const char kDisableBlockContentAnimation[];
 extern const char kDisableClientSidePhishingDetection[];
 extern const char kDisableConnectBackupJobs[];
 extern const char kDisableCustomJumpList[];
-extern const char kDisableDataTransferItems[];
 extern const char kDisableDevTools[];
-extern const char kDisableDeviceOrientation[];
 extern const char kDisableExtensionsFileAccessCheck[];
 extern const char kDisableExtensions[];
 extern const char kDisableFlashSandbox[];
-extern const char kDisableGLMultisampling[];
-extern const char kDisableGLSLTranslator[];
-extern const char kDisableGeolocation[];
 extern const char kDisableHangMonitor[];
 extern const char kDisableHistoryQuickProvider[];
 extern const char kDisableHistoryURLProvider[];
 extern const char kDisableInteractiveFormValidation[];
 extern const char kDisableInternalFlash[];
-extern const char kDisableIndexedDatabase[];
 extern const char kDisableIPv6[];
 extern const char kDisableIPPooling[];
-extern const char kDisableJava[];
-extern const char kDisableJavaScript[];
-extern const char kDisableJavaScriptI18NAPI[];
-extern const char kDisableLocalStorage[];
 extern const char kDisableNewTabFirstRun[];
 extern const char kDisablePreconnect[];
 extern const char kDisablePromptOnRepost[];
 extern const char kDisableRemoteFonts[];
 extern const char kDisableRendererAccessibility[];
 extern const char kDisableRestoreBackgroundContents[];
-extern const char kDisableSessionStorage[];
-extern const char kDisableSharedWorkers[];
 extern const char kDisableSiteSpecificQuirks[];
-extern const char kDisableSpeechInput[];
 extern const char kDisableSSLFalseStart[];
 extern const char kDisableSync[];
 extern const char kDisableSyncApps[];
@@ -136,7 +122,6 @@ extern const char kEnableCloudPrint[];
 extern const char kEnableCompositeToTexture[];
 extern const char kEnableConnectBackupJobs[];
 extern const char kEnableCrxlessWebApps[];
-extern const char kEnableDeviceMotion[];
 extern const char kEnableDNSCertProvenanceChecking[];
 extern const char kEnableDNSSECCerts[];
 extern const char kEnableExperimentalAppManifests[];
@@ -172,7 +157,6 @@ extern const char kEnableVerticalTabs[];
 extern const char kEnableVideoFullscreen[];
 extern const char kEnableVideoLogging[];
 extern const char kEnableWatchdog[];
-extern const char kEnableWebAudio[];
 extern const char kExperimentalSpellcheckerFeatures[];
 extern const char kExplicitlyAllowedPorts[];
 extern const char kExtensionsUpdateFrequency[];
@@ -198,10 +182,8 @@ extern const char kHostResolverRules[];
 extern const char kIgnoreGpuBlacklist[];
 extern const char kImport[];
 extern const char kImportFromFile[];
-extern const char kInProcessPlugins[];
 extern const char kIncognito[];
 extern const char kInstantURL[];
-extern const char kJavaScriptFlags[];
 extern const char kKeepAliveForTest[];
 extern const char kLoadExtension[];
 extern const char kLoadOpencryptoki[];
@@ -223,7 +205,6 @@ extern const char kNoDefaultBrowserCheck[];
 extern const char kNoEvents[];
 extern const char kNoExperiments[];
 extern const char kNoFirstRun[];
-extern const char kNoJsRandomness[];
 extern const char kNoProxyServer[];
 extern const char kNoPings[];
 extern const char kNoServiceAutorun[];
@@ -235,7 +216,6 @@ extern const char kOrganicInstall[];
 extern const char kPackExtension[];
 extern const char kPackExtensionKey[];
 extern const char kParentProfile[];
-extern const char kPlaybackMode[];
 extern const char kPpapiFlashInProcess[];
 extern const char kPrerender[];
 extern const char kPrerenderSwitchValueAuto[];
@@ -252,7 +232,6 @@ extern const char kProxyBypassList[];
 extern const char kProxyPacUrl[];
 extern const char kProxyServer[];
 extern const char kPurgeMemoryButton[];
-extern const char kRecordMode[];
 extern const char kReloadKilledTabs[];
 extern const char kRemoteDebuggingPort[];
 extern const char kRemoteShellPort[];
@@ -269,7 +248,6 @@ extern const char kShowCompositedLayerBorders[];
 extern const char kShowCompositedLayerTree[];
 extern const char kShowFPSCounter[];
 extern const char kShowIcons[];
-extern const char kShowPaintRects[];
 extern const char kSilentDumpOnDCHECK[];
 extern const char kSimpleDataSource[];
 extern const char kStartMaximized[];

chrome/common/render_messages.h
@@ -174,11 +174,6 @@ IPC_MESSAGE_CONTROL1(ViewMsg_VisitedLink_Add, std::vector<uint64>)
 // re-calculated.
 IPC_MESSAGE_CONTROL0(ViewMsg_VisitedLink_Reset)
 
-// Sent when user prompting is required before a ViewHostMsg_GetCookies
-// message can complete.  This message indicates that the renderer should
-// pump messages while waiting for cookies.
-IPC_MESSAGE_CONTROL0(ViewMsg_SignalCookiePromptEvent)
-
 // Set the content settings for a particular url that the renderer is in the
 // process of loading.  This will be stored, to be used if the load commits
 // and ignored otherwise.
@@ -194,11 +189,6 @@ IPC_MESSAGE_CONTROL2(ViewMsg_SetContentSettingsForCurrentURL,
 
 // Install the first missing pluign.
 IPC_MESSAGE_ROUTED0(ViewMsg_InstallMissingPlugin)
-
-// Tells the renderer to empty its plugin list cache, optional reloading
-// pages containing plugins.
-IPC_MESSAGE_CONTROL1(ViewMsg_PurgePluginListCache,
-                     bool /* reload_pages */)
 
 // Tells the render view to load all blocked plugins.
 IPC_MESSAGE_ROUTED0(ViewMsg_LoadBlockedPlugins)

chrome/renderer/chrome_content_renderer_client.cc
@@ -9,8 +9,11 @@
 #include "base/command_line.h"
 #include "base/message_loop.h"
 #include "base/metrics/histogram.h"
+#include "base/path_service.h"
+#include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/common/child_process_logging.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_constants.h"
@@ -25,7 +28,8 @@
 #include "chrome/renderer/automation/automation_renderer_helper.h"
 #include "chrome/renderer/automation/dom_automation_v8_extension.h"
 #include "chrome/renderer/blocked_plugin.h"
-#include "chrome/renderer/chrome_render_observer.h"
+#include "chrome/renderer/chrome_render_process_observer.h"
+#include "chrome/renderer/chrome_render_view_observer.h"
 #include "chrome/renderer/devtools_agent.h"
 #include "chrome/renderer/devtools_agent_filter.h"
 #include "chrome/renderer/extensions/bindings_utils.h"
@@ -64,6 +68,7 @@
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginParams.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityPolicy.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURL.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURLError.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURLRequest.h"
@@ -83,6 +88,7 @@ using WebKit::WebCache;
 using WebKit::WebFrame;
 using WebKit::WebPlugin;
 using WebKit::WebPluginParams;
+using WebKit::WebSecurityPolicy;
 using WebKit::WebString;
 using WebKit::WebURLError;
 using WebKit::WebURLRequest;
@@ -216,6 +222,7 @@ ChromeContentRendererClient::~ChromeContentRendererClient() {
 }
 
 void ChromeContentRendererClient::RenderThreadStarted() {
+  chrome_observer_.reset(new ChromeRenderProcessObserver());
   extension_dispatcher_.reset(new ExtensionDispatcher());
   histogram_snapshots_.reset(new RendererHistogramSnapshots());
   net_predictor_.reset(new RendererNetPredictor());
@@ -229,6 +236,7 @@ void ChromeContentRendererClient::RenderThreadStarted() {
   thread->AddFilter(new SuicideOnChannelErrorFilter());
 #endif
 
+  thread->AddObserver(chrome_observer_.get());
   thread->AddObserver(extension_dispatcher_.get());
   thread->AddObserver(histogram_snapshots_.get());
   thread->AddObserver(phishing_classifier_.get());
@@ -249,6 +257,16 @@ void ChromeContentRendererClient::RenderThreadStarted() {
   }
 
   thread->resource_dispatcher()->set_observer(new RenderResourceObserver());
+
+  // chrome: pages should not be accessible by normal content, and should
+  // also be unable to script anything but themselves (to help limit the damage
+  // that a corrupt chrome: page could cause).
+  WebString chrome_ui_scheme(ASCIIToUTF16(chrome::kChromeUIScheme));
+  WebSecurityPolicy::registerURLSchemeAsDisplayIsolated(chrome_ui_scheme);
+
+  // chrome-extension: resources shouldn't trigger insecure content warnings.
+  WebString extension_scheme(ASCIIToUTF16(chrome::kExtensionScheme));
+  WebSecurityPolicy::registerURLSchemeAsSecure(extension_scheme);
 }
 
 void ChromeContentRendererClient::RenderViewCreated(RenderView* render_view) {
@@ -281,7 +299,7 @@ void ChromeContentRendererClient::RenderViewCreated(RenderView* render_view) {
   page_click_tracker->AddListener(autofill_agent);
 
   TranslateHelper* translate = new TranslateHelper(render_view, autofill_agent);
-  new ChromeRenderObserver(render_view, translate, phishing_classifier);
+  new ChromeRenderViewObserver(render_view, translate, phishing_classifier);
 
   // Used only for testing/automation.
   if (CommandLine::ForCurrentProcess()->HasSwitch(
@@ -545,6 +563,21 @@ bool ChromeContentRendererClient::WillSendRequest(WebKit::WebFrame* frame,
   }
 
   return false;
+}
+
+FilePath ChromeContentRendererClient::GetMediaLibraryPath() {
+  FilePath rv;
+  PathService::Get(chrome::DIR_MEDIA_LIBS, &rv);
+  return rv;
+}
+
+bool ChromeContentRendererClient::ShouldPumpEventsDuringCookieMessage() {
+  // We only need to pump events for chrome frame processes as the
+  // cookie policy is controlled by the host browser (IE). If the
+  // policy is set to prompt then the host would put up UI which
+  // would require plugins if any to also pump to ensure that we
+  // don't have a deadlock.
+  return CommandLine::ForCurrentProcess()->HasSwitch(switches::kChromeFrame);
 }
 
 void ChromeContentRendererClient::DidCreateScriptContext(WebFrame* frame) {

chrome/renderer/chrome_content_renderer_client.h
@@ -9,6 +9,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "content/renderer/content_renderer_client.h"
 
+class ChromeRenderProcessObserver;
 class ExtensionDispatcher;
 class RendererHistogramSnapshots;
 class RendererNetPredictor;
@@ -53,6 +54,8 @@ class ChromeContentRendererClient : public content::ContentRendererClient {
   virtual bool WillSendRequest(WebKit::WebFrame* frame,
                                const GURL& url,
                                GURL* new_url);
+  virtual FilePath GetMediaLibraryPath();
+  virtual bool ShouldPumpEventsDuringCookieMessage();
   virtual void DidCreateScriptContext(WebKit::WebFrame* frame);
   virtual void DidDestroyScriptContext(WebKit::WebFrame* frame);
   virtual void DidCreateIsolatedScriptContext(WebKit::WebFrame* frame);
@@ -84,6 +87,7 @@ class ChromeContentRendererClient : public content::ContentRendererClient {
   // as window.opener) if the window navigates back.
   bool CrossesExtensionExtents(WebKit::WebFrame* frame, const GURL& new_url);
 
+  scoped_ptr<ChromeRenderProcessObserver> chrome_observer_;
   scoped_ptr<ExtensionDispatcher> extension_dispatcher_;
   scoped_ptr<RendererHistogramSnapshots> histogram_snapshots_;
   scoped_ptr<RendererNetPredictor> net_predictor_;

commit 0265ef1c4c74cb958ffd6edc3c2b520562453e36

Get tests working with single-process mode.
single-process mode appears to have been broken by r81851.

BUG=NONE
TEST=run browser_test with --single-process

Review URL: http://codereview.chromium.org/6902122

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83521 0039d316-1c4b-4281-b951-d872f2087c98

chrome/test/in_process_browser_test.cc
@@ -36,6 +36,7 @@
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/common/main_function_params.h"
 #include "content/common/notification_type.h"
+#include "content/renderer/content_renderer_client.h"
 #include "net/base/mock_host_resolver.h"
 #include "net/test/test_server.h"
 #include "sandbox/src/dep.h"
@@ -127,9 +128,14 @@ void InProcessBrowserTest::SetUp() {
   // Add command line arguments that are used by all InProcessBrowserTests.
   PrepareTestCommandLine(command_line);
 
-  // Single-process mode is not set in BrowserMain, so process it explicitly.
-  if (command_line->HasSwitch(switches::kSingleProcess))
+  // Single-process mode is not set in BrowserMain, so process it explicitly,
+  // and set up renderer.
+  if (command_line->HasSwitch(switches::kSingleProcess)) {
     RenderProcessHost::set_run_renderer_in_process(true);
+    single_process_renderer_client_.reset(new content::ContentRendererClient);
+    content::GetContentClient()->set_renderer(
+        single_process_renderer_client_.get());
+  }
 
 #if defined(OS_CHROMEOS)
   // Make sure that the log directory exists.

chrome/test/in_process_browser_test.h
@@ -22,6 +22,10 @@ class Browser;
 class CommandLine;
 class Profile;
 
+namespace content {
+class ContentRendererClient;
+}
+
 namespace net {
 class RuleBasedHostResolverProc;
 }
@@ -188,6 +192,9 @@ class InProcessBrowserTest : public testing::Test {
 
   // Testing server, started on demand.
   scoped_ptr<net::TestServer> test_server_;
+
+  // ContentRendererClient when running in single-process mode.
+  scoped_ptr<content::ContentRendererClient> single_process_renderer_client_;
 
   // Whether this test requires the browser windows to be shown (interactive
   // tests for example need the windows shown).