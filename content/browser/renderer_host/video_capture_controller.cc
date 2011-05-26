OpenHarmony-TPC/chromium_arkweb

commit c27aec01ed3a4baac80f3311af7b254ad23fa0bf

VideoCaptureHost
This is the patch containing code necessary for communicating with the VideoCaptureMessageFilter from the browser process and transferring Transport Dibs filled with video frames in I420 color format. It also contain code for color converting video frames to I420.

Color conversion has been tested on Linux and Windows by using video_capture_host_unittest and dumping I420 frames to file. See #define DUMP_VIDEO #define TEST_REAL_CAPTURE_DEVICE.

Patch by perk@google.com:
http://codereview.chromium.org/7002027/

BUG=none
TEST=try bots

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86927 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/video_capture_controller.cc
@@ -1,0 +1,225 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/video_capture_controller.h"
+
+#include "base/logging.h"
+#include "base/stl_util-inl.h"
+#include "content/browser/browser_thread.h"
+#include "content/browser/media_stream/video_capture_manager.h"
+#include "media/base/yuv_convert.h"
+
+#if defined(OS_WIN)
+#include "content/common/section_util_win.h"
+#endif
+
+// The number of TransportDIBs VideoCaptureController allocate.
+static const size_t kNoOfDIBS = 3;
+
+VideoCaptureController::VideoCaptureController(
+    ControllerId id,
+    base::ProcessHandle render_process,
+    EventHandler* event_handler)
+    : render_handle_(render_process),
+      report_ready_to_delete_(false),
+      event_handler_(event_handler),
+      id_(id) {}
+
+VideoCaptureController::~VideoCaptureController() {
+  // Delete all TransportDIBs.
+  STLDeleteContainerPairSecondPointers(owned_dibs_.begin(),
+                                       owned_dibs_.end());
+}
+
+void VideoCaptureController::StartCapture(
+    const media::VideoCaptureParams& params) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  params_ = params;
+  media_stream::VideoCaptureManager* manager =
+      media_stream::VideoCaptureManager::Get();
+  // Order the manager to start the actual capture.
+  manager->Start(params, this);
+}
+
+void VideoCaptureController::StopCapture(Task* stopped_task) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  media_stream::VideoCaptureManager* manager =
+      media_stream::VideoCaptureManager::Get();
+  manager->Stop(params_.session_id,
+                NewRunnableMethod(this,
+                                  &VideoCaptureController::OnDeviceStopped,
+                                  stopped_task));
+}
+
+void VideoCaptureController::ReturnTransportDIB(TransportDIB::Handle handle) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  bool ready_to_delete;
+  {
+    base::AutoLock lock(lock_);
+    free_dibs_.push_back(handle);
+    ready_to_delete = (free_dibs_.size() == owned_dibs_.size());
+  }
+  if (report_ready_to_delete_ && ready_to_delete) {
+    event_handler_->OnReadyToDelete(id_);
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Implements VideoCaptureDevice::EventHandler.
+// OnIncomingCapturedFrame is called the thread running the capture device.
+// I.e.- DirectShow thread on windows and v4l2_thread on Linux.
+void VideoCaptureController::OnIncomingCapturedFrame(const uint8* data,
+                                                     int length,
+                                                     base::Time timestamp) {
+  TransportDIB::Handle handle;
+  TransportDIB* dib;
+  // Check if there is a TransportDIB to fill.
+  bool buffer_exist = false;
+  {
+    base::AutoLock lock(lock_);
+    if (!report_ready_to_delete_ && free_dibs_.size() > 0) {
+      handle = free_dibs_.back();
+      free_dibs_.pop_back();
+      DIBMap::iterator it = owned_dibs_.find(handle);
+      if (it != owned_dibs_.end()) {
+        dib = it->second;
+        buffer_exist = true;
+      }
+    }
+  }
+
+  if (!buffer_exist) {
+    return;
+  }
+
+  if (!dib->Map()) {
+    VLOG(1) << "OnIncomingCapturedFrame - Failed to map handle.";
+    base::AutoLock lock(lock_);
+    free_dibs_.push_back(handle);
+    return;
+  }
+  uint8* target = static_cast<uint8*>(dib->memory());
+  CHECK(dib->size() >= static_cast<size_t> (frame_info_.width *
+                                            frame_info_.height * 3) /
+                                            2);
+
+  // Do color conversion from the camera format to I420.
+  switch (frame_info_.color) {
+    case media::VideoCaptureDevice::kColorUnknown:  // Color format not set.
+      break;
+    case media::VideoCaptureDevice::kI420: {
+      memcpy(target, data, (frame_info_.width * frame_info_.height * 3) / 2);
+      break;
+    }
+    case media::VideoCaptureDevice::kYUY2: {
+      uint8* yplane = target;
+      uint8* uplane = target + frame_info_.width * frame_info_.height;
+      uint8* vplane = uplane + (frame_info_.width * frame_info_.height) / 4;
+      media::ConvertYUY2ToYUV(data, yplane, uplane, vplane, frame_info_.width,
+                              frame_info_.height);
+      break;
+    }
+    case media::VideoCaptureDevice::kRGB24: {
+#if defined(OS_WIN)  // RGB on Windows start at the bottom line.
+      uint8* yplane = target;
+      uint8* uplane = target + frame_info_.width * frame_info_.height;
+      uint8* vplane = uplane + (frame_info_.width * frame_info_.height) / 4;
+      int ystride = frame_info_.width;
+      int uvstride = frame_info_.width / 2;
+      int rgb_stride = - 3 * frame_info_.width;
+      const uint8* rgb_src = data + 3 * frame_info_.width *
+          (frame_info_.height -1);
+#else
+      uint8* yplane = target;
+      uint8* uplane = target + frame_info_.width * frame_info_.height;
+      uint8* vplane = uplane + (frame_info_.width * frame_info_.height) / 4;
+      int ystride = frame_info_.width;
+      int uvstride = frame_info_.width / 2;
+      int rgb_stride = 3 * frame_info_.width;
+      const uint8* rgb_src = data;
+#endif
+      media::ConvertRGB24ToYUV(rgb_src, yplane, uplane, vplane,
+                               frame_info_.width, frame_info_.height,
+                               rgb_stride, ystride, uvstride);
+      break;
+    }
+    case media::VideoCaptureDevice::kARGB: {
+      uint8* yplane = target;
+      uint8* uplane = target +  frame_info_.width * frame_info_.height;
+      uint8* vplane = uplane +  (frame_info_.width * frame_info_.height) / 4;
+      media::ConvertRGB32ToYUV(data, yplane, uplane, vplane, frame_info_.width,
+                               frame_info_.height, frame_info_.width * 4,
+                               frame_info_.width, frame_info_.width / 2);
+      break;
+    }
+    default:
+      NOTREACHED();
+  }
+
+  event_handler_->OnBufferReady(id_, handle, timestamp);
+}
+
+void VideoCaptureController::OnError() {
+  event_handler_->OnError(id_);
+}
+
+void VideoCaptureController::OnFrameInfo(
+    const media::VideoCaptureDevice::Capability& info) {
+  DCHECK(owned_dibs_.empty());
+  bool frames_created = true;
+  const size_t needed_size = (info.width * info.height * 3) / 2;
+  for (size_t i = 0; i < kNoOfDIBS; ++i) {
+    TransportDIB* dib = TransportDIB::Create(needed_size, i);
+    if (!dib) {
+      frames_created = false;
+      break;
+    }
+    // Lock needed since the buffers are used in OnIncomingFrame
+    // and we need to use it there in order to avoid memcpy of complete frames.
+    base::AutoLock lock(lock_);
+#if defined(OS_WIN)
+    // On Windows we need to get a handle the can be used in the render process.
+    TransportDIB::Handle handle = chrome::GetSectionForProcess(
+        dib->handle(), render_handle_, false);
+#else
+    TransportDIB::Handle handle = dib->handle();
+#endif
+    owned_dibs_.insert(std::make_pair(handle, dib));
+    free_dibs_.push_back(handle);
+  }
+  frame_info_= info;
+
+  // Check that all DIBs where created successfully.
+  if (!frames_created) {
+    event_handler_->OnError(id_);
+  }
+  event_handler_->OnFrameInfo(id_, info.width, info.height,
+                              info.frame_rate);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Called by VideoCaptureManager when a device have been stopped.
+// This will report to the event handler that this object is ready to be deleted
+// if all DIBS have been returned.
+void VideoCaptureController::OnDeviceStopped(Task* stopped_task) {
+  bool ready_to_delete_now;
+
+  // Set flag to indicate we need to report when all DIBs have been returned.
+  report_ready_to_delete_ = true;
+  {
+    base::AutoLock lock(lock_);
+    ready_to_delete_now = (free_dibs_.size() == owned_dibs_.size());
+  }
+
+  if (ready_to_delete_now) {
+    event_handler_->OnReadyToDelete(id_);
+  }
+  if (stopped_task) {
+    stopped_task->Run();
+    delete stopped_task;
+  }
+}

chrome/chrome_tests.gypi
@@ -1930,6 +1930,7 @@
         '../content/browser/renderer_host/render_widget_host_unittest.cc',
         '../content/browser/renderer_host/resource_dispatcher_host_unittest.cc',
         '../content/browser/renderer_host/resource_queue_unittest.cc',
+        '../content/browser/renderer_host/video_capture_host_unittest.cc',
         '../content/browser/resolve_proxy_msg_helper_unittest.cc',
         '../content/browser/site_instance_unittest.cc',
         '../content/browser/speech/endpointer/endpointer_unittest.cc',

content/browser/renderer_host/video_capture_controller.h
@@ -1,0 +1,116 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// VideoCaptureController is the glue between VideoCaptureHost,
+// VideoCaptureManager and VideoCaptureDevice.
+// It provides functions for VideoCaptureHost to start a VideoCaptureDevice and
+// is responsible for keeping track of TransportDIBs and filling them with I420
+// video frames for IPC communication between VideoCaptureHost and
+// VideoCaptureMessageFilter.
+// It implements media::VideoCaptureDevice::EventHandler to get video frames
+// from a VideoCaptureDevice object and do color conversion straight into the
+// TransportDIBs to avoid a memory copy.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_CONTROLLER_H_
+#define CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_CONTROLLER_H_
+
+#include <list>
+#include <map>
+
+#include "base/memory/ref_counted.h"
+#include "base/process.h"
+#include "base/synchronization/lock.h"
+#include "base/task.h"
+#include "media/video/capture/video_capture_device.h"
+#include "media/video/capture/video_capture_types.h"
+#include "ui/gfx/surface/transport_dib.h"
+
+class VideoCaptureController
+    : public base::RefCountedThreadSafe<VideoCaptureController>,
+      public media::VideoCaptureDevice::EventHandler {
+ public:
+  // Id used for identifying an object of VideoCaptureController.
+  typedef std::pair<int32, int> ControllerId;
+  class EventHandler {
+   public:
+    // An Error have occurred in the VideoCaptureDevice.
+    virtual void OnError(ControllerId id) = 0;
+
+    // An TransportDIB have been filled with I420 video.
+    virtual void OnBufferReady(ControllerId id,
+                               TransportDIB::Handle handle,
+                               base::Time timestamp) = 0;
+
+    // The frame resolution the VideoCaptureDevice capture video in.
+    virtual void OnFrameInfo(ControllerId id,
+                             int width,
+                             int height,
+                             int frame_rate) = 0;
+
+    // Report that this object can be deleted.
+    virtual void OnReadyToDelete(ControllerId id) = 0;
+
+   protected:
+    virtual ~EventHandler() {}
+  };
+
+  VideoCaptureController(ControllerId id,
+                         base::ProcessHandle render_process,
+                         EventHandler* event_handler);
+  virtual ~VideoCaptureController();
+
+  // Starts video capturing and tries to use the resolution specified in
+  // params.
+  // When capturing has started EventHandler::OnFrameInfo is called with
+  // resolution that best matches the requested that the video capture device
+  // support.
+  void StartCapture(const media::VideoCaptureParams& params);
+
+  // Stop video capture.
+  // When the capture is stopped and all TransportDIBS have been returned
+  // EventHandler::OnReadyToDelete will be called.
+  // stopped_task may be null but it can be used to get a notification when the
+  // device is stopped.
+  void StopCapture(Task* stopped_task);
+
+  // Return a DIB previously given in EventHandler::OnBufferReady.
+  void ReturnTransportDIB(TransportDIB::Handle handle);
+
+  // Implement media::VideoCaptureDevice::EventHandler.
+  virtual void OnIncomingCapturedFrame(const uint8* data,
+                                       int length,
+                                       base::Time timestamp);
+  virtual void OnError();
+  virtual void OnFrameInfo(const media::VideoCaptureDevice::Capability& info);
+
+ private:
+  // Called by VideoCaptureManager when a device have been stopped.
+  void OnDeviceStopped(Task* stopped_task);
+
+  // Lock to protect free_dibs_ and owned_dibs_.
+  base::Lock lock_;
+  // Handle to the render process that will receive the DIBs.
+  base::ProcessHandle render_handle_;
+  bool report_ready_to_delete_;
+  typedef std::list<TransportDIB::Handle> DIBHandleList;
+  typedef std::map<TransportDIB::Handle, TransportDIB*> DIBMap;
+
+  // Free DIBS that can be filled with video frames.
+  DIBHandleList free_dibs_;
+
+  // All DIBS created by this object.
+  DIBMap owned_dibs_;
+  EventHandler* event_handler_;
+
+  // The parameter that was requested when starting the capture device.
+  media::VideoCaptureParams params_;
+
+  // Id used for identifying this object.
+  ControllerId id_;
+  media::VideoCaptureDevice::Capability frame_info_;
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(VideoCaptureController);
+};
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_CONTROLLER_H_

content/browser/renderer_host/video_capture_host.cc
@@ -1,0 +1,186 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/video_capture_host.h"
+
+#include "base/memory/scoped_ptr.h"
+#include "base/stl_util-inl.h"
+#include "content/common/video_capture_messages.h"
+
+VideoCaptureHost::VideoCaptureHost() {}
+
+VideoCaptureHost::~VideoCaptureHost() {}
+
+void VideoCaptureHost::OnChannelClosing() {
+  BrowserMessageFilter::OnChannelClosing();
+
+  // Since the IPC channel is gone, close all requested VideCaptureDevices.
+  for (EntryMap::iterator it = entries_.begin(); it != entries_.end(); it++) {
+    VideoCaptureController* controller = it->second;
+    // Since the channel is closing we need a task to make sure VideoCaptureHost
+    // is not deleted before VideoCaptureController.
+    controller->StopCapture(
+        NewRunnableMethod(this, &VideoCaptureHost::OnReadyToDelete, it->first));
+  }
+}
+
+void VideoCaptureHost::OnDestruct() const {
+  BrowserThread::DeleteOnIOThread::Destruct(this);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Implements VideoCaptureController::EventHandler.
+void VideoCaptureHost::OnError(VideoCaptureController::ControllerId id) {
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      NewRunnableMethod(this, &VideoCaptureHost::DoHandleError, id.first,
+                        id.second));
+}
+
+void VideoCaptureHost::OnBufferReady(
+    VideoCaptureController::ControllerId id,
+    TransportDIB::Handle handle,
+    base::Time timestamp) {
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      NewRunnableMethod(this, &VideoCaptureHost::DoSendFilledBuffer, id.first,
+                        id.second, handle, timestamp));
+}
+
+void VideoCaptureHost::OnFrameInfo(VideoCaptureController::ControllerId id,
+                                   int width,
+                                   int height,
+                                   int frame_per_second) {
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      NewRunnableMethod(this, &VideoCaptureHost::DoSendFrameInfo, id.first,
+                        id.second, width, height, frame_per_second));
+}
+
+void VideoCaptureHost::OnReadyToDelete(
+    VideoCaptureController::ControllerId id) {
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      NewRunnableMethod(this, &VideoCaptureHost::DoDeleteVideoCaptureController,
+                        id));
+}
+
+void VideoCaptureHost::DoSendFilledBuffer(int32 routing_id,
+                                          int device_id,
+                                          TransportDIB::Handle handle,
+                                          base::Time timestamp) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  Send(new VideoCaptureMsg_BufferReady(routing_id, device_id, handle,
+                                       timestamp));
+}
+
+void VideoCaptureHost::DoHandleError(int32 routing_id, int device_id) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  Send(new VideoCaptureMsg_StateChanged(routing_id, device_id,
+                                        media::VideoCapture::kError));
+
+  VideoCaptureController::ControllerId id(routing_id, device_id);
+  EntryMap::iterator it = entries_.find(id);
+  if (it != entries_.end()) {
+    VideoCaptureController* controller = it->second;
+    controller->StopCapture(NULL);
+  }
+}
+
+void VideoCaptureHost::DoSendFrameInfo(int32 routing_id,
+                                       int device_id,
+                                       int width,
+                                       int height,
+                                       int frame_per_second) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  media::VideoCaptureParams params;
+  params.width = width;
+  params.height = height;
+  params.frame_per_second = frame_per_second;
+  Send(new VideoCaptureMsg_DeviceInfo(routing_id, device_id, params));
+  Send(new VideoCaptureMsg_StateChanged(routing_id, device_id,
+                                        media::VideoCapture::kStarted));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// IPC Messages handler.
+bool VideoCaptureHost::OnMessageReceived(const IPC::Message& message,
+                                         bool* message_was_ok) {
+  bool handled = true;
+  IPC_BEGIN_MESSAGE_MAP_EX(VideoCaptureHost, message, *message_was_ok)
+    IPC_MESSAGE_HANDLER(VideoCaptureHostMsg_Start, OnStartCapture)
+    IPC_MESSAGE_HANDLER(VideoCaptureHostMsg_Pause, OnPauseCapture)
+    IPC_MESSAGE_HANDLER(VideoCaptureHostMsg_Stop, OnStopCapture)
+    IPC_MESSAGE_HANDLER(VideoCaptureHostMsg_BufferReady, OnReceiveEmptyBuffer)
+    IPC_MESSAGE_UNHANDLED(handled = false)
+  IPC_END_MESSAGE_MAP_EX()
+
+  return handled;
+}
+
+void VideoCaptureHost::OnStartCapture(const IPC::Message& msg, int device_id,
+                                      const media::VideoCaptureParams& params) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  VideoCaptureController::ControllerId controller_id(msg.routing_id(),
+                                                     device_id);
+
+  DCHECK(entries_.find(controller_id) == entries_.end());
+
+  scoped_refptr<VideoCaptureController> controller =
+      new VideoCaptureController(controller_id, peer_handle(), this);
+  entries_.insert(std::make_pair(controller_id, controller));
+  controller->StartCapture(params);
+}
+
+void VideoCaptureHost::OnStopCapture(const IPC::Message& msg, int device_id) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  VideoCaptureController::ControllerId controller_id(msg.routing_id(),
+                                                     device_id);
+  EntryMap::iterator it = entries_.find(controller_id);
+  if (it != entries_.end()) {
+    scoped_refptr<VideoCaptureController> controller = it->second;
+    controller->StopCapture(NULL);
+  } else {
+    // It does not exist so it must have been stopped already.
+    Send(new VideoCaptureMsg_StateChanged(msg.routing_id(), device_id,
+                                          media::VideoCapture::kStopped));
+  }
+}
+
+void VideoCaptureHost::OnPauseCapture(const IPC::Message& msg, int device_id) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  // Not used.
+  Send(new VideoCaptureMsg_StateChanged(msg.routing_id(), device_id,
+                                        media::VideoCapture::kError));
+}
+
+void VideoCaptureHost::OnReceiveEmptyBuffer(const IPC::Message& msg,
+                                            int device_id,
+                                            TransportDIB::Handle handle) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  VideoCaptureController::ControllerId controller_id(msg.routing_id(),
+                                                     device_id);
+  EntryMap::iterator it = entries_.find(controller_id);
+  if (it != entries_.end()) {
+    scoped_refptr<VideoCaptureController> controller = it->second;
+    controller->ReturnTransportDIB(handle);
+  }
+}
+
+void VideoCaptureHost::DoDeleteVideoCaptureController(
+    VideoCaptureController::ControllerId id) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+  // Report that the device have successfully been stopped.
+  Send(new VideoCaptureMsg_StateChanged(id.first, id.second,
+                                        media::VideoCapture::kStopped));
+  entries_.erase(id);
+}

content/browser/renderer_host/video_capture_host.h
@@ -1,0 +1,127 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// VideoCaptureHost serves video capture related messages from
+// VideCaptureMessageFilter which lives inside the render process.
+//
+// This class is owned by BrowserRenderProcessHost, and instantiated on UI
+// thread, but all other operations and method calls happen on IO thread.
+//
+// Here's an example of a typical IPC dialog for video capture:
+//
+//   Renderer                          VideoCaptureHost
+//      |                                     |
+//      |  VideoCaptureHostMsg_Start >        |
+//      |  < VideoCaptureMsg_DeviceInfo       |
+//      |                                     |
+//      | < VideoCaptureMsg_StateChanged      |
+//      |        (kStarted)                   |
+//      | < VideoCaptureMsg_BufferReady       |
+//      |             ...                     |
+//      | < VideoCaptureMsg_BufferReady       |
+//      |             ...                     |
+//      | VideoCaptureHostMsg_BufferReady >   |
+//      | VideoCaptureHostMsg_BufferReady >   |
+//      |                                     |
+//      |             ...                     |
+//      |                                     |
+//      | < VideoCaptureMsg_BufferReady       |
+//      |  VideoCaptureHostMsg_Stop >         |
+//      | VideoCaptureHostMsg_BufferReady >   |
+//      | < VideoCaptureMsg_StateChanged      |
+//      |         (kStopped)                  |
+//      v                                     v
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_HOST_H_
+#define CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_HOST_H_
+
+#include <map>
+
+#include "base/memory/ref_counted.h"
+#include "content/browser/browser_message_filter.h"
+#include "content/browser/renderer_host/video_capture_controller.h"
+#include "ipc/ipc_message.h"
+
+class VideoCaptureHost : public BrowserMessageFilter,
+                         public VideoCaptureController::EventHandler {
+ public:
+  VideoCaptureHost();
+
+  // BrowserMessageFilter implementation.
+  virtual void OnChannelClosing();
+  virtual void OnDestruct() const;
+  virtual bool OnMessageReceived(const IPC::Message& message,
+                                 bool* message_was_ok);
+
+  // VideoCaptureController::EventHandler implementation.
+  virtual void OnError(VideoCaptureController::ControllerId id);
+  virtual void OnBufferReady(VideoCaptureController::ControllerId id,
+                             TransportDIB::Handle handle,
+                             base::Time timestamp);
+  virtual void OnFrameInfo(VideoCaptureController::ControllerId id,
+                           int width,
+                           int height,
+                           int frame_per_second);
+  virtual void OnReadyToDelete(VideoCaptureController::ControllerId id);
+
+ private:
+  friend class BrowserThread;
+  friend class DeleteTask<VideoCaptureHost>;
+  friend class MockVideoCaptureHost;
+  friend class VideoCaptureHostTest;
+
+  virtual ~VideoCaptureHost();
+
+  // IPC message: Start capture on the VideoCaptureDevice referenced by
+  // VideoCaptureParams::session_id. device_id is an id created by
+  // VideCaptureMessageFilter to identify a session
+  // between a VideCaptureMessageFilter and a VideoCaptureHost.
+  void OnStartCapture(const IPC::Message& msg, int device_id,
+                      const media::VideoCaptureParams& params);
+
+  // IPC message: Stop capture on device referenced by device_id.
+  void OnStopCapture(const IPC::Message& msg, int device_id);
+
+  // IPC message: Pause capture on device referenced by device_id.
+  void OnPauseCapture(const IPC::Message& msg, int device_id);
+
+  // IPC message: Receive an empty buffer from renderer. Send it to device
+  // referenced by |device_id|.
+  void OnReceiveEmptyBuffer(const IPC::Message& msg,
+                            int device_id,
+                            TransportDIB::Handle handle);
+
+
+  // Called on the IO thread when VideoCaptureController have
+  // reported that all DIBs have been returned.
+  void DoDeleteVideoCaptureController(VideoCaptureController::ControllerId id);
+
+  // Send a filled buffer to the VideoCaptureMessageFilter.
+  void DoSendFilledBuffer(int32 routing_id,
+                          int device_id,
+                          TransportDIB::Handle handle,
+                          base::Time timestamp);
+
+  // Send a information about frame resolution and frame rate
+  // to the VideoCaptureMessageFilter.
+  void DoSendFrameInfo(int32 routing_id,
+                       int device_id,
+                       int width,
+                       int height,
+                       int frame_per_second);
+
+  // Handle error coming from VideoCaptureDevice.
+  void DoHandleError(int32 routing_id, int device_id);
+
+  typedef std::map<VideoCaptureController::ControllerId,
+                   scoped_refptr<VideoCaptureController> >EntryMap;
+
+  // A map of VideoCaptureController::ControllerId to VideoCaptureController
+  // objects that is currently active.
+  EntryMap entries_;
+
+  DISALLOW_COPY_AND_ASSIGN(VideoCaptureHost);
+};
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_VIDEO_CAPTURE_HOST_H_

commit 1042bad04f6beb83f03a9cb50e78766066d06f34

Fix ARM build break introduced in r86927.
Review URL: http://codereview.chromium.org/7076034

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86933 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/renderer_host/video_capture_controller.cc
@@ -76,7 +76,7 @@ void VideoCaptureController::OnIncomingCapturedFrame(const uint8* data,
                                                      int length,
                                                      base::Time timestamp) {
   TransportDIB::Handle handle;
-  TransportDIB* dib;
+  TransportDIB* dib = NULL;
   // Check if there is a TransportDIB to fill.
   bool buffer_exist = false;
   {