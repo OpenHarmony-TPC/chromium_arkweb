OpenHarmony-TPC/chromium_arkweb

commit 5fa5650d663cff720a3711cf4cc37153e74f2da0

Delete appcaches when missing entries are detected.
BUG=88968,50657
TEST=appcache_service_unittest.cc
Review URL: http://codereview.chromium.org/7342031

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93736 0039d316-1c4b-4281-b951-d872f2087c98

webkit/appcache/appcache_url_request_job.cc
@@ -17,6 +17,7 @@
 #include "net/http/http_util.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_status.h"
+#include "webkit/appcache/appcache_service.h"
 
 namespace appcache {
 
@@ -134,8 +135,9 @@ void AppCacheURLRequestJob::OnResponseInfoLoaded(
     // from the appcache.
     cache_entry_not_found_ = true;
     NotifyRestartRequired();
+    storage_->service()->CheckAppCacheResponse(manifest_url_, cache_id_,
+                                               entry_.response_id());
   }
-  storage_ = NULL;  // no longer needed
 }
 
 const net::HttpResponseInfo* AppCacheURLRequestJob::http_info() const {
@@ -187,13 +189,15 @@ void AppCacheURLRequestJob::SetupRangeResponse() {
 
 void AppCacheURLRequestJob::OnReadComplete(int result) {
   DCHECK(is_delivering_appcache_response());
-  if (result == 0)
+  if (result == 0) {
     NotifyDone(net::URLRequestStatus());
-  else if (result < 0)
+  } else if (result < 0) {
     NotifyDone(net::URLRequestStatus(net::URLRequestStatus::FAILED, result));
-  else
+    storage_->service()->CheckAppCacheResponse(manifest_url_, cache_id_,
+                                               entry_.response_id());
+  } else {
     SetStatus(net::URLRequestStatus());  // Clear the IO_PENDING status
-
+  }
   NotifyReadComplete(result);
 }
 

webkit/appcache/appcache.cc
@@ -70,6 +70,15 @@ AppCacheEntry* AppCache::GetEntry(const GURL& url) {
   return (it != entries_.end()) ? &(it->second) : NULL;
 }
 
+const AppCacheEntry* AppCache::GetEntryWithResponseId(int64 response_id) {
+  for (EntryMap::const_iterator iter = entries_.begin();
+       iter !=  entries_.end(); ++iter) {
+    if (iter->second.response_id() == response_id)
+      return &iter->second;
+  }
+  return NULL;
+}
+
 GURL AppCache::GetFallbackEntryUrl(const GURL& namespace_url) const {
   size_t count = fallback_namespaces_.size();
   for (size_t i = 0; i < count; ++i) {

webkit/appcache/appcache.h
@@ -55,6 +55,7 @@ class AppCache : public base::RefCounted<AppCache> {
 
   // Do not store the returned object as it could be deleted anytime.
   AppCacheEntry* GetEntry(const GURL& url);
+  const AppCacheEntry* GetEntryWithResponseId(int64 response_id);
 
   const EntryMap& entries() const { return entries_; }
 

webkit/appcache/appcache_histograms.cc
@@ -15,4 +15,12 @@ void AppCacheHistograms::CountInitResult(InitResultType init_result) {
        init_result, NUM_INIT_RESULT_TYPES);
 }
 
+// static
+void AppCacheHistograms::CountCheckResponseResult(
+    CheckResponseResultType result) {
+  UMA_HISTOGRAM_ENUMERATION(
+       "appcache.CheckResponseResult",
+       result, NUM_CHECK_RESPONSE_RESULT_TYPES);
+}
+
 }  // namespace appcache

webkit/appcache/appcache_histograms.h
@@ -1,9 +1,12 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef WEBKIT_APPCACHE_APPCACHE_HISTOGRAMS_H_
 #define WEBKIT_APPCACHE_APPCACHE_HISTOGRAMS_H_
+#pragma once
+
+#include "base/basictypes.h"
 
 namespace appcache {
 
@@ -14,6 +17,16 @@ class AppCacheHistograms {
     NUM_INIT_RESULT_TYPES
   };
   static void CountInitResult(InitResultType init_result);
+
+  enum CheckResponseResultType {
+    RESPONSE_OK, MANIFEST_OUT_OF_DATE, RESPONSE_OUT_OF_DATE, ENTRY_NOT_FOUND,
+    READ_HEADERS_ERROR, READ_DATA_ERROR, UNEXPECTED_DATA_SIZE, CHECK_CANCELED,
+    NUM_CHECK_RESPONSE_RESULT_TYPES
+  };
+  static void CountCheckResponseResult(CheckResponseResultType result);
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(AppCacheHistograms);
 };
 
 }  // namespace appcache

webkit/appcache/appcache_response.cc
@@ -132,6 +132,7 @@ void AppCacheResponseIO::WriteRaw(int index, int offset,
 }
 
 void AppCacheResponseIO::OnRawIOComplete(int result) {
+  DCHECK_NE(net::ERR_IO_PENDING, result);
   raw_callback_->Release();  // Balance the AddRefs
   OnIOComplete(result);
 }
@@ -214,11 +215,12 @@ void AppCacheResponseReader::SetReadRange(int offset, int length) {
 void AppCacheResponseReader::OnIOComplete(int result) {
   if (result >= 0) {
     if (info_buffer_.get()) {
-      // Deserialize the http info structure.
+      // Deserialize the http info structure, ensuring we got headers.
       Pickle pickle(buffer_->data(), result);
       scoped_ptr<net::HttpResponseInfo> info(new net::HttpResponseInfo);
       bool response_truncated = false;
-      if (!info->InitFromPickle(pickle, &response_truncated)) {
+      if (!info->InitFromPickle(pickle, &response_truncated) ||
+          !info->headers) {
         InvokeUserCompletionCallback(net::ERR_FAILED);
         return;
       }
@@ -289,6 +291,7 @@ void AppCacheResponseWriter::WriteInfo(HttpResponseInfoIOBuffer* info_buf,
   DCHECK(callback && !IsWritePending());
   DCHECK(info_buf && info_buf->http_info.get());
   DCHECK(!buffer_.get() && !info_buffer_.get());
+  DCHECK(info_buf->http_info->headers);
 
   info_buffer_ = info_buf;
   user_callback_ = callback;  // cleared on completion

webkit/appcache/appcache_response.h
@@ -113,11 +113,11 @@ class AppCacheResponseIO {
   scoped_refptr<net::IOBuffer> buffer_;
   int buffer_len_;
   net::CompletionCallback* user_callback_;
+  ScopedRunnableMethodFactory<AppCacheResponseIO> method_factory_;
 
  private:
   void OnRawIOComplete(int result);
 
-  ScopedRunnableMethodFactory<AppCacheResponseIO> method_factory_;
   scoped_refptr<net::CancelableCompletionCallback<AppCacheResponseIO> >
       raw_callback_;
 };
@@ -139,8 +139,9 @@ class AppCacheResponseReader : public AppCacheResponseIO {
   // contain a NULL http_info when ReadInfo is called. The 'callback' is a
   // required parameter.
   // Should only be called where there is no Read operation in progress.
-  void ReadInfo(HttpResponseInfoIOBuffer* info_buf,
-                net::CompletionCallback* callback);
+  // (virtual for testing)
+  virtual void ReadInfo(HttpResponseInfoIOBuffer* info_buf,
+                        net::CompletionCallback* callback);
 
   // Reads data from storage. Always returns the result of the read
   // asynchronously through the 'callback'. Returns the number of bytes read
@@ -149,8 +150,9 @@ class AppCacheResponseReader : public AppCacheResponseIO {
   // at which time the callback is invoked with either a negative error code
   // or the number of bytes read. The 'callback' is a required parameter.
   // Should only be called where there is no Read operation in progress.
-  void ReadData(net::IOBuffer* buf, int buf_len,
-                net::CompletionCallback* callback);
+  // (virtual for testing)
+  virtual void ReadData(net::IOBuffer* buf, int buf_len,
+                        net::CompletionCallback* callback);
 
   // Returns true if there is a read operation, for data or info, pending.
   bool IsReadPending() { return IsIOPending(); }
@@ -160,7 +162,7 @@ class AppCacheResponseReader : public AppCacheResponseIO {
   // to the first call to the ReadData method.
   void SetReadRange(int offset, int length);
 
- private:
+ protected:
   friend class AppCacheStorageImpl;
   friend class MockAppCacheStorage;
 

webkit/appcache/appcache_service.cc
@@ -7,9 +7,13 @@
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/stl_util.h"
+#include "net/base/io_buffer.h"
+#include "webkit/appcache/appcache.h"
 #include "webkit/appcache/appcache_backend_impl.h"
 #include "webkit/appcache/appcache_entry.h"
+#include "webkit/appcache/appcache_histograms.h"
 #include "webkit/appcache/appcache_quota_client.h"
+#include "webkit/appcache/appcache_response.h"
 #include "webkit/appcache/appcache_storage_impl.h"
 #include "webkit/quota/quota_manager.h"
 #include "webkit/quota/special_storage_policy.h"
@@ -267,6 +271,136 @@ void AppCacheService::GetInfoHelper::OnAllInfo(
   delete this;
 }
 
+// CheckResponseHelper -------
+
+class AppCacheService::CheckResponseHelper : AsyncHelper {
+ public:
+  CheckResponseHelper(
+      AppCacheService* service, const GURL& manifest_url, int64 cache_id,
+      int64 response_id)
+      : AsyncHelper(service, NULL),
+        manifest_url_(manifest_url),
+        cache_id_(cache_id),
+        response_id_(response_id),
+        kIOBufferSize(32 * 1024),
+        expected_total_size_(0),
+        amount_headers_read_(0),
+        amount_data_read_(0),
+        ALLOW_THIS_IN_INITIALIZER_LIST(read_info_callback_(
+            this, &CheckResponseHelper::OnReadInfoComplete)),
+        ALLOW_THIS_IN_INITIALIZER_LIST(read_data_callback_(
+            this, &CheckResponseHelper::OnReadDataComplete)) {
+  }
+
+  virtual void Start() {
+    service_->storage()->LoadOrCreateGroup(manifest_url_, this);
+  }
+
+  virtual void Cancel() {
+    AppCacheHistograms::CountCheckResponseResult(
+        AppCacheHistograms::CHECK_CANCELED);
+    response_reader_.reset();
+    AsyncHelper::Cancel();
+  }
+
+ private:
+  virtual void OnGroupLoaded(AppCacheGroup* group, const GURL& manifest_url);
+  void OnReadInfoComplete(int result);
+  void OnReadDataComplete(int result);
+
+  // Inputs describing what to check.
+  GURL manifest_url_;
+  int64 cache_id_;
+  int64 response_id_;
+
+  // Internals used to perform the checks.
+  const int kIOBufferSize;
+  scoped_refptr<AppCache> cache_;
+  scoped_ptr<AppCacheResponseReader> response_reader_;
+  scoped_refptr<HttpResponseInfoIOBuffer> info_buffer_;
+  scoped_refptr<net::IOBuffer> data_buffer_;
+  int64 expected_total_size_;
+  int amount_headers_read_;
+  int amount_data_read_;
+  net::CompletionCallbackImpl<CheckResponseHelper> read_info_callback_;
+  net::CompletionCallbackImpl<CheckResponseHelper> read_data_callback_;
+  DISALLOW_COPY_AND_ASSIGN(CheckResponseHelper);
+};
+
+void AppCacheService::CheckResponseHelper::OnGroupLoaded(
+    AppCacheGroup* group, const GURL& manifest_url) {
+  DCHECK_EQ(manifest_url_, manifest_url);
+  if (!group || !group->newest_complete_cache() || group->is_being_deleted() ||
+      group->is_obsolete()) {
+    AppCacheHistograms::CountCheckResponseResult(
+        AppCacheHistograms::MANIFEST_OUT_OF_DATE);
+    delete this;
+    return;
+  }
+
+  cache_ = group->newest_complete_cache();
+  const AppCacheEntry* entry = cache_->GetEntryWithResponseId(response_id_);
+  if (!entry) {
+    if (cache_->cache_id() == cache_id_) {
+      AppCacheHistograms::CountCheckResponseResult(
+          AppCacheHistograms::ENTRY_NOT_FOUND);
+      service_->DeleteAppCacheGroup(manifest_url_, NULL);
+    } else {
+      AppCacheHistograms::CountCheckResponseResult(
+          AppCacheHistograms::RESPONSE_OUT_OF_DATE);
+    }
+    delete this;
+    return;
+  }
+
+  // Verify that we can read the response info and data.
+  expected_total_size_ = entry->response_size();
+  response_reader_.reset(service_->storage()->CreateResponseReader(
+      manifest_url_, response_id_));
+  info_buffer_ = new HttpResponseInfoIOBuffer();
+  response_reader_->ReadInfo(info_buffer_, &read_info_callback_);
+}
+
+void AppCacheService::CheckResponseHelper::OnReadInfoComplete(int result) {
+  if (result < 0) {
+    AppCacheHistograms::CountCheckResponseResult(
+        AppCacheHistograms::READ_HEADERS_ERROR);
+    service_->DeleteAppCacheGroup(manifest_url_, NULL);
+    delete this;
+    return;
+  }
+  amount_headers_read_ = result;
+
+  // Start reading the data.
+  data_buffer_ = new net::IOBuffer(kIOBufferSize);
+  response_reader_->ReadData(data_buffer_, kIOBufferSize,
+                             &read_data_callback_);
+}
+
+void AppCacheService::CheckResponseHelper::OnReadDataComplete(int result) {
+  if (result > 0) {
+    // Keep reading until we've read thru everything or failed to read.
+    amount_data_read_ += result;
+    response_reader_->ReadData(data_buffer_, kIOBufferSize,
+                               &read_data_callback_);
+    return;
+  }
+
+  AppCacheHistograms::CheckResponseResultType check_result;
+  if (result < 0)
+    check_result = AppCacheHistograms::READ_DATA_ERROR;
+  else if (info_buffer_->response_data_size != amount_data_read_ ||
+           expected_total_size_ != amount_data_read_ + amount_headers_read_)
+    check_result = AppCacheHistograms::UNEXPECTED_DATA_SIZE;
+  else
+    check_result = AppCacheHistograms::RESPONSE_OK;
+  AppCacheHistograms::CountCheckResponseResult(check_result);
+
+  if (check_result != AppCacheHistograms::RESPONSE_OK)
+    service_->DeleteAppCacheGroup(manifest_url_, NULL);
+  delete this;
+}
+
 
 // AppCacheService -------
 
@@ -326,6 +460,14 @@ void AppCacheService::DeleteAppCacheGroup(const GURL& manifest_url,
 void AppCacheService::DeleteAppCachesForOrigin(
     const GURL& origin,  net::CompletionCallback* callback) {
   DeleteOriginHelper* helper = new DeleteOriginHelper(this, origin, callback);
+  helper->Start();
+}
+
+void AppCacheService::CheckAppCacheResponse(const GURL& manifest_url,
+                                            int64 cache_id,
+                                            int64 response_id) {
+  CheckResponseHelper* helper = new CheckResponseHelper(
+      this, manifest_url, cache_id, response_id);
   helper->Start();
 }
 

commit 028985a5782da5842e14d40da8f53cfd52803dec

Fix a problem introduced in r93736.
Initiate the 'check' of the response prior to calling out via NotifyDone or NotifyRestartRequired. The job may be killed prior to those notification methods returning in which case the storage_ ptr is not valid.

BUG=none
Review URL: http://codereview.chromium.org/7493047

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93918 0039d316-1c4b-4281-b951-d872f2087c98

webkit/appcache/appcache_url_request_job.cc
@@ -133,10 +133,10 @@ void AppCacheURLRequestJob::OnResponseInfoLoaded(
     // Instead of failing the request, we restart the request. The retry
     // attempt will fallthru to the network instead of trying to load
     // from the appcache.
-    cache_entry_not_found_ = true;
-    NotifyRestartRequired();
     storage_->service()->CheckAppCacheResponse(manifest_url_, cache_id_,
                                                entry_.response_id());
+    cache_entry_not_found_ = true;
+    NotifyRestartRequired();
   }
 }
 
@@ -192,9 +192,9 @@ void AppCacheURLRequestJob::OnReadComplete(int result) {
   if (result == 0) {
     NotifyDone(net::URLRequestStatus());
   } else if (result < 0) {
-    NotifyDone(net::URLRequestStatus(net::URLRequestStatus::FAILED, result));
     storage_->service()->CheckAppCacheResponse(manifest_url_, cache_id_,
                                                entry_.response_id());
+    NotifyDone(net::URLRequestStatus(net::URLRequestStatus::FAILED, result));
   } else {
     SetStatus(net::URLRequestStatus());  // Clear the IO_PENDING status
   }