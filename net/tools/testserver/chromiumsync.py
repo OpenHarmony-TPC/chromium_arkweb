OpenHarmony-TPC/chromium_arkweb

commit 6349a0205ff12b4c8d47cb61faa325ae684d962d

Simulate transient error and verify exponential backoff.
BUG=
TEST=


Review URL: http://codereview.chromium.org/7477004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96515 0039d316-1c4b-4281-b951-d872f2087c98

net/tools/testserver/chromiumsync.py
@@ -101,6 +101,10 @@ class StoreBirthdayError(Error):
   """The client sent a birthday that doesn't correspond to this server."""
 
 
+class TransientError(Error):
+  """The client would be sent a transient error."""
+
+
 def GetEntryType(entry):
   """Extract the sync type from a SyncEntry.
 
@@ -877,6 +881,7 @@ class TestServer(object):
     self.clients = {}
     self.client_name_generator = ('+' * times + chr(c)
         for times in xrange(0, sys.maxint) for c in xrange(ord('A'), ord('Z')))
+    self.transient_error = False
 
   def GetShortClientName(self, query):
     parsed = cgi.parse_qs(query[query.find('?')+1:])
@@ -894,6 +899,11 @@ class TestServer(object):
       return
     if self.account.StoreBirthday() != request.store_birthday:
       raise StoreBirthdayError
+
+  def CheckTransientError(self):
+    """Raises TransientError if transient_error variable is set."""
+    if self.transient_error:
+      raise TransientError
 
   def HandleMigrate(self, path):
     query = urlparse.urlparse(path)[4]
@@ -923,6 +933,12 @@ class TestServer(object):
         200,
         '<html><title>Birthday error</title><H1>Birthday error</H1></html>')
 
+  def HandleSetTransientError(self):
+    self.transient_error = True
+    return (
+        200,
+        '<html><title>Transient error</title><H1>Transient error</H1></html>')
+
   def HandleCommand(self, query, raw_request):
     """Decode and handle a sync command from a raw input of bytes.
 
@@ -951,6 +967,7 @@ class TestServer(object):
       response.error_code = sync_pb2.ClientToServerResponse.SUCCESS
       self.CheckStoreBirthday(request)
       response.store_birthday = self.account.store_birthday
+      self.CheckTransientError();
 
       print_context('->')
 
@@ -987,6 +1004,12 @@ class TestServer(object):
       response = sync_pb2.ClientToServerResponse()
       response.store_birthday = self.account.store_birthday
       response.error_code = sync_pb2.ClientToServerResponse.NOT_MY_BIRTHDAY
+      return (200, response.SerializeToString())
+    except TransientError as error:
+      print_context('<-')
+      print 'TRANSIENT_ERROR'
+      response.store_birthday = self.account.store_birthday
+      response.error_code = sync_pb2.ClientToServerResponse.TRANSIENT_ERROR
       return (200, response.SerializeToString())
     finally:
       self.account_lock.release()

chrome/browser/sync/backend_migrator_unittest.cc
@@ -55,7 +55,8 @@ class BackendMigratorTest : public TestingBrowserProcessTest {
 
     snap_.reset(new SyncSessionSnapshot(SyncerStatus(), ErrorCounters(),
         0, false, syncable::ModelTypeBitSet(), download_progress_markers,
-        false, false, 0, 0, 0, false, sessions::SyncSourceInfo(), 0));
+        false, false, 0, 0, 0, false, sessions::SyncSourceInfo(), 0,
+        base::Time::Now()));
     EXPECT_CALL(service_, GetLastSessionSnapshot())
         .WillOnce(Return(snap_.get()));
   }

chrome/browser/sync/engine/all_status.cc
@@ -53,8 +53,9 @@ sync_api::SyncManager::Status AllStatus::CalcSyncing(
   // But this is only used for status, so it is better to have visibility.
   status.conflicting_count += snapshot->num_conflicting_updates;
 
-  status.syncing |= snapshot->syncer_status.syncing;
-  status.syncing = snapshot->has_more_to_sync && snapshot->is_silenced;
+  status.syncing |= snapshot->syncer_status.sync_in_progress;
+  status.syncing =
+      snapshot->has_more_to_sync && snapshot->is_silenced;
   status.initial_sync_ended |= snapshot->is_share_usable;
   status.syncer_stuck |= snapshot->syncer_status.syncer_stuck;
 

chrome/browser/sync/engine/process_updates_command.cc
@@ -67,9 +67,6 @@ void ProcessUpdatesCommand::ModelChangingExecuteImpl(SyncSession* session) {
   status->set_num_consecutive_errors(0);
 
   status->mutable_update_progress()->ClearVerifiedUpdates();
-
-  // TODO(nick): The following line makes no sense to me.
-  status->set_syncing(true);
   return;
 }
 

chrome/browser/sync/engine/syncer.cc
@@ -54,6 +54,15 @@ using sessions::StatusController;
 using sessions::SyncSession;
 using sessions::ConflictProgress;
 
+Syncer::ScopedSyncStartStopTracker::ScopedSyncStartStopTracker(
+    sessions::SyncSession* session) : session_(session) {
+  session_->status_controller()->SetSyncInProgressAndUpdateStartTime(true);
+}
+
+Syncer::ScopedSyncStartStopTracker::~ScopedSyncStartStopTracker() {
+  session_->status_controller()->SetSyncInProgressAndUpdateStartTime(false);
+}
+
 Syncer::Syncer()
     : early_exit_requested_(false),
       pre_conflict_resolution_closure_(NULL) {
@@ -83,6 +92,8 @@ void Syncer::SyncShare(sessions::SyncSession* session,
 
   ScopedSessionContextConflictResolver scoped(session->context(),
                                               &resolver_);
+
+  ScopedSyncStartStopTracker start_stop_tracker(session);
   SyncerStep current_step = first_step;
 
   SyncerStep next_step = current_step;
@@ -168,8 +179,6 @@ void Syncer::SyncShare(sessions::SyncSession* session,
       // These two steps are combined since they are executed within the same
       // write transaction.
       case BUILD_COMMIT_REQUEST: {
-        session->status_controller()->set_syncing(true);
-
         VLOG(1) << "Processing Commit Request";
         ScopedDirLookup dir(session->context()->directory_manager(),
                             session->context()->account_name());

chrome/browser/sync/engine/syncer.h
@@ -85,6 +85,16 @@ class Syncer {
                          SyncerStep first_step,
                          SyncerStep last_step);
 
+  class ScopedSyncStartStopTracker {
+   public:
+    explicit ScopedSyncStartStopTracker(sessions::SyncSession* session);
+    ~ScopedSyncStartStopTracker();
+   private:
+    sessions::SyncSession* session_;
+
+    DISALLOW_COPY_AND_ASSIGN(ScopedSyncStartStopTracker);
+  };
+
  private:
   // Implements the PROCESS_CLIENT_COMMAND syncer step.
   void ProcessClientCommand(sessions::SyncSession* session);

chrome/browser/sync/engine/syncer_end_command.cc
@@ -15,7 +15,6 @@ SyncerEndCommand::~SyncerEndCommand() {}
 
 void SyncerEndCommand::ExecuteImpl(sessions::SyncSession* session) {
   // Always send out a cycle ended notification, regardless of end-state.
-  session->status_controller()->set_syncing(false);
   SyncEngineEvent event(SyncEngineEvent::SYNC_CYCLE_ENDED);
   sessions::SyncSessionSnapshot snapshot(session->TakeSnapshot());
   event.snapshot = &snapshot;

chrome/browser/sync/js/js_sync_manager_observer_unittest.cc
@@ -106,7 +106,8 @@ TEST_F(JsSyncManagerObserverTest, OnSyncCycleCompleted) {
                                          5,
                                          false,
                                          sessions::SyncSourceInfo(),
-                                         0);
+                                         0,
+                                         base::Time::Now());
   DictionaryValue expected_details;
   expected_details.Set("snapshot", snapshot.ToValue());
 

commit 0320ae3ee667b236c07089faff1fe559631607af

Fix syntax error introduced in r96515.
This should fix the sync server so it runs again in
our tests.

R=lipalani@chromium.org


Review URL: http://codereview.chromium.org/7789029

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100094 0039d316-1c4b-4281-b951-d872f2087c98

net/tools/testserver/chromiumsync.py
@@ -1029,7 +1029,7 @@ class TestServer(object):
       response.store_birthday = self.account.store_birthday
       response.error_code = sync_pb2.ClientToServerResponse.NOT_MY_BIRTHDAY
       return (200, response.SerializeToString())
-    except TransientError as error:
+    except TransientError, error:
       print_context('<-')
       print 'TRANSIENT_ERROR'
       response.store_birthday = self.account.store_birthday