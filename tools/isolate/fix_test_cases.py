OpenHarmony-TPC/chromium_arkweb

commit fa373a6447ca15bbaaf93d4b55acfdbcc6aaa145

Make run_test_cases.py abort early if 10% of test cases fails.
This includes retries, so in fact it can abort as soon as 3.34% of test cases
fail.

This will greatly increase the worst case scenario when all test cases on
browser_tests fail with a 1 minute timeout. In general, if a large number of
test cases fail, each additional failure doesn't add much information and more
often than fails with a timeout.

TBR=cmp@chromium.org,csharp@chromium.org
NOTRY=true
BUG=


Review URL: https://chromiumcodereview.appspot.com/10898016

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153890 0039d316-1c4b-4281-b951-d872f2087c98

tools/isolate/run_test_cases.py
@@ -469,8 +469,87 @@ def list_test_cases(executable, index, shards, disabled, fails, flaky):
   return filter_bad_tests(tests, disabled, fails, flaky)
 
 
+class RunSome(object):
+  """Thread-safe object deciding if testing should continue."""
+  def __init__(self, expected_count, retries, min_failures, max_failure_ratio):
+    """Determines if it is better to give up testing after an amount of failures
+    and successes.
+
+    Arguments:
+    - expected_count is the expected number of elements to run.
+    - retries is how many time a failing element can be retried. retries should
+      be set to the maximum number of retries per failure. This permits
+      dampening the curve to determine threshold where to stop.
+    - min_failures is the minimal number of failures to tolerate, to put a lower
+      limit when expected_count is small. This value is multiplied by the number
+      of retries.
+    - max_failure_ratio is the the ratio of permitted failures, e.g. 0.1 to stop
+      after 10% of failed test cases.
+
+    For large values of expected_count, the number of tolerated failures will be
+    at maximum "(expected_count * retries) * max_failure_ratio".
+
+    For small values of expected_count, the number of tolerated failures will be
+    at least "min_failures * retries".
+    """
+    assert 0 < expected_count
+    assert 0 <= retries < 100
+    assert 0 <= min_failures
+    assert 0. < max_failure_ratio < 1.
+    # Constants.
+    self._expected_count = expected_count
+    self._retries = retries
+    self._min_failures = min_failures
+    self._max_failure_ratio = max_failure_ratio
+
+    self._min_failures_tolerated = self._min_failures * self._retries
+    # Pre-calculate the maximum number of allowable failures. Note that
+    # _max_failures can be lower than _min_failures.
+    self._max_failures_tolerated = round(
+        (expected_count * retries) * max_failure_ratio)
+
+    # Variables.
+    self._lock = threading.Lock()
+    self._passed = 0
+    self._failures = 0
+
+  def should_stop(self):
+    """Stops once a threshold was reached. This includes retries."""
+    with self._lock:
+      # Accept at least the minimum number of failures.
+      if self._failures <= self._min_failures_tolerated:
+        return False
+      return self._failures >= self._max_failures_tolerated
+
+  def got_result(self, passed):
+    with self._lock:
+      if passed:
+        self._passed += 1
+      else:
+        self._failures += 1
+
+  def __str__(self):
+    return '%s(%d, %d, %d, %.3f)' % (
+        self.__class__.__name__,
+        self._expected_count,
+        self._retries,
+        self._min_failures,
+        self._max_failure_ratio)
+
+
+class RunAll(object):
+  """Never fails."""
+  @staticmethod
+  def should_stop():
+    return False
+  @staticmethod
+  def got_result(_):
+    pass
+
+
 class Runner(object):
-  def __init__(self, executable, cwd_dir, timeout, progress, retry_count=3):
+  def __init__(
+      self, executable, cwd_dir, timeout, progress, retry_count, decider):
     # Constants
     self.executable = executable
     self.cwd_dir = cwd_dir
@@ -480,6 +559,7 @@ class Runner(object):
     # It is important to remove the shard environment variables since it could
     # conflict with --gtest_filter.
     self.env = setup_gtest_env()
+    self.decider = decider
 
   def map(self, test_case):
     """Traces a single test case and returns its output."""
@@ -487,6 +567,9 @@ class Runner(object):
     cmd = fix_python_path(cmd)
     out = []
     for retry in range(self.retry_count):
+      if self.decider.should_stop():
+        break
+
       start = time.time()
       output, returncode = call_with_timeout(
           cmd,
@@ -505,6 +588,7 @@ class Runner(object):
       if '[ RUN      ]' not in output:
         # Can't find gtest marker, mark it as invalid.
         returncode = returncode or 1
+      self.decider.got_result(bool(returncode))
       out.append(data)
       if sys.platform == 'win32':
         output = output.replace('\r\n', '\n')
@@ -566,11 +650,20 @@ def LogResults(result_file, results):
     json.dump(results, f, sort_keys=True, indent=2)
 
 
-def run_test_cases(executable, test_cases, jobs, timeout, result_file):
+def run_test_cases(executable, test_cases, jobs, timeout, run_all, result_file):
   """Traces test cases one by one."""
+  if not test_cases:
+    return 0
   progress = Progress(len(test_cases))
+  retries = 3
+  if run_all:
+    decider = RunAll()
+  else:
+    # If 10% of test cases fail, just too bad.
+    decider = RunSome(len(test_cases), retries, 2, 0.1)
   with ThreadPool(jobs) as pool:
-    function = Runner(executable, os.getcwd(), timeout, progress).map
+    function = Runner(
+        executable, os.getcwd(), timeout, progress, retries, decider).map
     for test_case in test_cases:
       pool.add_task(function, test_case)
     results = pool.join(progress, 0.1)
@@ -607,6 +700,8 @@ def run_test_cases(executable, test_cases, jobs, timeout, result_file):
   for test_case in sorted(fail):
     print '%s failed' % (test_case)
 
+  if decider.should_stop():
+    print '** STOPPED EARLY due to high failure rate **'
   print 'Success: %4d %5.2f%%' % (len(success), len(success) * 100. / total)
   print 'Flaky:   %4d %5.2f%%' % (len(flaky), len(flaky) * 100. / total)
   print 'Fail:    %4d %5.2f%%' % (len(fail), len(fail) * 100. / total)
@@ -707,6 +802,10 @@ def main(argv):
       default=int(os.environ.get('ISOLATE_DEBUG', 0)),
       help='Use multiple times')
   parser.add_option(
+      '--run-all',
+      action='store_true',
+      help='Do not fail early when a large number of test cases fail')
+  parser.add_option(
       '--no-dump',
       action='store_true',
       help='do not generate a .run_test_cases file')
@@ -766,6 +865,7 @@ def main(argv):
       test_cases,
       options.jobs,
       options.timeout,
+      options.run_all,
       result_file)
 
 

tools/isolate/run_test_cases_test.py
@@ -99,11 +99,12 @@ class RunTestCases(unittest.TestCase):
   def test_gtest_filter(self):
     old = run_test_cases.run_test_cases
     exe = os.path.join(ROOT_DIR, 'data', 'gtest_fake', 'gtest_fake_pass.py')
-    def expect(executable, test_cases, jobs, timeout, result_file):
+    def expect(executable, test_cases, jobs, timeout, run_all, result_file):
       self.assertEquals(exe, executable)
       self.assertEquals(['Foo.Bar1', 'Foo.Bar3'], test_cases)
       self.assertEquals(run_test_cases.num_processors(), jobs)
       self.assertEquals(120, timeout)
+      self.assertEquals(None, run_all)
       self.assertEquals(exe + '.run_test_cases', result_file)
       return 89
     try:
@@ -112,6 +113,50 @@ class RunTestCases(unittest.TestCase):
       self.assertEquals(89, result)
     finally:
       run_test_cases.run_test_cases = old
+
+  def testRunSome(self):
+    tests = [
+        # Try with named arguments. Accepts 3*1 failures.
+        (
+          run_test_cases.RunSome(
+              expected_count=10,
+              retries=3,
+              min_failures=1,
+              max_failure_ratio=0.001),
+          [False] * 4),
+        # Same without named arguments.
+        (run_test_cases.RunSome(  10, 3, 1, 0.001), [False] * 4),
+
+        (run_test_cases.RunSome(  10, 1, 1, 0.001), [False] * 2),
+        (run_test_cases.RunSome(  10, 1, 1, 0.010), [False] * 2),
+
+        # For low expected_count value, retries * min_failures is the minimum
+        # bound of accepted failures.
+        (run_test_cases.RunSome(  10, 3, 1, 0.010), [False] * 4),
+        (run_test_cases.RunSome(  10, 3, 1, 0.020), [False] * 4),
+        (run_test_cases.RunSome(  10, 3, 1, 0.050), [False] * 4),
+        (run_test_cases.RunSome(  10, 3, 1, 0.100), [False] * 4),
+        (run_test_cases.RunSome(  10, 3, 1, 0.110), [False] * 4),
+
+        # Allows expected_count + retries failures at maximum.
+        (run_test_cases.RunSome(  10, 3, 1, 0.200), [False] * 6),
+        (run_test_cases.RunSome(  10, 3, 1, 0.999), [False] * 30),
+
+        # The asympthote is nearing max_failure_ratio for large expected_count
+        # values.
+        (run_test_cases.RunSome(1000, 3, 1, 0.050), [False] * 150),
+    ]
+    for index, (decider, rounds) in enumerate(tests):
+      for index2, r in enumerate(rounds):
+        self.assertFalse(decider.should_stop(), (index, index2, str(decider)))
+        decider.got_result(r)
+      self.assertTrue(decider.should_stop(), (index, str(decider)))
+
+  def testStatsInfinite(self):
+    decider = run_test_cases.RunAll()
+    for _ in xrange(200):
+      self.assertFalse(decider.should_stop())
+      decider.got_result(False)
 
 
 class WorkerPoolTest(unittest.TestCase):

tools/isolate/run_test_cases_smoke_test.py
@@ -17,17 +17,12 @@ sys.path.append(os.path.join(ROOT_DIR, 'data', 'gtest_fake'))
 import gtest_fake_base
 
 
-def RunTest(test_file, dump_file=None):
+def RunTest(test_file, extra_flags):
   target = os.path.join(ROOT_DIR, 'data', 'gtest_fake', test_file)
   cmd = [
       sys.executable,
       os.path.join(ROOT_DIR, 'run_test_cases.py'),
-  ]
-
-  if dump_file:
-    cmd.extend(['--result', dump_file])
-  else:
-    cmd.append('--no-dump')
+  ] + extra_flags
 
   cmd.append(target)
   logging.debug(' '.join(cmd))
@@ -76,8 +71,8 @@ class TraceTestCases(unittest.TestCase):
       self.assertEqual(entry_count, len(file_contents[entry_name]))
 
   def test_simple_pass(self):
-    out, err, return_code = RunTest('gtest_fake_pass.py',
-                                    dump_file=self.filename)
+    out, err, return_code = RunTest(
+        'gtest_fake_pass.py', ['--result', self.filename])
 
     self.assertEquals(0, return_code)
 
@@ -101,7 +96,8 @@ class TraceTestCases(unittest.TestCase):
     self._check_results_file(expected_result_file_entries)
 
   def test_simple_fail(self):
-    out, err, return_code = RunTest('gtest_fake_fail.py', self.filename)
+    out, err, return_code = RunTest(
+        'gtest_fake_fail.py', ['--result', self.filename])
 
     self.assertEquals(1, return_code)
 
@@ -141,7 +137,8 @@ class TraceTestCases(unittest.TestCase):
     self._check_results_file(expected_result_file_entries)
 
   def test_simple_gtest_list_error(self):
-    out, err, return_code = RunTest('gtest_fake_error.py')
+    out, err, return_code = RunTest(
+        'gtest_fake_error.py', ['--no-dump'])
 
     expected_out_re = [
         'Failed to run .+gtest_fake_error.py',

commit 1bddef4cf8ac6d9366d8f7a16a552e50c01b001f

Fix run_test_cases.py, it was broken by r153890.
1. It would crash if aborted early.
2. It would always abort early.

Fix fix_test_cases.py to run all test cases, even if ithe failure rate is too
high.

TBR=cmp@chromium.org,csharp@chromium.org
NOTRY=true
BUG=


Review URL: https://chromiumcodereview.appspot.com/10885033

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153934 0039d316-1c4b-4281-b951-d872f2087c98

tools/isolate/fix_test_cases.py
@@ -41,11 +41,14 @@ else:
 
 def trace_and_merge(result, test):
   """Traces a single test case and merges the result back into .isolate."""
+  env = os.environ.copy()
+  env['RUN_TEST_CASES_RUN_ALL'] = '1'
   subprocess.call(
       [
         sys.executable, 'isolate.py', 'trace', '-r', result,
         '--', '--gtest_filter=' + test,
-      ])
+      ],
+      env=env)
   return not subprocess.call(
       [sys.executable, 'isolate.py', 'merge', '-r', result])
 
@@ -59,6 +62,7 @@ def run_all(result, shard_index, shard_count):
   os.close(handle)
   env = os.environ.copy()
   env['RUN_TEST_CASES_RESULT_FILE'] = result_file
+  env['RUN_TEST_CASES_RUN_ALL'] = '1'
   env['GTEST_SHARD_INDEX'] = str(shard_index)
   env['GTEST_TOTAL_SHARDS'] = str(shard_count)
   cmd = [sys.executable, 'isolate.py', 'run', '-r', result]
@@ -116,7 +120,8 @@ def fix_all(result, shard_index, shard_count):
   """
   # These could have adverse side-effects.
   # TODO(maruel): Be more intelligent about it, for now be safe.
-  for i in run_test_cases.KNOWN_GTEST_ENV_VARS:
+  run_test_cases_env = ['RUN_TEST_CASES_RESULT_FILE', 'RUN_TEST_CASES_RUN_ALL']
+  for i in run_test_cases.KNOWN_GTEST_ENV_VARS + run_test_cases_env:
     if i in os.environ:
       print >> 'Please unset %s' % i
       return False

tools/isolate/run_test_cases.py
@@ -588,7 +588,7 @@ class Runner(object):
       if '[ RUN      ]' not in output:
         # Can't find gtest marker, mark it as invalid.
         returncode = returncode or 1
-      self.decider.got_result(bool(returncode))
+      self.decider.got_result(not bool(returncode))
       out.append(data)
       if sys.platform == 'win32':
         output = output.replace('\r\n', '\n')
@@ -668,7 +668,7 @@ def run_test_cases(executable, test_cases, jobs, timeout, run_all, result_file):
       pool.add_task(function, test_case)
     results = pool.join(progress, 0.1)
     duration = time.time() - progress.start
-  results = dict((item[0]['test_case'], item) for item in results)
+  results = dict((item[0]['test_case'], item) for item in results if item)
   LogResults(result_file, results)
   sys.stdout.write('\n')
   total = len(results)
@@ -804,6 +804,7 @@ def main(argv):
   parser.add_option(
       '--run-all',
       action='store_true',
+      default=bool(int(os.environ.get('RUN_TEST_CASES_RUN_ALL', '0'))),
       help='Do not fail early when a large number of test cases fail')
   parser.add_option(
       '--no-dump',

tools/isolate/run_test_cases_test.py
@@ -104,7 +104,7 @@ class RunTestCases(unittest.TestCase):
       self.assertEquals(['Foo.Bar1', 'Foo.Bar3'], test_cases)
       self.assertEquals(run_test_cases.num_processors(), jobs)
       self.assertEquals(120, timeout)
-      self.assertEquals(None, run_all)
+      self.assertEquals(False, run_all)
       self.assertEquals(exe + '.run_test_cases', result_file)
       return 89
     try: