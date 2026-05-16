0866faa06bd0dc1f7fbc4e2b3b7ef7b16b71f5a1
lgh-w <wulonghui2@huawei.com>
2026-05-16 14:43:55 +0800

    Adjust network connection declarations
    Signed-off-by: lgh-w <wulonghui2@huawei.com>

--- ohos_nweb/src/nweb_impl.h
+++ ohos_nweb/src/nweb_impl.h
@@ -745,9 +745,9 @@ class NWebImpl : public NWeb {
 #endif  // ARKWEB_VIDEO_ASSISTANT
 
 #if BUILDFLAG(ARKWEB_EX_NETWORK_CONNECTION)
-  static void SetConnectTimeout(int32_t seconds);
   static void BindToNetwork(int network_id);
 #endif
+  static void SetConnectTimeout(int32_t seconds);
   static void SetConnectionTimeout(int32_t timeout);
 
 #if BUILDFLAG(ARKWEB_EXT_UA)
