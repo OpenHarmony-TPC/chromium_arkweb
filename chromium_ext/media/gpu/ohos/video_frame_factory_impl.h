3023045565a59b643d4fb5d2d750a7b09151d291
zwpless1 <zhaowenpu1@huawei.com>
2026-05-26 10:54:26 +0800

    update: 更新文件 video_frame_factory_impl.h
    Signed-off-by: zwpless1 <zhaowenpu1@huawei.com>

--- chromium_ext/media/gpu/ohos/video_frame_factory_impl.h
+++ chromium_ext/media/gpu/ohos/video_frame_factory_impl.h
@@ -107,11 +107,11 @@ class MEDIA_GPU_EXPORT VideoFrameFactoryImpl
 #endif
   scoped_refptr<CodecBufferWaitCoordinator> codec_buffer_wait_coordinator_;
 
-  bool video_frame_copy_required_ =
+  bool video_frame_copy_required_ = base::ohos::IsEmulator() ||
 #if BUILDFLAG(ARKWEB_PIP)
                                     IsPipEnable() ||
 #endif
-                                    false;
+                                    base::SysInfo::IsLowEndDevice();
 
   std::unique_ptr<FrameInfoHelper> frame_info_helper_;
 
